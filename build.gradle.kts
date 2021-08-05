07b2764f47b79fc770e641de8a22f3d44251b2ed
David Herman <bit.spittle@gmail.com>
2021-08-05 09:20:23 -0700

    Upgrade compose dependency to 1.0.0-alpha
    See also:
    
    https://blog.jetbrains.com/kotlin/2021/08/compose-multiplatform-goes-alpha/

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     val kotlinVersion = "1.5.21"
-    val composeVersion = "0.5.0-build262"
+    val composeVersion = "1.0.0-alpha1"
     kotlin("multiplatform") version kotlinVersion apply false
     kotlin("jvm") version kotlinVersion apply false
     kotlin("plugin.serialization") version kotlinVersion apply false
