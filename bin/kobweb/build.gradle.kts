d8bd3866d3a95a1715b310bc54a83b7f4ad9c600
David Herman <bit.spittle@gmail.com>
2021-09-07 11:37:01 -0700

    Add konsole to deps

--- bin/kobweb/build.gradle.kts
+++ bin/kobweb/build.gradle.kts
@@ -8,11 +8,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://us-central1-maven.pkg.dev/varabyte-repos/public") }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(libs.kotlinx.cli)
+    implementation(libs.konsole)
 }
 
 application {
