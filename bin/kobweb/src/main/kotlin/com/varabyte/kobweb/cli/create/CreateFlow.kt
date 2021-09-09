2fa81ba08ca9088ba1811f75aa06aa8306c0c34e
David Herman <bit.spittle@gmail.com>
2021-09-09 10:26:55 -0700

    Use KonsoleApp#data to delete cloned path

--- bin/kobweb/src/main/kotlin/com/varabyte/kobweb/cli/create/CreateFlow.kt
+++ bin/kobweb/src/main/kotlin/com/varabyte/kobweb/cli/create/CreateFlow.kt
@@ -7,34 +7,35 @@ import com.varabyte.kobweb.cli.common.queryUser
 import com.varabyte.konsole.foundation.konsoleApp
 import com.varabyte.konsole.foundation.text.red
 import com.varabyte.konsole.foundation.text.textLine
+import com.varabyte.konsole.runtime.KonsoleApp
+import com.varabyte.konsole.runtime.concurrent.ConcurrentScopedData
+import com.varabyte.konsole.runtime.concurrent.createKey
 import org.eclipse.jgit.api.Git
+import java.io.File
 import java.nio.file.Files
+
+private val TempDirKey = KonsoleApp.Lifecycle.createKey<File>()
 
 fun runCreateFlow(template: String) = konsoleApp {
     val repo = "https://github.com/varabyte/kobweb-templates"
     val tempDir = Files.createTempDirectory("kobweb").toFile()
-    try {
-        if (processing("Cloning \"$repo\"") {
-                Git.cloneRepository()
-                    .setURI(repo)
-                    .setDirectory(tempDir)
-                    .call()
-            }) {
-            konsole { textLine() }.run()
-        } else {
-            konsole {
-                red {
-                    textLine("We were unable to fetch templates. Please check the repository path and your internet connection.")
-                }
-            }.run()
+    data.set(TempDirKey, tempDir, dispose = { tempDir.deleteRecursively() })
+    if (processing("Cloning \"$repo\"") {
+            Git.cloneRepository()
+                .setURI(repo)
+                .setDirectory(tempDir)
+                .call()
+        }) {
+        konsole { textLine() }.run()
+    } else {
+        konsole {
+            red {
+                textLine("We were unable to fetch templates. Please check the repository path and your internet connection.")
+            }
+        }.run()
 
-            return@konsoleApp
-        }
+        return@konsoleApp
     }
-    finally {
-        tempDir.deleteRecursively()
-    }
-
 
     val projectName = queryUser("What is your project named?", "My Project")
     val defaultFolderName = PathUtils.generateEmptyPathName(projectName.lowercase().filter { it.isLetterOrDigit() })
