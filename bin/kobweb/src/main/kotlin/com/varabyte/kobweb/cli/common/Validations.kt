5315bba276ea3a3fd950325cd02e867f26c8cff3
David Herman <bit.spittle@gmail.com>
2021-09-08 17:24:56 -0700

    Add support for empty path validation

--- bin/kobweb/src/main/kotlin/com/varabyte/kobweb/cli/common/Validations.kt
+++ bin/kobweb/src/main/kotlin/com/varabyte/kobweb/cli/common/Validations.kt
@@ -1,11 +1,31 @@
 package com.varabyte.kobweb.cli.common
 
+import java.nio.file.Files
+import java.nio.file.Path
+
 object Validations {
     fun folderName(name: String): String? {
-        return if (!name.all { it.isLetterOrDigit() || it == '-' || it == '_' }) {
+        return if (name != "." && !name.all { it.isLetterOrDigit() || it == '-' || it == '_' }) {
             "Invalid folder name. Can only contain: letters, digits, dash, underscore"
         } else {
             null
+        }
+    }
+
+    fun emptyPath(pathStr: String): String? {
+        Path.of(pathStr).let { path ->
+            return if (Files.exists(path)) {
+                if (Files.isRegularFile(path)) {
+                    "A file already exists at that location"
+                } else if (Files.newDirectoryStream(path).any()) {
+                    "The specified directory is not empty"
+                }
+                else {
+                    null
+                }
+            } else {
+                null
+            }
         }
     }
 }
--- bin/kobweb/src/main/kotlin/com/varabyte/kobweb/cli/create/CreateFlow.kt
+++ bin/kobweb/src/main/kotlin/com/varabyte/kobweb/cli/create/CreateFlow.kt
@@ -12,8 +12,17 @@ fun runCreateFlow(template: String) = konsoleApp {
     val processingAnim = konsoleAnimOf(listOf(".", "..", "..."), Duration.ofMillis(250))
 
     val projectName = queryUser("What is your project named?", "My Project")
-    val defaultFolderName = projectName.lowercase().filter { it.isLetterOrDigit() }
+    val defaultFolderName = run {
+        val nameBase = projectName.lowercase().filter { it.isLetterOrDigit() }
+        var finalName = nameBase
+        var i = 2
+        while (Validations.emptyPath(finalName) != null) {
+            finalName = "$nameBase$i"
+            i++
+        }
+        finalName
+    }
     val projectFolder = queryUser("Specify a folder for your project:", defaultFolderName) { answer ->
-        Validations.folderName(answer)
+        Validations.folderName(answer) ?: Validations.emptyPath(answer)
     }
 }
