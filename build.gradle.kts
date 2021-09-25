5d130a661ccb802de8cb1c66d26957203fc93b4e
David Herman <bit.spittle@gmail.com>
2021-09-25 15:11:58 -0700

    Update Gradle to remove JVM / Kotlin version warning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 // Plugins declared here instead of settings.gradle.kts because otherwise I get an error saying the kotlin plugin was
 // applied multiple times.
 plugins {
@@ -16,5 +18,17 @@ subprojects {
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+    }
+
+    val versionStr = JavaVersion.VERSION_1_8.toString()
+    tasks.withType<JavaCompile> {
+        sourceCompatibility = versionStr
+        targetCompatibility = versionStr
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = versionStr
+        }
     }
 }
