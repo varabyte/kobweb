2df8373cdc00a68080af0bdbe2642da7babf62cc
David Herman <bitspittle@gmail.com>
2021-10-03 23:02:46 -0700

    Support "Box" correctly using Grid with a single cell
    This now means if we write the following:
    
    ```
    Box {
      Text("A")
      Text("B")
      Text("C")
    }
    ```
    
    the items would render one on top of the other. (Before, we were using
    flexbox under the hood, so they would render as a column)

--- frontend/web-compose-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/foundation/layout/Box.kt
+++ frontend/web-compose-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/foundation/layout/Box.kt
@@ -11,49 +11,53 @@ import org.jetbrains.compose.web.dom.Div
 fun Box(
     modifier: Modifier = Modifier,
     contentAlignment: Alignment = Alignment.TopStart,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit = {}
 ) {
     Div(attrs = modifier.asAttributeBuilder {
         style {
-            display(DisplayStyle.Flex)
-            flexDirection(FlexDirection.Column)
+            // The Compose "Box" concept means: all children should be stacked one of top of the other. We do this by
+            // setting the current element to grid but then jam all of its children into its top-left (and only) cell.
+            display(DisplayStyle.Grid)
+            gridTemplateColumns("1fr")
+            gridTemplateRows("1fr")
 
             when {
+                // justify in grid means "row" while align means "col"
                 contentAlignment === Alignment.TopStart -> {
-                    justifyContent(JustifyContent.FlexStart)
-                    alignItems(AlignItems.FlexStart)
+                    alignItems(AlignItems.Start)
+                    justifyItems(AlignItems.Start.value)
                 }
                 contentAlignment === Alignment.TopCenter -> {
-                    justifyContent(JustifyContent.FlexStart)
-                    alignItems(AlignItems.Center)
+                    alignItems(AlignItems.Start)
+                    justifyItems(AlignItems.Center.value)
                 }
                 contentAlignment === Alignment.TopEnd -> {
-                    justifyContent(JustifyContent.FlexStart)
-                    alignItems(AlignItems.FlexEnd)
+                    alignItems(AlignItems.Start)
+                    justifyItems(AlignItems.End.value)
                 }
                 contentAlignment === Alignment.CenterStart -> {
-                    justifyContent(JustifyContent.Center)
-                    alignItems(AlignItems.FlexStart)
+                    alignItems(AlignItems.Center)
+                    justifyItems(AlignItems.Start.value)
                 }
                 contentAlignment === Alignment.Center -> {
-                    justifyContent(JustifyContent.Center)
                     alignItems(AlignItems.Center)
+                    justifyItems(AlignItems.Center.value)
                 }
                 contentAlignment === Alignment.CenterEnd -> {
-                    justifyContent(JustifyContent.Center)
-                    alignItems(AlignItems.FlexEnd)
-                }
-                contentAlignment === Alignment.BoottomStart -> {
-                    justifyContent(JustifyContent.FlexEnd)
-                    alignItems(AlignItems.FlexStart)
-                }
-                contentAlignment === Alignment.BoottomCenter -> {
-                    justifyContent(JustifyContent.FlexEnd)
+                    justifyItems(AlignItems.End.value)
                     alignItems(AlignItems.Center)
                 }
+                contentAlignment === Alignment.BoottomStart -> {
+                    justifyItems(AlignItems.Start.value)
+                    alignItems(AlignItems.End)
+                }
+                contentAlignment === Alignment.BoottomCenter -> {
+                    justifyItems(AlignItems.Center.value)
+                    alignItems(AlignItems.End)
+                }
                 contentAlignment === Alignment.BoottomEnd -> {
-                    justifyContent(JustifyContent.FlexEnd)
-                    alignItems(AlignItems.FlexEnd)
+                    justifyItems(AlignItems.End.value)
+                    alignItems(AlignItems.End)
                 }
             }
         }
