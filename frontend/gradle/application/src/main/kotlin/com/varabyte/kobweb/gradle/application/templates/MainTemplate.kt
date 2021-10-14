a28b818893991497a77f26847b613b875e2a7b48
David Herman <bitspittle@gmail.com>
2021-10-13 22:12:34 -0700

    Fix broken export
    I accidentally broke release codegen because imports were getting
    stripped while code was being left in.

--- frontend/gradle/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/templates/MainTemplate.kt
+++ frontend/gradle/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/templates/MainTemplate.kt
@@ -22,19 +22,20 @@ fun createMainFunction(appFqcn: String?, pageFqcnRoutes: Map<String, String>, ta
     imports.add(appFqcn ?: "com.varabyte.kobweb.core.DefaultApp")
     imports.sort()
 
-    return """
-        ${
-            imports.joinToString("\n        ") { fcqn -> "import $fcqn" }
+    return buildString {
+        imports.forEach { fqcn ->
+            appendLine("import $fqcn")
         }
+        appendLine()
+        if (target == BuildTarget.DEBUG) {
+            appendLine(
+                """
+            private fun forceReloadNow() {
+                window.stop()
+                window.location.reload()
+            }
 
-        private fun forceReloadNow() {
-            window.stop()
-            window.location.reload()
-        }
-
-        // In production, this will not be called and will get stripped out of the final javascript
-        private fun pollServerStatus() {
-            run {
+            private fun pollServerStatus() {
                 val status = document.getElementById("status")!!
                 var lastVersion: Int? = null
                 var shouldReload = false
@@ -105,35 +106,42 @@ fun createMainFunction(appFqcn: String?, pageFqcnRoutes: Map<String, String>, ta
                     timeout = 250,
                 )
             }
+                """.trimIndent()
+            )
+            appendLine()
         }
 
-        fun main() {
-            ${if (target == BuildTarget.DEBUG) "pollServerStatus()" else "" }
-
-            ${
-                // Generates lines like: Router.register("/about") { AboutPage() }
-                pageFqcnRoutes.entries.joinToString("\n            ") { entry ->
-                    val pageFqcn = entry.key
-                    val route = entry.value
-
-                    """Router.register("$route") { $pageFqcn() }"""
-                }
-            }
-            Router.navigateTo(window.location.pathname)
-
-            // For SEO, we may bake the contents of a page in at build time. However, we will overwrite them the first
-            // time we render this page with their composable, dynamic versions. Think of this as poor man's
-            // hydration :) See also: https://en.wikipedia.org/wiki/Hydration_(web_development)
-            val root = document.getElementById("root")!!
-            while (root.firstChild != null) {
-                root.removeChild(root.firstChild!!)
-            }
-
-            renderComposable(rootElementId = "root") {
-                ${appFqcn?.let { appFqcn.substringAfterLast('.') } ?: "DefaultApp"} {
-                    Router.renderActivePage()
-                }
-            }
+        appendLine("fun main() {")
+        if (target == BuildTarget.DEBUG) {
+            appendLine("    pollServerStatus()")
         }
-        """.trimIndent()
+        pageFqcnRoutes.entries.forEach { entry ->
+            val pageFqcn = entry.key
+            val route = entry.value
+
+            appendLine("""    Router.register("$route") { $pageFqcn() }""")
+        }
+
+        appendLine()
+        appendLine(
+            """
+                Router.navigateTo(window.location.pathname)
+
+                // For SEO, we may bake the contents of a page in at build time. However, we will overwrite them the first
+                // time we render this page with their composable, dynamic versions. Think of this as poor man's
+                // hydration :) See also: https://en.wikipedia.org/wiki/Hydration_(web_development)
+                val root = document.getElementById("root")!!
+                while (root.firstChild != null) {
+                    root.removeChild(root.firstChild!!)
+                }
+
+                renderComposable(rootElementId = "root") {
+                    ${appFqcn?.let { appFqcn.substringAfterLast('.') } ?: "DefaultApp"} {
+                        Router.renderActivePage()
+                    }
+                }
+            }
+            """.trimIndent()
+        )
+    }
 }
