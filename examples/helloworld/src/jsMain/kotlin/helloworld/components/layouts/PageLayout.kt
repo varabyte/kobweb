521c8f879e9ff608f5f8d92e424aa2caa9093c7f
David Herman <bitspittle@gmail.com>
2021-10-21 12:28:29 -0700

    Add "LaunchedEffect" demo to "PageLayout"
    ... demonstrating how to modify a document using a LaunchedEffect.

--- examples/helloworld/src/jsMain/kotlin/helloworld/components/layouts/PageLayout.kt
+++ examples/helloworld/src/jsMain/kotlin/helloworld/components/layouts/PageLayout.kt
@@ -1,16 +1,22 @@
 package helloworld.components.layouts
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.fillMaxSize
 import com.varabyte.kobweb.silk.components.text.Text
-import helloworld.components.sections.NavHeader
+import kotlinx.browser.document
 import org.jetbrains.compose.web.dom.H1
+import helloworld.components.sections.NavHeader
 
 @Composable
 fun PageLayout(title: String, content: @Composable () -> Unit) {
+    LaunchedEffect(title) {
+        document.title = title
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
