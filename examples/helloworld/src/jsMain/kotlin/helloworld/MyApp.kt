406fddf962bd351ca8e90628130049b8c81327c0
David Herman <bitspittle@gmail.com>
2021-10-22 21:40:34 -0700

    Update helloworld font to match the site template

--- examples/helloworld/src/jsMain/kotlin/helloworld/MyApp.kt
+++ examples/helloworld/src/jsMain/kotlin/helloworld/MyApp.kt
@@ -1,27 +1,34 @@
 package helloworld
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.height
 import com.varabyte.kobweb.compose.ui.width
 import com.varabyte.kobweb.core.App
-import com.varabyte.kobweb.core.DefaultApp
-import com.varabyte.kobweb.silk.components.layout.Surface
 import com.varabyte.kobweb.silk.SilkApp
+import com.varabyte.kobweb.silk.components.layout.Surface
 import com.varabyte.kobweb.silk.theme.SilkTheme
-import com.varabyte.kobweb.silk.theme.colors.rememberColorMode
-import org.jetbrains.compose.web.css.height
+import org.jetbrains.compose.web.css.Style
+import org.jetbrains.compose.web.css.StyleSheet
+import org.jetbrains.compose.web.css.fontFamily
 import org.jetbrains.compose.web.css.vh
 import org.jetbrains.compose.web.css.vw
-import org.jetbrains.compose.web.css.width
-import org.jetbrains.compose.web.dom.Div
+
+object CssGlobalsStyleSheet : StyleSheet() {
+    init {
+        "body" style {
+            fontFamily("-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
+                "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif")
+        }
+    }
+}
 
 @App
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     SilkApp {
         SilkTheme {
+            Style(CssGlobalsStyleSheet)
             Surface(Modifier.width(100.vw).height(100.vh)) {
                 content()
             }
