00f46395661d05328c16b1618767ede7f49e79a7
David Herman <bitspittle@gmail.com>
2021-11-29 22:25:06 -0800

    Add color mode key to helloworld app

--- examples/helloworld/src/jsMain/kotlin/helloworld/MyApp.kt
+++ examples/helloworld/src/jsMain/kotlin/helloworld/MyApp.kt
@@ -1,12 +1,17 @@
 package helloworld
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.core.App
+import com.varabyte.kobweb.silk.InitSilk
+import com.varabyte.kobweb.silk.InitSilkContext
 import com.varabyte.kobweb.silk.SilkApp
 import com.varabyte.kobweb.silk.components.layout.Surface
 import com.varabyte.kobweb.silk.theme.SilkTheme
+import com.varabyte.kobweb.silk.theme.colors.ColorMode
+import com.varabyte.kobweb.silk.theme.colors.getColorMode
+import kotlinx.browser.localStorage
 import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.css.StyleSheet
 import org.jetbrains.compose.web.css.fontFamily
@@ -24,10 +29,22 @@ object CssGlobalsStyleSheet : StyleSheet() {
     }
 }
 
+private const val COLOR_MODE_KEY = "helloworld:app:colorMode"
+
+@InitSilk
+fun updateTheme(ctx: InitSilkContext) {
+    ctx.config.initialColorMode = localStorage.getItem(COLOR_MODE_KEY)?.let { ColorMode.valueOf(it) } ?: ColorMode.DARK
+}
+
 @App
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     SilkApp {
+        val colorMode = getColorMode()
+        LaunchedEffect(colorMode) {
+            localStorage.setItem(COLOR_MODE_KEY, colorMode.name)
+        }
+
         Style(CssGlobalsStyleSheet)
         Surface(Modifier.width(100.vw).height(100.vh)) {
             content()
