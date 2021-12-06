fcf7a609083599f79ce5da94b5e045e0f446b8d4
David Herman <bitspittle@gmail.com>
2021-12-05 20:14:16 -0800

    Search for gradlew in ancestors if not found in curr dir
    Fixes #73

--- cli/kobweb/src/main/kotlin/com/varabyte/kobweb/cli/common/RuntimeUtils.kt
+++ cli/kobweb/src/main/kotlin/com/varabyte/kobweb/cli/common/RuntimeUtils.kt
@@ -4,14 +4,23 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.BufferedReader
+import java.io.File
+import java.io.FileNotFoundException
 import java.io.InputStream
 import java.io.InputStreamReader
 
 fun Runtime.gradlew(vararg args: String): Process {
-    val finalArgs = mutableListOf(
-        if (!Os.isWindows()) "./gradlew" else "./gradlew.bat"
-    )
+    val gradlewBinary = if (!Os.isWindows()) "gradlew" else "gradlew.bat"
+    var currDir: File? = File(".").absoluteFile
+    while (currDir != null && !File(currDir, gradlewBinary).exists()) {
+        currDir = currDir.parentFile
+    }
 
+    if (currDir == null) {
+        throw FileNotFoundException("\"$gradlewBinary\" not found in current or parent directories")
+    }
+
+    val finalArgs = mutableListOf("${currDir.absolutePath}/$gradlewBinary")
     finalArgs.addAll(args)
     return exec(finalArgs.toTypedArray())
 }
