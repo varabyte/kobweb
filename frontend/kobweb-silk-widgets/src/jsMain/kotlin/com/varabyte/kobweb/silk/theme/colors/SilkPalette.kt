8bb3aea220ec10c1c8f5e1938aa67ebedce2b987
David Herman <bitspittle@gmail.com>
2021-12-15 19:12:11 -0800

    Tweak dark link colors

--- frontend/kobweb-silk-widgets/src/jsMain/kotlin/com/varabyte/kobweb/silk/theme/colors/SilkPalette.kt
+++ frontend/kobweb-silk-widgets/src/jsMain/kotlin/com/varabyte/kobweb/silk/theme/colors/SilkPalette.kt
@@ -59,8 +59,9 @@ val DarkSilkPalette = run {
         background = Colors.Black,
         color = Colors.White,
         link = SilkPalette.Link(
-            default = Colors.Blue.lightened(),
-            visited = Colors.Purple.lightened(),
+            // One "lightened" still too dark against black background
+            default = Colors.Blue.lightened().lightened(),
+            visited = Colors.Purple.lightened().lightened(),
         ),
         button = SilkPalette.Button(
             default = buttonBase,
