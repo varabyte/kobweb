7307d986700e753a14b1c43c71a2a1e48f724c13
David Herman <bitspittle@gmail.com>
2022-01-14 10:45:45 -0800

    Fix issue with yes/no validation

--- cli/kobweb/src/main/kotlin/com/varabyte/kobweb/cli/create/freemarker/methods/BoolMethods.kt
+++ cli/kobweb/src/main/kotlin/com/varabyte/kobweb/cli/create/freemarker/methods/BoolMethods.kt
@@ -1,23 +1,21 @@
 package com.varabyte.kobweb.cli.create.freemarker.methods
 
-class YesNoToBoolMethod : SingleArgMethodModel() {
-    override fun exec(value: String): String {
+class IsYesNoMethod : SingleArgMethodModel() {
+    override fun exec(value: String): String? {
         val valueLower = value.lowercase()
-        return when {
-            "yes".startsWith(valueLower) -> "true"
-            "true".startsWith(valueLower) -> "true"
-            else -> "false"
+        return if (listOf("yes", "no", "true", "false").any { it.startsWith(valueLower) }) {
+            null
+        }
+        else {
+            "Answer must be yes or no"
         }
     }
 }
 
-class IsYesNoMethod : SingleArgMethodModel() {
+class YesNoToBoolMethod : SingleArgMethodModel() {
     override fun exec(value: String): String {
         val valueLower = value.lowercase()
-        return when {
-            "yes".startsWith(valueLower) -> "true"
-            "no".startsWith(valueLower) -> "true"
-            else -> "false"
-        }
+        return (listOf("yes", "true").any { it.startsWith(valueLower) }).toString()
     }
 }
+
