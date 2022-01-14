0ea778171dcfbf07d83e774007a0ad9c6f36730e
David Herman <bitspittle@gmail.com>
2022-01-14 09:03:45 -0800

    Remove obsolete comment from API header docs

--- backend/kobweb-api/src/main/kotlin/com/varabyte/kobweb/api/Api.kt
+++ backend/kobweb-api/src/main/kotlin/com/varabyte/kobweb/api/Api.kt
@@ -4,8 +4,8 @@ package com.varabyte.kobweb.api
  * An annotation which identifies a suspend function as one which will be used to serve the result of an API call.
  * The method should take an [ApiContext] as its only parameter.
  *
- * By default, the method's filename will be used to generate its slug, e.g. "api/user/Fetch.kt" ->
- * "/api/user/fetch", but you can provide a specific override if necessary.
+ * The method's filename will be used to generate its slug, e.g. "api/user/Fetch.kt" ->
+ * "/api/user/fetch".
  */
 @Target(AnnotationTarget.FUNCTION)
 annotation class Api
