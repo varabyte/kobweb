9d5bb8ab7e6b73dbcf63485d9d3bf639269c1d7d
David Herman <bitspittle@gmail.com>
2022-02-05 17:31:15 -0800

    Refactor jreleaser logic for better null handling

--- cli/kobweb/build.gradle.kts
+++ cli/kobweb/build.gradle.kts
@@ -31,96 +31,103 @@ tasks.jar {
     archiveFileName.set("kobweb-cli.jar")
 }
 
-// Read about JReleaser at https://jreleaser.org/guide/latest/index.html
-jreleaser {
-    dryrun.set(false) // Specified explicitly for convenience - set dryrun to true when experimenting with values!
-    gitRootSearch.set(true)
-    project {
-        website.set("https://kobweb.varabyte.com/")
-        docsUrl.set("https://kobweb.varabyte.com/docs")
-        description.set("Set up and manage your Compose for Web app")
-        longDescription.set("""
-            Kobweb CLI provides commands to handle the tedious parts of building a Compose for Web app, including
-            project setup and configuration.
-        """.trimIndent())
-        authors.set(listOf("David Herman"))
-        license.set("Apache-2.0")
-        copyright.set("Copyright © 2022 Varabyte. All rights reserved.")
-    }
-    release {
-        github {
-            owner.set("varabyte")
-            tagName.set("cli-v{{projectVersion}}")
+// These values are specified in ~/.gradle/gradle.properties; otherwise sorry, no jreleasing for you :P
+val (githubUsername, githubToken) = listOf("varabyte.github.username", "varabyte.github.token")
+    .map { key -> findProperty(key) as? String }
 
-            // Tags and releases are handled manually via the GitHub UI for now. TODO(#104)
-            skipTag.set(true)
-            skipRelease.set(true)
+if (githubUsername != null && githubToken != null) {
+    // Read about JReleaser at https://jreleaser.org/guide/latest/index.html
+    jreleaser {
+        dryrun.set(false) // Specified explicitly for convenience - set dryrun to true when experimenting with values!
+        gitRootSearch.set(true)
+        project {
+            website.set("https://kobweb.varabyte.com/")
+            docsUrl.set("https://kobweb.varabyte.com/docs")
+            description.set("Set up and manage your Compose for Web app")
+            longDescription.set(
+                """
+                Kobweb CLI provides commands to handle the tedious parts of building a Compose for Web app, including
+                project setup and configuration.
+            """.trimIndent()
+            )
+            authors.set(listOf("David Herman"))
+            license.set("Apache-2.0")
+            copyright.set("Copyright © 2022 Varabyte. All rights reserved.")
+        }
+        release {
+            github {
+                owner.set("varabyte")
+                tagName.set("cli-v{{projectVersion}}")
+                username.set(githubUsername)
+                token.set(githubToken)
 
-            overwrite.set(true)
-            uploadAssets.set(Active.RELEASE)
-            commitAuthor {
-                name.set("David Herman")
-                email.set("bitspittle@gmail.com")
-            }
-            changelog {
-                enabled.set(false)
-            }
-            milestone {
-                // milestone management handled manually for now
-                close.set(false)
-            }
-            prerelease {
-                enabled.set(false)
-            }
+                // Tags and releases are handled manually via the GitHub UI for now. TODO(#104)
+                skipTag.set(true)
+                skipRelease.set(true)
 
-            // These values are specified in ~/.gradle/gradle.properties; otherwise sorry, no jreleasing for you :P
-            mapOf<String, (String) -> Unit>(
-                "varabyte.github.username" to { username.set(it) },
-                "varabyte.github.token" to { token.set(it) },
-            ).forEach { (key, setter) ->
-                (findProperty(key) as? String)?.let { setter(it) } ?: run {
-                    println("\"$key\" is missing so disabling jreleaser config")
+                overwrite.set(true)
+                uploadAssets.set(Active.RELEASE)
+                commitAuthor {
+                    name.set("David Herman")
+                    email.set("bitspittle@gmail.com")
+                }
+                changelog {
+                    enabled.set(false)
+                }
+                milestone {
+                    // milestone management handled manually for now
+                    close.set(false)
+                }
+                prerelease {
                     enabled.set(false)
                 }
             }
         }
-    }
-    packagers {
-        // Redundant downloadUrl temporarily required. See https://github.com/jreleaser/jreleaser/issues/699
-        val artifactDownloadPath = "https://{{repoHost}}/{{repoOwner}}/{{repoName}}/releases/download/{{tagName}}/{{artifactFile}}"
-        brew {
-            active.set(Active.RELEASE)
-            downloadUrl.set(artifactDownloadPath)
-            templateDirectory.set(File("jreleaser/templates/brew"))
-        }
-        scoop {
-            active.set(Active.RELEASE)
-            downloadUrl.set(artifactDownloadPath)
+        packagers {
+            // Redundant downloadUrl temporarily required. See https://github.com/jreleaser/jreleaser/issues/699
+            val artifactDownloadPath =
+                "https://{{repoHost}}/{{repoOwner}}/{{repoName}}/releases/download/{{tagName}}/{{artifactFile}}"
+            brew {
+                active.set(Active.RELEASE)
+                downloadUrl.set(artifactDownloadPath)
+                templateDirectory.set(File("jreleaser/templates/brew"))
+            }
+            scoop {
+                active.set(Active.RELEASE)
+                downloadUrl.set(artifactDownloadPath)
+            }
+
+            // Renable when https://github.com/jreleaser/jreleaser/issues/737 is marked resolved
+            //        sdkman {
+            //            val (key, token) = listOf(findProperty("sdkman.key") as? String, findProperty("sdkman.token") as? String)
+            //            if (key != null && token != null) {
+            //                downloadUrl.set(artifactDownloadPath)
+            //                consumerKey.set(key)
+            //                consumerToken.set(token)
+            //                active.set(Active.RELEASE)
+            //            }
+            //            else {
+            //                println("SDKMAN! packager disabled on this machine since key and/or token are not defined")
+            //                active.set(Active.NEVER)
+            //            }
+            //        }
         }
 
-        // Renable when https://github.com/jreleaser/jreleaser/issues/737 is marked resolved
-//        sdkman {
-//            val (key, token) = listOf(findProperty("sdkman.key") as? String, findProperty("sdkman.token") as? String)
-//            if (key != null && token != null) {
-//                downloadUrl.set(artifactDownloadPath)
-//                consumerKey.set(key)
-//                consumerToken.set(token)
-//                active.set(Active.RELEASE)
-//            }
-//            else {
-//                println("SDKMAN! packager disabled on this machine since key and/or token are not defined")
-//                active.set(Active.NEVER)
-//            }
-//        }
-    }
-
-    distributions {
-        create("kobweb") {
-            listOf("zip", "tar").forEach { artifactExtension ->
-                artifact {
-                    setPath("build/distributions/{{distributionName}}-{{projectVersion}}.$artifactExtension")
+        distributions {
+            create("kobweb") {
+                listOf("zip", "tar").forEach { artifactExtension ->
+                    artifact {
+                        setPath("build/distributions/{{distributionName}}-{{projectVersion}}.$artifactExtension")
+                    }
                 }
             }
         }
     }
+} else {
+    println(
+        """
+            NOTE: JReleaser disabled for this machine due to missing github username and/or token properties.
+            This is expected (unless you intentionally configured these values).
+        """.trimIndent()
+    )
 }
