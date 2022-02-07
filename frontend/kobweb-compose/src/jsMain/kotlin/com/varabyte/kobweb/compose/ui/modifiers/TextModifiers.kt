79222f233c1e65eb0fbbfeaef0b37be944bad963
David Herman <bitspittle@gmail.com>
2022-02-07 10:30:48 -0800

    Add letterSpacing Modifier extension
    Also combine font and text modifiers together to simplify package
    organization a bit.

--- frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/TextModifiers.kt
+++ frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/TextModifiers.kt
@@ -5,6 +5,39 @@ import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.attrModifier
 import com.varabyte.kobweb.compose.ui.styleModifier
 import org.jetbrains.compose.web.attributes.DirType
+import org.jetbrains.compose.web.css.*
+
+fun Modifier.dir(dirType: DirType) = attrModifier {
+    dir(dirType)
+}
+
+fun Modifier.fontFamily(vararg values: String): Modifier = styleModifier {
+    fontFamily(*values)
+}
+
+fun Modifier.fontSize(value: CSSNumeric): Modifier = styleModifier {
+    fontSize(value)
+}
+
+fun Modifier.fontStyle(value: FontStyle): Modifier = styleModifier {
+    fontStyle(value)
+}
+
+fun Modifier.fontWeight(value: FontWeight): Modifier = styleModifier {
+    fontWeight(value)
+}
+
+fun Modifier.lang(value: String) = attrModifier {
+    lang(value)
+}
+
+fun Modifier.letterSpacing(value: CSSNumeric) = styleModifier {
+    letterSpacing(value)
+}
+
+fun Modifier.spellCheck(enabled: Boolean) = attrModifier {
+    spellCheck(enabled)
+}
 
 fun Modifier.textAlign(textAlign: TextAlign): Modifier = styleModifier {
     textAlign(textAlign)
@@ -16,16 +49,4 @@ fun Modifier.textDecorationLine(textDecorationLine: TextDecorationLine): Modifie
 
 fun Modifier.whiteSpace(whiteSpace: WhiteSpace): Modifier = styleModifier {
     whiteSpace(whiteSpace)
-}
-
-fun Modifier.dir(dirType: DirType) = attrModifier {
-    dir(dirType)
-}
-
-fun Modifier.spellCheck(enabled: Boolean) = attrModifier {
-    spellCheck(enabled)
-}
-
-fun Modifier.lang(value: String) = attrModifier {
-    lang(value)
 }
