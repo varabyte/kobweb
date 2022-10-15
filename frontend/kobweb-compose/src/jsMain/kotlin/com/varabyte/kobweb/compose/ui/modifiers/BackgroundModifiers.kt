1c3f986c3be8f7a4a74f569aff559cae74ecbbe3
David Herman <bitspittle@gmail.com>
2022-10-14 20:53:36 -0700

    Add missing "background" style modifiers

---
+++ frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/BackgroundModifiers.kt
@@ -0,0 +1,51 @@
+package com.varabyte.kobweb.compose.ui.modifiers
+
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Color
+import com.varabyte.kobweb.compose.ui.graphics.toCssColor
+import com.varabyte.kobweb.compose.ui.styleModifier
+import org.jetbrains.compose.web.css.*
+
+fun Modifier.background(value: String) = styleModifier {
+    background(value)
+}
+
+fun Modifier.backgroundAttachment(value: String) = styleModifier {
+    backgroundAttachment(value)
+}
+
+fun Modifier.backgroundClip(value: String) = styleModifier {
+    backgroundClip(value)
+}
+
+fun Modifier.backgroundColor(color: Color) = styleModifier {
+    backgroundColor(color.toCssColor())
+}
+
+fun Modifier.backgroundColor(value: String) = styleModifier {
+    property("background-color", value)
+}
+
+fun Modifier.backgroundColor(color: CSSColorValue) = styleModifier {
+    backgroundColor(color)
+}
+
+fun Modifier.backgroundImage(value: String) = styleModifier {
+    backgroundImage(value)
+}
+
+fun Modifier.backgroundOrigin(value: String) = styleModifier {
+    backgroundOrigin(value)
+}
+
+fun Modifier.backgroundPosition(value: String) = styleModifier {
+    backgroundPosition(value)
+}
+
+fun Modifier.backgroundRepeat(value: String) = styleModifier {
+    backgroundRepeat(value)
+}
+
+fun Modifier.backgroundSize(value: String) = styleModifier {
+    backgroundSize(value)
+}
--- frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/ColorModifiers.kt
+++ frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/ColorModifiers.kt
@@ -4,29 +4,7 @@ import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.Color
 import com.varabyte.kobweb.compose.ui.graphics.toCssColor
 import com.varabyte.kobweb.compose.ui.styleModifier
-import org.jetbrains.compose.web.css.CSSColorValue
-import org.jetbrains.compose.web.css.CSSSizeValue
-import org.jetbrains.compose.web.css.CSSUnit
-import org.jetbrains.compose.web.css.background
-import org.jetbrains.compose.web.css.backgroundColor
-import org.jetbrains.compose.web.css.color
-import org.jetbrains.compose.web.css.opacity
-
-fun Modifier.background(value: String) = styleModifier {
-    background(value)
-}
-
-fun Modifier.backgroundColor(color: Color) = styleModifier {
-    backgroundColor(color.toCssColor())
-}
-
-fun Modifier.backgroundColor(value: String) = styleModifier {
-    property("background-color", value)
-}
-
-fun Modifier.backgroundColor(color: CSSColorValue) = styleModifier {
-    backgroundColor(color)
-}
+import org.jetbrains.compose.web.css.*
 
 fun Modifier.color(color: Color) = styleModifier {
     color(color.toCssColor())
