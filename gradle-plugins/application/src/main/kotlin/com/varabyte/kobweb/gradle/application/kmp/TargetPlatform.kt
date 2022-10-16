e318a5c3a3186b3771e608c4ce4b6c9fbaf60d47
David Herman <bitspittle@gmail.com>
2022-10-15 17:39:31 -0700

    Nit: Change lazy calls to get()
    "by lazy" has a decent amount of overhead (it allocates an
    intermediate class and adds a bunch of thread guarding logic)
    which isn't worth it for immutable strings.

--- gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/kmp/TargetPlatform.kt
+++ gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/kmp/TargetPlatform.kt
@@ -19,19 +19,19 @@ interface TargetPlatform<T : KotlinTarget> {
 class JsTarget(private val target: KotlinJsIrTarget) : TargetPlatform<KotlinJsIrTarget> {
     override val name: String = target.name
 
-    val browserDevelopmentRun by lazy { "${target.name}BrowserDevelopmentRun" }
-    val browserProductionRun by lazy { "${target.name}BrowserProductionRun" }
-    val browserRun by lazy { "${target.name}BrowserRun" }
-    val run by lazy { "${target.name}Run" }
+    val browserDevelopmentRun get() = "${target.name}BrowserDevelopmentRun"
+    val browserProductionRun get() = "${target.name}BrowserProductionRun"
+    val browserRun get() = "${target.name}BrowserRun"
+    val run get() = "${target.name}Run"
 
-    override val compileKotlin by lazy { "compileKotlin${target.name.capitalize()}" }
-    val processResources by lazy { "${target.name}ProcessResources" }
+    override val compileKotlin get() = "compileKotlin${target.name.capitalize()}"
+    val processResources get() = "${target.name}ProcessResources"
 
-    val browserDevelopmentWebpack by lazy { "${target.name}BrowserDevelopmentWebpack" }
-    val browserProductionWebpack by lazy { "${target.name}BrowserProductionWebpack" }
+    val browserDevelopmentWebpack get() = "${target.name}BrowserDevelopmentWebpack"
+    val browserProductionWebpack get() = "${target.name}BrowserProductionWebpack"
 
-    val developmentExecutableCompileSync by lazy { "${target.name}DevelopmentExecutableCompileSync" }
-    val productionExecutableCompileSync by lazy { "${target.name}ProductionExecutableCompileSync" }
+    val developmentExecutableCompileSync get() = "${target.name}DevelopmentExecutableCompileSync"
+    val productionExecutableCompileSync get() = "${target.name}ProductionExecutableCompileSync"
 }
 
 val Project.jsTarget: JsTarget
@@ -41,9 +41,9 @@ val Project.jsTarget: JsTarget
 class JvmTarget(private val target: KotlinJvmTarget) : TargetPlatform<KotlinJvmTarget> {
     override val name: String = target.name
 
-    override val compileKotlin by lazy { "compileKotlin${target.name.capitalize()}" }
+    override val compileKotlin get() = "compileKotlin${target.name.capitalize()}"
 
-    val jar by lazy { "${target.name}Jar" }
+    val jar get() = "${target.name}Jar"
 }
 
 val Project.jvmTarget: JvmTarget?
