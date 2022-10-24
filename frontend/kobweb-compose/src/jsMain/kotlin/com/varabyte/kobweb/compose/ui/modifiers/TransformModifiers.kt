3625e2078e038da857e50344f7894f72e0c65289
David Herman <bitspittle@gmail.com>
2022-10-24 15:29:25 -0700

    Add a bunch of transform modifiers

--- frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/TransformModifiers.kt
+++ frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/TransformModifiers.kt
@@ -1,5 +1,6 @@
 package com.varabyte.kobweb.compose.ui.modifiers
 
+import com.varabyte.kobweb.compose.css.*
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.styleModifier
 import org.jetbrains.compose.web.ExperimentalComposeWebApi
@@ -10,3 +11,94 @@ fun Modifier.transform(transformContext: TransformBuilder.() -> Unit) = styleMod
     transform(transformContext)
 }
 
+fun Modifier.rotate(a: CSSAngleValue) = styleModifier {
+    rotate(a)
+}
+
+fun Modifier.rotate(x: Number, y: Number, z: Number, a: CSSAngleValue) = styleModifier {
+    rotate(x, y, z, a)
+}
+
+fun Modifier.rotateX(ax: CSSAngleValue) = styleModifier {
+    rotateX(ax)
+}
+
+fun Modifier.rotateY(ay: CSSAngleValue) = styleModifier {
+    rotateY(ay)
+}
+
+fun Modifier.rotateZ(az: CSSAngleValue) = styleModifier {
+    rotateZ(az)
+}
+
+fun Modifier.scale(s: Number) = styleModifier{
+    scale(s)
+}
+
+fun Modifier.scale(sx: Number, sy: Number) = styleModifier {
+    scale(sx, sy)
+}
+
+fun Modifier.scale(sx: Number, sy: Number, sz: Number) = styleModifier {
+    scale(sx, sy, sz)
+}
+
+fun Modifier.scaleX(s: Number) = styleModifier {
+    scaleX(s)
+}
+
+fun Modifier.scaleY(s: Number) = styleModifier {
+    scaleY(s)
+}
+
+fun Modifier.scaleZ(s: Number) = styleModifier {
+    scaleZ(s)
+}
+
+fun Modifier.scale(s: CSSPercentageValue) = styleModifier {
+    scale(s)
+}
+
+fun Modifier.scale(sx: CSSPercentageValue, sy: CSSPercentageValue) = styleModifier {
+    scale(sx, sy)
+}
+
+fun Modifier.scale(sx: CSSPercentageValue, sy: CSSPercentageValue, sz: CSSPercentageValue) = styleModifier {
+    scale(sx, sy, sz)
+}
+
+fun Modifier.scaleX(s: CSSPercentageValue) = styleModifier {
+    scaleX(s)
+}
+
+fun Modifier.scaleY(s: CSSPercentageValue) = styleModifier {
+    scaleY(s)
+}
+
+fun Modifier.scaleZ(s: CSSPercentageValue) = styleModifier {
+    scaleZ(s)
+}
+
+fun Modifier.translate(tx: CSSLengthOrPercentageValue) = styleModifier {
+    translate(tx)
+}
+
+fun Modifier.translate(tx: CSSLengthOrPercentageValue, ty: CSSLengthOrPercentageValue) = styleModifier {
+    translate(tx, ty)
+}
+
+fun Modifier.translate(tx: CSSLengthOrPercentageValue, ty: CSSLengthOrPercentageValue, tz: CSSLengthOrPercentageValue) = styleModifier {
+    translate(tx, ty, tz)
+}
+
+fun Modifier.translateX(tx: CSSLengthOrPercentageValue) = styleModifier {
+    translateX(tx)
+}
+
+fun Modifier.translateY(ty: CSSLengthOrPercentageValue) = styleModifier {
+    translateY(ty)
+}
+
+fun Modifier.translateZ(tz: CSSLengthOrPercentageValue) = styleModifier {
+    translateZ(tz)
+}
---
+++ frontend/web-compose-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Transform.kt
@@ -0,0 +1,107 @@
+package com.varabyte.kobweb.compose.css
+
+import org.jetbrains.compose.web.css.*
+
+// region Rotate
+
+fun StyleScope.rotate(a: CSSAngleValue) {
+    property("rotate", a)
+}
+
+fun StyleScope.rotate(x: Number, y: Number, z: Number, a: CSSAngleValue) {
+    property("rotate", "$x $y $z $a")
+}
+
+fun StyleScope.rotateX(a: CSSAngleValue) {
+    property("rotate", "x $a")
+}
+
+fun StyleScope.rotateY(a: CSSAngleValue) {
+    property("rotate", "y $a")
+}
+
+fun StyleScope.rotateZ(a: CSSAngleValue) {
+    property("rotate", "z $a")
+}
+
+// endregion
+
+// region Scale
+
+fun StyleScope.scale(s: Number) {
+    property("scale", s)
+}
+
+fun StyleScope.scale(sx: Number, sy: Number) {
+    property("scale", "$sx $sy")
+}
+
+fun StyleScope.scale(sx: Number, sy: Number, sz: Number) {
+    property("scale", "$sx $sy $sz")
+}
+
+fun StyleScope.scaleX(s: Number) {
+    scale(s, 1)
+}
+
+fun StyleScope.scaleY(s: Number) {
+    scale(1, s)
+}
+
+fun StyleScope.scaleZ(s: Number) {
+    scale(1, 1, s)
+}
+
+fun StyleScope.scale(s: CSSPercentageValue) {
+    property("scale", s)
+}
+
+fun StyleScope.scale(sx: CSSPercentageValue, sy: CSSPercentageValue) {
+    property("scale", "$sx $sy")
+}
+
+fun StyleScope.scale(sx: CSSPercentageValue, sy: CSSPercentageValue, sz: CSSPercentageValue) {
+    property("scale", "$sx $sy $sz")
+}
+
+fun StyleScope.scaleX(s: CSSPercentageValue) {
+    scale(s, 100.percent)
+}
+
+fun StyleScope.scaleY(s: CSSPercentageValue) {
+    scale(100.percent, s)
+}
+
+fun StyleScope.scaleZ(s: CSSPercentageValue) {
+    scale(100.percent, 100.percent, s)
+}
+
+// endregion
+
+// region Translate
+
+fun StyleScope.translate(tx: CSSLengthOrPercentageValue) {
+    property("translate", tx)
+}
+
+fun StyleScope.translate(tx: CSSLengthOrPercentageValue, ty: CSSLengthOrPercentageValue) {
+    property("translate", "$tx $ty")
+}
+
+fun StyleScope.translate(tx: CSSLengthOrPercentageValue, ty: CSSLengthOrPercentageValue, tz: CSSLengthOrPercentageValue) {
+    property("translate", "$tx $ty $tz")
+}
+
+fun StyleScope.translateX(tx: CSSLengthOrPercentageValue) {
+    translate(tx)
+}
+
+fun StyleScope.translateY(ty: CSSLengthOrPercentageValue) {
+    translate(0.percent, ty)
+}
+
+fun StyleScope.translateZ(tz: CSSLengthOrPercentageValue) {
+    translate(0.percent, 0.percent, tz)
+}
+
+// endregion
