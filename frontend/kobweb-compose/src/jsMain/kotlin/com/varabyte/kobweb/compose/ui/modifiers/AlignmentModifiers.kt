59add217e24ec53eac4ed1abe0a86a5cdeb1a48c
David Herman <bitspittle@gmail.com>
2022-11-03 08:38:21 -0700

    Add (deprecated) support for JB justifycontent/aligncontent etc. modifiers
    Users should prefer the Kobweb versions (because the Compose for Web versions
    currently aren't totally accurate to the CSS API)

--- frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/AlignmentModifiers.kt
+++ frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/AlignmentModifiers.kt
@@ -3,6 +3,42 @@ package com.varabyte.kobweb.compose.ui.modifiers
 import com.varabyte.kobweb.compose.css.*
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.styleModifier
+import org.jetbrains.compose.web.css.AlignContent as JbAlignContent
+import org.jetbrains.compose.web.css.alignContent as jbAlignContent
+import org.jetbrains.compose.web.css.AlignItems as JbAlignItems
+import org.jetbrains.compose.web.css.alignItems as jbAlignItems
+import org.jetbrains.compose.web.css.AlignSelf as JbAlignSelf
+import org.jetbrains.compose.web.css.alignSelf as jbAlignSelf
+import org.jetbrains.compose.web.css.JustifyContent as JbJustifyContent
+import org.jetbrains.compose.web.css.justifyContent as jbJustifyContent
+
+// region Legacy JB modifiers
+
+@Suppress("DeprecatedCallableAddReplaceWith") // ReplaceWith too complicated
+@Deprecated("Prefer using Kobweb's more fully featured com.varabyte.kobweb.compose.css.AlignContent class to the one provided by Compose for Web")
+fun Modifier.alignContent(alignContent: JbAlignContent) = styleModifier {
+    jbAlignContent(alignContent)
+}
+
+@Suppress("DeprecatedCallableAddReplaceWith") // ReplaceWith too complicated
+@Deprecated("Prefer using Kobweb's more fully featured com.varabyte.kobweb.compose.css.AlignItems class to the one provided by Compose for Web")
+fun Modifier.alignItems(alignItems: JbAlignItems) = styleModifier {
+    jbAlignItems(alignItems)
+}
+
+@Suppress("DeprecatedCallableAddReplaceWith") // ReplaceWith too complicated
+@Deprecated("Prefer using Kobweb's more fully featured com.varabyte.kobweb.compose.css.AlignSelf class to the one provided by Compose for Web")
+fun Modifier.alignSelf(alignSelf: JbAlignSelf) = styleModifier {
+    jbAlignSelf(alignSelf)
+}
+
+@Suppress("DeprecatedCallableAddReplaceWith") // ReplaceWith too complicated
+@Deprecated("Prefer using Kobweb's more fully featured com.varabyte.kobweb.compose.css.JustifyContent class to the one provided by Compose for Web")
+fun Modifier.justifyContent(justifyContent: JbJustifyContent) = styleModifier {
+    jbJustifyContent(justifyContent)
+}
+
+// endregion
 
 fun Modifier.alignContent(alignContent: AlignContent) = styleModifier {
     alignContent(alignContent)
