445fa0d4e657d69f8f039acbaace1dc428710389
David Herman <bitspittle@gmail.com>
2022-11-04 16:10:26 -0700

    Rename "Link" in kobweb-core to "Anchor"
    This avoids the common confusion caused by core's "Link" class and
    Silk's "Link" class. In most cases, users will want to use the
    Silk version.
    
    I would have called this class "A", like the Compose for Web version,
    but then we'd just be moving the namespace collision from Kobweb code
    into Compose for Web code. So "Anchor" seems like a good compromise
    for now.

--- frontend/kobweb-core/src/jsMain/kotlin/com/varabyte/kobweb/navigation/Anchor.kt
+++ frontend/kobweb-core/src/jsMain/kotlin/com/varabyte/kobweb/navigation/Anchor.kt
@@ -8,6 +8,21 @@ import org.jetbrains.compose.web.dom.AttrBuilderContext
 import org.jetbrains.compose.web.dom.ContentBuilder
 import org.w3c.dom.HTMLAnchorElement
 
+@Deprecated("Kobweb core's Link class has been renamed to Anchor",
+    ReplaceWith("Anchor(href, attrs, openInternalLinksStrategy, openExternalLinksStrategy, autoPrefix, content)")
+)
+@Composable
+fun Link(
+    href: String,
+    attrs: AttrBuilderContext<HTMLAnchorElement>? = null,
+    openInternalLinksStrategy: OpenLinkStrategy? = null,
+    openExternalLinksStrategy: OpenLinkStrategy? = null,
+    autoPrefix: Boolean = true,
+    content: ContentBuilder<HTMLAnchorElement>? = null
+) {
+    Anchor(href, attrs, openInternalLinksStrategy, openExternalLinksStrategy, autoPrefix, content)
+}
+
 /**
  * A special version of the A tag which, when clicked on, will not reload the current page if it's within the
  * same site.
@@ -23,12 +38,14 @@ import org.w3c.dom.HTMLAnchorElement
  *   domain). If not set, this behavior will be determined depending on what control keys are being pressed. See
  *   [SyntheticMouseEvent.toOpenLinkStrategy] for more information.
  *
- * @param autoPrefix If true AND if a route prefix is configured for this site, auto-affix it to the front. You usually
- *   want this to be true, unless you are intentionally linking outside this site's root folder while still staying in
- *   the same domain.
+ * @param autoPrefix If true AND if a route prefix is configured for this site, auto-affix it to the front.For example,
+ *   if the [href] parameter was set to "example/path" and the site's route prefix was set to "parent-site/nested", then
+ *   the `href` value will be converted to "parent-site/nested/example/path". You usually want this to be true, unless
+ *   you are intentionally linking outside this site's root folder while still staying in the same domain, e.g. you are
+ *   linking to "parent-site/about".
  */
 @Composable
-fun Link(
+fun Anchor(
     href: String,
     attrs: AttrBuilderContext<HTMLAnchorElement>? = null,
     openInternalLinksStrategy: OpenLinkStrategy? = null,
--- frontend/kobweb-silk/src/jsMain/kotlin/com/varabyte/kobweb/silk/components/navigation/Link.kt
+++ frontend/kobweb-silk/src/jsMain/kotlin/com/varabyte/kobweb/silk/components/navigation/Link.kt
@@ -6,13 +6,13 @@ import com.varabyte.kobweb.compose.dom.ElementRefScope
 import com.varabyte.kobweb.compose.dom.registerRefScope
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.asAttributesBuilder
+import com.varabyte.kobweb.navigation.Anchor
 import com.varabyte.kobweb.navigation.OpenLinkStrategy
 import com.varabyte.kobweb.navigation.toOpenLinkStrategy
 import com.varabyte.kobweb.silk.components.style.ComponentVariant
 import com.varabyte.kobweb.silk.components.style.toModifier
 import org.jetbrains.compose.web.dom.Text
 import org.w3c.dom.HTMLElement
-import com.varabyte.kobweb.navigation.Link as KobwebLink
 
 /**
  * Linkable text which, when clicked, navigates to the target [path].
@@ -22,11 +22,11 @@ import com.varabyte.kobweb.navigation.Link as KobwebLink
  *
  * @param openInternalLinksStrategy If set, force the behavior of how internal links (links under the site's root) open.
  *   If not set, this behavior will be determined depending on what control keys are being pressed. See
- *   [SyntheticMouseEvent.toOpenLinkStrategy] for more information.
+ *   [SyntheticMouseEvent.toOpenLinkStrategy] to see which key modifier combinations result in which behavior.
  *
  * @param openExternalLinksStrategy If set, force the behavior of how external links open (links outside this site's
  *   domain). If not set, this behavior will be determined depending on what control keys are being pressed. See
- *   [SyntheticMouseEvent.toOpenLinkStrategy] for more information.
+ *   [SyntheticMouseEvent.toOpenLinkStrategy] to see which key modifier combinations result in which behavior.
  *
  * @param autoPrefix If true AND if a route prefix is configured for this site, auto-affix it to the front. You usually
  *   want this to be true, unless you are intentionally linking outside this site's root folder while still staying in
@@ -64,7 +64,7 @@ fun Link(
     ref: ElementRefScope<HTMLElement>? = null,
     content: @Composable () -> Unit = {}
 ) {
-    KobwebLink(
+    Anchor(
         href = path,
         attrs = LinkStyle.toModifier(variant).then(modifier).asAttributesBuilder(),
         openInternalLinksStrategy,
