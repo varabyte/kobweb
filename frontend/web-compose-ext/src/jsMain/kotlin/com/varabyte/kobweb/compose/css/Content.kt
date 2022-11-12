4c94a27083fc7cd662c999b23c17d48a38f25b2f
David Herman <bitspittle@gmail.com>
2022-11-11 16:34:57 -0800

    Auto-quote Modifier.content

--- frontend/web-compose-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Content.kt
+++ frontend/web-compose-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Content.kt
@@ -3,5 +3,13 @@ package com.varabyte.kobweb.compose.css
 import org.jetbrains.compose.web.css.*
 
 fun StyleScope.content(value: String) {
-    property("content", value)
+    // CSS content should always be surrounded by quotes, but this is a pretty subtle requirement that's easy to miss
+    // and causes silent failures. The person is passing in a String so their intention is clear. Let's just quote it
+    // for them if they don't have it!
+    val content = if (value.length >= 2 && value.first() == '"' && value.last() == '"') {
+        value
+    } else {
+        '"' + value.replace("\"", "\\\"") + '"'
+    }
+    property("content", content)
 }
