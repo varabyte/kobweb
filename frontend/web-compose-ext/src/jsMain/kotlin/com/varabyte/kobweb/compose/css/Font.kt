9763ab984251b321524922ebbffd95666249ab48
David Herman <bitspittle@gmail.com>
2022-11-17 15:02:10 -0800

    Fix Font issues
    * Eliminated / deprecated factory methods
    * Construction by String is no longer public
    * Added category comments and more keywords
    * Added oblique + angle support for font style

--- frontend/web-compose-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Font.kt
+++ frontend/web-compose-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Font.kt
@@ -6,35 +6,57 @@ fun StyleScope.fontFamily(value: String) {
     property("font-family", value)
 }
 
-class FontStyle(val value: String) {
+class FontStyle private constructor(val value: String) {
     companion object {
+        // Keyword
         val Normal get() = FontStyle("normal")
         val Italic get() = FontStyle("italic")
-    }
+        val Oblique get() = FontStyle("oblique")
+
+        fun Oblique(angle: CSSAngleValue) = FontStyle("oblique $angle")
+
+        // Global
+        val Inherit get() = FontStyle("inherit")
+        val Initial get() = FontStyle("initial")
+        val Revert get() = FontStyle("revert")
+        val RevertLayer get() = FontStyle("revert-layer")
+        val Unset get() = FontStyle("unset")    }
 }
 
 fun StyleScope.fontStyle(style: FontStyle) {
     property("font-style", style.value)
 }
 
-sealed interface FontWeight {
+class FontWeight private constructor(val value: StylePropertyValue) {
+    private constructor(value: String) : this(StylePropertyValue(value))
+    constructor(value: Int) : this(value.let {
+        require(it in 1..1000) { "Font weight must be between 1 and 1000. Got: $it" }
+        StylePropertyValue(it)
+    })
+
     companion object {
-        val Normal get() = StringFontWeight("normal")
-        val Bold get() = StringFontWeight("bold")
-        val Lighter get() = StringFontWeight("lighter")
-        val Bolder get() = StringFontWeight("bolder")
+        // Keyword
+        val Normal get() = FontWeight("normal")
+        val Bold get() = FontWeight("bold")
+
+        // Relative
+        val Lighter get() = FontWeight("lighter")
+        val Bolder get() = FontWeight("bolder")
+
+        // Global
+        val Inherit get() = FontWeight("inherit")
+        val Initial get() = FontWeight("initial")
+        val Revert get() = FontWeight("revert")
+        val RevertLayer get() = FontWeight("revert-layer")
+        val Unset get() = FontWeight("unset")
     }
 }
 
-class StringFontWeight(val value: String) : FontWeight
-class IntFontWeight(val value: Int) : FontWeight
+@Deprecated("This factory method is no longer required. Construct a FontWeight directly instead.",
+    ReplaceWith("FontWeight(value)", "com.varabyte.kobweb.compose.css.FontWeight"),
+)
+fun IntFontWeight(value: Int) = FontWeight(value)
 
 fun StyleScope.fontWeight(weight: FontWeight) {
-    when (weight) {
-        is StringFontWeight -> property("font-weight", weight.value)
-        is IntFontWeight -> {
-            require(weight.value in 1..1000) { "Font weight must be between 1 and 1000. Got: ${weight.value}" }
-            property("font-weight", weight.value)
-        }
-    }
+    property("font-weight", weight.value)
 }
