71785a89000e5770286c841206a88624fd8e36b3
David Herman <bitspittle@gmail.com>
2022-12-07 17:54:48 -0800

    Update plugins block to use alias instead of id syntax

--- frontend/kobweb-silk-icons-fa/build.gradle.kts
+++ frontend/kobweb-silk-icons-fa/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform")
-    id("org.jetbrains.compose")
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.jetbrains.compose)
     id("com.varabyte.kobweb.internal.publish")
 }
 
