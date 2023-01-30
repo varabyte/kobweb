7e7665917838a447a402786c8c14191cda43997e
David Herman <bitspittle@gmail.com>
2023-01-30 09:25:36 -0800

    Fix server routes being broken on Windows

--- gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/tasks/KobwebStartTask.kt
+++ gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/tasks/KobwebStartTask.kt
@@ -78,10 +78,11 @@ abstract class KobwebStartTask @Inject constructor(
 
         val process = Runtime.getRuntime().exec(
             processParams,
-            // Set envp to empty to indicate that the new process should get its own fresh environment
-            // Note: It's possible it could be better to pass in null here, and let the process inherit Gradle's
-            // environment. Happy to revisit this later.
-            emptyArray(),
+            // Note: We intentionally set envp null here, to inherit our environment. One of the
+            // things that gets inherited is the tmp file location, which seems to be particularly
+            // important on Windows, as it will otherwise try to create temp files in folders that
+            // we don't have permissions to write to... (See #208)
+            null,
             kobwebApplication.path.toFile()
         )
 
