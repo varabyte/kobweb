a364f39d85f45357ffa041e66073ff272b7e6f31
David Herman <bitspittle@gmail.com>
2023-02-09 09:46:39 -0800

    Add modifiers for animation events

--- frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/AnimationModifiers.kt
+++ frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/AnimationModifiers.kt
@@ -2,8 +2,22 @@ package com.varabyte.kobweb.compose.ui.modifiers
 
 import com.varabyte.kobweb.compose.css.*
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.attrsModifier
 import com.varabyte.kobweb.compose.ui.styleModifier
+import org.jetbrains.compose.web.events.SyntheticAnimationEvent
 
 fun Modifier.animation(vararg animations: CSSAnimation) = styleModifier {
     animation(*animations)
+}
+
+fun Modifier.onAnimationEnd(onAnimationEnd: (SyntheticAnimationEvent) -> Unit): Modifier = attrsModifier {
+    onAnimationEnd(onAnimationEnd)
+}
+
+fun Modifier.onAnimationIteration(onAnimationIteration: (SyntheticAnimationEvent) -> Unit): Modifier = attrsModifier {
+    onAnimationIteration(onAnimationIteration)
+}
+
+fun Modifier.onAnimationStart(onAnimationStart: (SyntheticAnimationEvent) -> Unit): Modifier = attrsModifier {
+    onAnimationStart(onAnimationStart)
 }
