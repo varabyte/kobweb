b36eddc9e6cc255d9a42a3b1bac3952aa15f3b68
David Herman <bitspittle@gmail.com>
2023-03-28 00:01:44 -0700

    Use the shadow jar to relocate our dependencies

--- backend/server/build.gradle.kts
+++ backend/server/build.gradle.kts
@@ -40,4 +40,25 @@ tasks.withType<ShadowJar> {
         // Logger classes are accessed at runtime
         exclude(dependency("ch.qos.logback:.*:.*"))
     }
+
+    // Move as much as we can out of the way, to avoid the change that we have our own dependency that conflicts with
+    // any code registered by the user in the uber jar they provide for handling server API routes.
+    // See also: build/libs, unzip server-0.12.4-SNAPSHOT-all after building `:shadowJar`
+    // Note: This workaround is (probably) necessary because Java classloading has disappointing limitations when it
+    // comes to isolating execution environments. In other words, we want to tell the JVM "this user's uber jar? It
+    // should get its own environment, except for a few interfaces it should get from us."
+    // Ideally, I'd have a common classloader, the user's classloader, and our server's classloader, where kobweb-api
+    // would live in the common classloader, and otherwise the user's classloader and our server's classloader would be
+    // totally separate. After a day fighting code, I couldn't figure out how to get this to work.
+    relocate("com", "relocated.com") {
+        // Leave varabyte code in place. Anything the user is using in their code should be
+        // provided by the server
+        exclude("com.varabyte.**")
+    }
+    relocate("org", "relocated.org") {
+        // Need to exclude this or else we get an exception at runtime
+        exclude("org.xml.sax.**")
+    }
+    relocate("io", "relocated.io")
+    relocate("kotlinx", "relocated.kotlinx")
 }
