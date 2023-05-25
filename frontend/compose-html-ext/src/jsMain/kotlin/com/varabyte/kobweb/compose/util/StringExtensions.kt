43f4ff7660bbf3f0d4d315faa27eb9f10bdf78fb
David Herman <bitspittle@gmail.com>
2023-05-25 11:37:25 -0700

    Add header comments for public methods

--- frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/util/StringExtensions.kt
+++ frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/util/StringExtensions.kt
@@ -1,6 +1,10 @@
 package com.varabyte.kobweb.compose.util
 
-// e.g. "ExampleText" to "example-text"
+/**
+ * Convert a String for a name that is using TitleCamelCase and convert it to kebab case.
+ *
+ * For example, "ExampleText" to "example-text"
+ */
 fun String.titleCamelCaseToKebabCase(): String {
     require(this.isNotBlank())
 
@@ -21,9 +25,20 @@ fun String.titleCamelCaseToKebabCase(): String {
     return words.joinToString("-") { it.decapitalize() }
 }
 
-// CSS text content should always be surrounded by quotes, but this is a pretty subtle requirement that's easy to miss
-// and causes silent failures. The person is passing in a String so their intention is clear. Let's just quote it
-// for them if they don't have it!
+/**
+ * Quote a CSS string (unless it is already quoted).
+ *
+ * CSS text content should always be surrounded by quotes, but this is a pretty subtle requirement that's easy to miss
+ * and causes silent failures.
+ *
+ * This method will investigate a string that is intended to be used as CSS text content and wrap it with quotes (unless
+ * it is already properly wrapped.
+ *
+ * For example:
+ * * `test` -> `"test"`
+ * * `"test"` -> `"test"` (unchanged)
+ * * `"Is the test today?" he asked` -> `"\"Is the test today?\" he asked"`
+ */
 fun String.wrapQuotesIfNecessary() = if (this.length >= 2 && this.first() == '"' && this.last() == '"') {
     this
 } else {
