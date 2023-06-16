4d7efffd62e4aefeb2fa1f05d4bda734ddb5925a
David Herman <bitspittle@gmail.com>
2023-06-16 11:30:59 -0700

    Rename "Modifier.displayIf" to "displayItAtLeast"
    The old name was a bit confusing, so the new name, despite being
    a little longer, should convey its behavior a lot clearer.

--- frontend/kobweb-silk-widgets/src/jsMain/kotlin/com/varabyte/kobweb/silk/components/layout/breakpoint/BreakpointConditions.kt
+++ frontend/kobweb-silk-widgets/src/jsMain/kotlin/com/varabyte/kobweb/silk/components/layout/breakpoint/BreakpointConditions.kt
@@ -17,20 +17,30 @@ private fun CSSMediaQuery.invert(): CSSMediaQuery {
     return CSSMediaQuery.Raw("not all and $this")
 }
 
-internal val DisplayIfSmStyle by ComponentStyle(prefix = "silk") {
+// Technically unnecessary -- this just means always display. But provided for completion for all breakpoint values.
+internal val DisplayIfAtLeastZeroStyle by ComponentStyle(prefix = "silk") {
+    cssRule(Breakpoint.ZERO.toMinWidthQuery().invert()) { Modifier.display(DisplayStyle.None) }
+}
+
+internal val DisplayIfAtLeastSmStyle by ComponentStyle(prefix = "silk") {
     cssRule(Breakpoint.SM.toMinWidthQuery().invert()) { Modifier.display(DisplayStyle.None) }
 }
 
-internal val DisplayIfMdStyle by ComponentStyle(prefix = "silk") {
+internal val DisplayIfAtLeastMdStyle by ComponentStyle(prefix = "silk") {
     cssRule(Breakpoint.MD.toMinWidthQuery().invert()) { Modifier.display(DisplayStyle.None) }
 }
 
-internal val DisplayIfLgStyle by ComponentStyle(prefix = "silk") {
+internal val DisplayIfAtLeastLgStyle by ComponentStyle(prefix = "silk") {
     cssRule(Breakpoint.LG.toMinWidthQuery().invert()) { Modifier.display(DisplayStyle.None) }
 }
 
-internal val DisplayIfXlStyle by ComponentStyle(prefix = "silk") {
+internal val DisplayIfAtLeastXlStyle by ComponentStyle(prefix = "silk") {
     cssRule(Breakpoint.XL.toMinWidthQuery().invert()) { Modifier.display(DisplayStyle.None) }
+}
+
+// Technically unnecessary -- this just means never display. But provided for completion for all breakpoint values.
+internal val DisplayUntilZeroStyle by ComponentStyle(prefix = "silk") {
+    Breakpoint.ZERO { Modifier.display(DisplayStyle.None) }
 }
 
 internal val DisplayUntilSmStyle by ComponentStyle(prefix = "silk") {
@@ -49,10 +59,12 @@ internal val DisplayUntilXlStyle by ComponentStyle(prefix = "silk") {
     Breakpoint.XL { Modifier.display(DisplayStyle.None) }
 }
 
-fun Modifier.displayIf(breakpoint: Breakpoint) = this.classNames("silk-display-if-${breakpoint.name.lowercase()}")
+@Deprecated("Method was renamed to `displayIfAtLeast` for clarity.", ReplaceWith("displayIfAtLeast(breakpoint)"))
+fun Modifier.displayIf(breakpoint: Breakpoint) = displayIfAtLeast(breakpoint)
+fun Modifier.displayIfAtLeast(breakpoint: Breakpoint) = this.classNames("silk-display-if-at-least-${breakpoint.name.lowercase()}")
 fun Modifier.displayUntil(breakpoint: Breakpoint) = this.classNames("silk-display-until-${breakpoint.name.lowercase()}")
 fun Modifier.displayBetween(breakpointLower: Breakpoint, breakpointUpper: Breakpoint): Modifier {
-    require(breakpointLower.ordinal < breakpointUpper.ordinal) { "showBetween breakpoints passed in wrong order: $breakpointLower should be smaller than $breakpointUpper" }
+    require(breakpointLower.ordinal < breakpointUpper.ordinal) { "displayBetween breakpoints passed in wrong order: $breakpointLower should be smaller than $breakpointUpper" }
 
     return this.classNames("silk-display-if-${breakpointLower.name.lowercase()}", "silk-display-until-${breakpointUpper.name.lowercase()}")
 }
