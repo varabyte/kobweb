3261c98dfabc889080e541428cc1b879ee355a8e
David Herman <bitspittle@gmail.com>
2023-06-19 15:52:33 -0700

    Add the "numericAuto" property
    An "auto" value that can be passed into any modifier that accepts
    a numeric paramter.

---
+++ frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Auto.kt
@@ -0,0 +1,21 @@
+package com.varabyte.kobweb.compose.css
+
+import org.jetbrains.compose.web.css.CSSNumeric
+import org.jetbrains.compose.web.css.CSSNumericValue
+import org.jetbrains.compose.web.css.CSSUnit
+
+/**
+ * A CSS value that represents the "auto" keyword and can be used anywhere that accepts a numeric argument.
+ *
+ * For example:
+ *
+ * ```
+ * Modifier.margin(top = numericAuto, bottom = 1.cssRem)
+ * ```
+
+ * Note that this can fail silently if you try to use it in a context that doesn't support it.
+ */
+inline val numericAuto: CSSNumeric
+    get() = object : CSSNumericValue<CSSUnit> {
+        override fun toString(): String = "auto"
+    }
