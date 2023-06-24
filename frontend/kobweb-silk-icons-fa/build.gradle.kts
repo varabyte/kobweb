623447f56bc8b3fd34a67ec8bd246ff48087fe3e
David Herman <bitspittle@gmail.com>
2023-06-24 10:06:53 -0700

    Update Kobweb project to force use lf (unix-style) endings
    ... and update Gradle code used by the icons codegen algorithms
    to match.

--- frontend/kobweb-silk-icons-fa/build.gradle.kts
+++ frontend/kobweb-silk-icons-fa/build.gradle.kts
@@ -189,7 +189,7 @@ fun FaIcon(
 ${iconMethodEntries.joinToString("\n")}
     """.trimIndent()
 
-    println(dstFile.asFile.writeText(iconsCode.replace("\n", System.lineSeparator())))
+    println(dstFile.asFile.writeText(iconsCode))
 }
 
 tasks.named("compileKotlinJs") {
--- frontend/kobweb-silk-icons-mdi/build.gradle.kts
+++ frontend/kobweb-silk-icons-mdi/build.gradle.kts
@@ -172,7 +172,7 @@ fun MdIcon(
 ${iconMethodEntries.joinToString("\n")}
     """.trimIndent()
 
-    println(dstFile.asFile.writeText(iconsCode.replace("\n", System.lineSeparator())))
+    println(dstFile.asFile.writeText(iconsCode))
 }
 
 tasks.named("compileKotlinJs") {
