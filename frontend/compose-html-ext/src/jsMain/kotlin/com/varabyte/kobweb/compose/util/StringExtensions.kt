5a443803167c71d8adccd6276af02132d4734a54
David Herman <bitspittle@gmail.com>
2023-06-28 15:41:54 -0700

    Tweak `titleCamelCaseToKebabCase` logic to handle acronyms
    So "ABCExample" now generates "abc-example", not "a-b-c-example"

--- frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/util/StringExtensions.kt
+++ frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/util/StringExtensions.kt
@@ -4,27 +4,47 @@ package com.varabyte.kobweb.compose.util
  * Convert a String for a name that is using TitleCamelCase into kebab-case.
  *
  * For example, "ExampleText" to "example-text"
+ *
+ * Note that there's special handling for acronyms, so "ExampleABC" will be converted to "example-abc", and "ABCExample"
+ * will be converted to "abc-example" (not "example-a-b-c" and "a-b-c-example").
  */
 fun String.titleCamelCaseToKebabCase(): String {
     require(this.isNotBlank())
 
-    val currentWord = StringBuilder()
-    val words = mutableListOf<String>()
+    val result = StringBuilder()
+    var lastIsUpper = false // Used to distinguish "E" as the place to break new words in "ABCExample"
 
-    this.forEach { c ->
-        if (c.isUpperCase()) {
-            if (currentWord.isNotEmpty()) {
-                words.add(currentWord.toString())
-                currentWord.clear()
+    this.forEachIndexed { i, c ->
+        val isUpper = c.isUpperCase()
+        if (isUpper) {
+            // Break new words when either:
+            // - right before the last capital followed by a lowercase (e.g. "E" in "ABCExample")
+            // - right before the first capital following a lowercase (e.g. "A" in "ExampleABC")
+            if (result.isNotEmpty() && (!lastIsUpper || (i < this.lastIndex && this[i + 1].isLowerCase()))) {
+                result.append("-")
             }
         }
-        currentWord.append(c)
+        result.append(c.lowercase())
+        lastIsUpper = isUpper
     }
-    words.add(currentWord.toString())
 
-    // The suggested replacement for "decapitalize" is awful
-    @Suppress("DEPRECATION") return words.joinToString("-") { it.decapitalize() }
+    return result.toString()
 }
+
+/**
+ * Convert a String for a name that is using kebab-case into TitleCamelCase.
+ *
+ * For example, "example-text" to "ExampleText"
+ *
+ * This is often but NOT ALWAYS the inverse of [titleCamelCaseToKebabCase], if there were acronyms in the original
+ * text. For example, "ABCExample" will get converted to "abc-example", which, when inversed, will become "AbcExample".
+ */
+fun String.kebabCaseToTitleCamelCase(): String {
+    require(this.isNotBlank())
+    // The suggested replacement for "capitalize" is awful
+    @Suppress("DEPRECATION") return this.split("-").joinToString("") { it.capitalize() }
+}
+
 
 /**
  * Quote a CSS string (unless it is already quoted).
