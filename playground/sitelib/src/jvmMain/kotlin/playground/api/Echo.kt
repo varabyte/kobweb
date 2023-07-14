0604c653fe1edbecc7698d6be9f25e3021073ff2
David Herman <bitspittle@gmail.com>
2023-07-13 17:14:46 -0700

    Nit: Clean up stale references in api/Echo.kt

--- playground/sitelib/src/jvmMain/kotlin/playground/api/Echo.kt
+++ playground/sitelib/src/jvmMain/kotlin/playground/api/Echo.kt
@@ -2,13 +2,13 @@ package playground.api
 
 import com.varabyte.kobweb.api.stream.ApiStream
 
-val echo = ApiStream { evt ->
-    evt.stream.send(evt.text)
+val echo = ApiStream { ctx ->
+    ctx.stream.send(ctx.text)
 }
 
-// Or, if you need access to connect / disconnect messages
+// Or, if you need access to connect / disconnect messages:
 // val echo = object : ApiStream() {
-//    override suspend fun onUserConnected(ctx: UserConnectedContext) { ... }
-//    override suspend fun onMessageReceived(ctx: MessageReceivedContext) { ... }
-//    override suspend fun onUserDisconnected(ctx: UserDisconnectedContext) { ... }
+//    override suspend fun onUserConnected(ctx: UserConnectedContext) {}
+//    override suspend fun onTextReceived(ctx: TextReceivedContext) {}
+//    override suspend fun onUserDisconnected(ctx: UserDisconnectedContext) {}
 //}
