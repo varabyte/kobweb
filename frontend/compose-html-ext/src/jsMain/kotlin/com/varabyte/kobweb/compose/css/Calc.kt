006c3a352b15f8e41d5b0791c32abb6119b2b9d3
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-08-17 16:05:23 -0400

    Add examples to `calc` documentation

--- frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Calc.kt
+++ frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Calc.kt
@@ -6,12 +6,29 @@ import kotlin.experimental.ExperimentalTypeInference
 // While operations with CSS numerics are already supported, the existing implementation does not properly handle using
 // StyleVariables (CSS Variables) in calculations. So, we implement explicit calc() functions for handling these cases.
 
-/** Expresses a CSS calculation for a [CSSNumericValue], particularly useful when it involves a [StyleVariable]. */
+/**
+ * Expresses a CSS calculation for a [CSSNumericValue], particularly useful when it involves a [StyleVariable].
+ *
+ * For example:
+ * ```
+ * val PaddingVar by StyleVariable(1.cssRem)
+ * Modifier.padding(calc { (BasePaddingVar.value() + 2.px) * 2 })
+ * ```
+ */
 @OptIn(ExperimentalTypeInference::class)
 @OverloadResolutionByLambdaReturnType
 fun <T : CSSNumericValue<*>> calc(action: CalcScope.() -> T): T = with(CalcScopeInstance, action)
 
-/** Expresses a CSS calculation for a [Number], particularly useful when it involves a [StyleVariable]. */
+/**
+ * Expresses a CSS calculation for a [Number], particularly useful when it involves a [StyleVariable]. To be used in
+ * operations, numbers (including those derived from StyleVariables) must be wrapped in a call to [CalcScope.num].
+ *
+ * For example:
+ * ```
+ * val BaseLineHeightVar by StyleVariable(1.5)
+ * Modifier.lineHeight(calc { num(2) * num(BaseLineHeightVar.value()) })
+ * ```
+ */
 fun <V : Number, T : CalcScope.CalcNum<V>> calc(action: CalcScope.() -> T): V =
     with(CalcScopeInstance, action).unsafeCast<V>()
 
