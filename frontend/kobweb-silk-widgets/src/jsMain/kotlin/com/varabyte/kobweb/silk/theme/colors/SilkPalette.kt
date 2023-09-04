f2c28101a6710dfe2b1292612ffd6750725ceafd
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-09-04 16:10:23 -0400

    Nit: add "silk" prefix

--- frontend/kobweb-silk-widgets/src/jsMain/kotlin/com/varabyte/kobweb/silk/theme/colors/SilkPalette.kt
+++ frontend/kobweb-silk-widgets/src/jsMain/kotlin/com/varabyte/kobweb/silk/theme/colors/SilkPalette.kt
@@ -13,7 +13,7 @@ val BackgroundColorVar by StyleVariable<CSSColorValue>(prefix = "silk")
 val ColorVar by StyleVariable<CSSColorValue>(prefix = "silk")
 val BorderColorVar by StyleVariable<CSSColorValue>(prefix = "silk")
 val FocusOutlineColorVar by StyleVariable<CSSColorValue>(prefix = "silk")
-val PlaceholderOpacityVar by StyleVariable<Number>(1.0)
+val PlaceholderOpacityVar by StyleVariable<Number>(prefix = "silk", defaultFallback = 1.0)
 val PlaceholderColorVar by StyleVariable<CSSColorValue>(prefix = "silk")
 
 /**
