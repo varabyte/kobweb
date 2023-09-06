c65075cb0bc31cd1a03469969ddd059dd26865ed
David Herman <bitspittle@gmail.com>
2023-09-06 11:33:54 -0700

    Nit: Update playground to use Silk Input

--- playground/site/src/jsMain/kotlin/playground/pages/Index.kt
+++ playground/site/src/jsMain/kotlin/playground/pages/Index.kt
@@ -2,10 +2,12 @@ package playground.pages
 
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.forms.TextInput
 import org.jetbrains.compose.web.attributes.InputType
 import org.jetbrains.compose.web.dom.Input
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.dom.TextInput
 import playground.components.layouts.PageLayout
 
 @Page
@@ -14,12 +16,7 @@ fun HomePage() {
     PageLayout("Welcome to Kobweb!") {
         Text("Please enter your name")
         var name by remember { mutableStateOf("") }
-        Input(
-            InputType.Text,
-            attrs = {
-                onInput { e -> name = e.value }
-            }
-        )
+        TextInput(name, onTextChanged = { name = it })
         P()
         Text("Hello ${name.takeIf { it.isNotBlank() } ?: "World"}!")
     }
