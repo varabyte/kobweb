1fb626da1ed6ee018cbcae88dc1f7d73d7a12595
David Herman <bitspittle@gmail.com>
2023-09-09 09:27:39 -0700

    Nit: Optimize imports in playground Index

--- playground/site/src/jsMain/kotlin/playground/pages/Index.kt
+++ playground/site/src/jsMain/kotlin/playground/pages/Index.kt
@@ -3,11 +3,8 @@ package playground.pages
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.silk.components.forms.TextInput
-import org.jetbrains.compose.web.attributes.InputType
-import org.jetbrains.compose.web.dom.Input
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
-import org.jetbrains.compose.web.dom.TextInput
 import playground.components.layouts.PageLayout
 
 @Page
