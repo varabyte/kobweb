ce8e7d43a00aa1e2e6a99c1cd7d4a823076ab249
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-09-10 11:19:38 -0400

    Stop KSP from processing code generated by kobweb
    Note that we currently do this by including said code in the gneeratedByKsp... source sets, but this should probably be our own source set instead?

--- tools/gradle-plugins/core/src/main/kotlin/com/varabyte/kobweb/gradle/core/KobwebCorePlugin.kt
+++ tools/gradle-plugins/core/src/main/kotlin/com/varabyte/kobweb/gradle/core/KobwebCorePlugin.kt
@@ -112,24 +112,28 @@ class KobwebCorePlugin : Plugin<Project> {
         }
 
         // Kobweb applications and libraries both put stuff in generated folders, which the Kotlin project should be
-        // aware of.
+        // aware of. Note that KSP processes all code sources in the main source set, so we don't include generated
+        // code here that shouldn't be processed (e.g. main.kt).
         val genDir = kobwebBlock.genDir.get()
+        val baseBuildDir = project.layout.buildDirectory
         project.buildTargets.withType<KotlinJsIrTarget>().configureEach {
             val jsTarget = JsTarget(this)
-
             project.kotlin.sourceSets.named(jsTarget.mainSourceSet) {
-                kotlin.srcDir(project.layout.buildDirectory.dir("$genDir${jsTarget.srcSuffix}"))
-                resources.srcDir(project.layout.buildDirectory.dir("generated/ksp/js/jsMain/resources")) // TODO
-                resources.srcDir(project.layout.buildDirectory.dir("$genDir${jsTarget.resourceSuffix}"))
+                resources.srcDirs(
+                    baseBuildDir.dir("generated/ksp/js/jsMain/resources"),
+                    baseBuildDir.dir("$genDir${jsTarget.resourceSuffix}"),
+                )
+                // TODO: "x" currently used for sources that should be parsed for KSP, standardize this
+                kotlin.srcDir(baseBuildDir.dir("${genDir}x${jsTarget.srcSuffix}"))
             }
         }
         project.buildTargets.withType<KotlinJvmTarget>().configureEach {
             val jvmTarget = JvmTarget(this)
-
             project.kotlin.sourceSets.named(jvmTarget.mainSourceSet) {
-                kotlin.srcDir(project.layout.buildDirectory.dir("$genDir${jvmTarget.srcSuffix}"))
-                resources.srcDir(project.layout.buildDirectory.dir("generated/ksp/jvm/jvmMain/resources")) // TODO
-                resources.srcDir(project.layout.buildDirectory.dir("$genDir${jvmTarget.resourceSuffix}"))
+                resources.srcDirs(
+                    baseBuildDir.dir("generated/ksp/jvm/jvmMain/resources"),
+                    baseBuildDir.dir("$genDir${jvmTarget.resourceSuffix}"),
+                )
             }
         }
     }
