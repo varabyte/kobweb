c931ded3e59a2dafefcb1f8f5758a0d01928cf26
David Herman <bitspittle@gmail.com>
2023-09-13 00:07:44 -0700

    Refactor icon build scripts to use generated output
    Previously, we would have a gradle task that created source code
    directly into the project source folder, which would then have to
     get checked in. Instead, now we generate code into a
    build/generated/ source folder and set that as a sourceset, which
    is probably a more idiomatic way to accomplish this task, as we
    now have a task outputting code into its own folder, instead of
    into an existing folder, which is generally discouraged by Gradle
    docs.

--- frontend/kobweb-silk-icons-fa/build.gradle.kts
+++ frontend/kobweb-silk-icons-fa/build.gradle.kts
@@ -10,6 +10,8 @@ plugins {
 group = "com.varabyte.kobweb"
 version = libs.versions.kobweb.libs.get()
 
+private val GENERATED_SRC_ROOT = "build/generated/kobweb-silk-icons-fa/src/jsMain/kotlin"
+
 kotlin {
     js {
         browser()
@@ -18,6 +20,7 @@ kotlin {
 
     sourceSets {
         val jsMain by getting {
+            kotlin.srcDir(GENERATED_SRC_ROOT)
             dependencies {
                 implementation(compose.runtime)
                 implementation(compose.html.core)
@@ -40,171 +43,165 @@ enum class IconCategory {
     BRAND,
 }
 
-val regenerateIconsTask = tasks.register("regenerateIcons") {
+val generateIconsTask = tasks.register("generateIcons") {
     val srcFile = layout.projectDirectory.file("fa-icon-list.txt")
     val dstFile =
-        layout.projectDirectory.file("src/jsMain/kotlin/com/varabyte/kobweb/silk/components/icons/fa/FaIcons.kt")
+        layout.projectDirectory.file("$GENERATED_SRC_ROOT/com/varabyte/kobweb/silk/components/icons/fa/FaIcons.kt")
 
-    inputs.files(srcFile, layout.projectDirectory.file("build.gradle.kts"))
+    inputs.files(srcFile)
     outputs.file(dstFile)
 
-    // {SOLID=[ad, address-book, address-card, ...], REGULAR=[address-book, address-card, angry, ...], ... }
-    val iconRawNames = srcFile.asFile
-        .readLines().asSequence()
-        .filter { line -> !line.startsWith("#") }
-        .map { line ->
-            // Convert icon name to function name, e.g.
-            // align-left -> FaAlignLeft
-            line.split("=", limit = 2).let { parts ->
-                val category = when (parts[0]) {
-                    "fas" -> IconCategory.SOLID
-                    "far" -> IconCategory.REGULAR
-                    "fab" -> IconCategory.BRAND
-                    else -> throw GradleException("Unexpected category string: ${parts[0]}")
-                }
-                val names = parts[1]
+    doLast {
+        // {SOLID=[ad, address-book, address-card, ...], REGULAR=[address-book, address-card, angry, ...], ... }
+        val iconRawNames = srcFile.asFile
+            .readLines().asSequence()
+            .filter { line -> !line.startsWith("#") }
+            .map { line ->
+                // Convert icon name to function name, e.g.
+                // align-left -> FaAlignLeft
+                line.split("=", limit = 2).let { parts ->
+                    val category = when (parts[0]) {
+                        "fas" -> IconCategory.SOLID
+                        "far" -> IconCategory.REGULAR
+                        "fab" -> IconCategory.BRAND
+                        else -> throw GradleException("Unexpected category string: ${parts[0]}")
+                    }
+                    val names = parts[1]
 
-                category to names.split(",")
+                    category to names.split(",")
+                }
+            }
+            .toMap()
+
+        // For each icon name, figure out what categories they are in. This will affect the function signature we generate.
+        // {ad=[SOLID], address-book=[SOLID, REGULAR], address-card=[SOLID, REGULAR], ...
+        val iconCategories = mutableMapOf<String, MutableSet<IconCategory>>()
+        iconRawNames.forEach { entry ->
+            val category = entry.key
+            entry.value.forEach { rawName ->
+                iconCategories.computeIfAbsent(rawName, { mutableSetOf() }).add(category)
             }
         }
-        .toMap()
 
-    // For each icon name, figure out what categories they are in. This will affect the function signature we generate.
-    // {ad=[SOLID], address-book=[SOLID, REGULAR], address-card=[SOLID, REGULAR], ...
-    val iconCategories = mutableMapOf<String, MutableSet<IconCategory>>()
-    iconRawNames.forEach { entry ->
-        val category = entry.key
-        entry.value.forEach { rawName ->
-            iconCategories.computeIfAbsent(rawName, { mutableSetOf() }).add(category)
-        }
+        // Sanity check results
+        iconCategories
+            .filterNot { entry ->
+                val categories = entry.value
+                categories.size == 1 ||
+                    (categories.size == 2 && categories.contains(IconCategory.SOLID) && categories.contains(IconCategory.REGULAR))
+            }
+            .let { invalidGroupings ->
+                if (invalidGroupings.isNotEmpty()) {
+                    throw GradleException("Found unexpected groupings. An icon should only be in its own category OR it can have solid and regular versions: $invalidGroupings")
+                }
+            }
+
+        // Generate four types of functions: solid only, regular only, solid or regular, and brand
+        val iconMethodEntries = iconCategories
+            .map { entry ->
+                val rawName = entry.key
+                // Convert e.g. "align-items" to "FaAlignItems"
+                @Suppress("DEPRECATION") // capitalize is way more readable than a direct replacement
+                val methodName = "Fa${rawName.split("-").joinToString("") { it.capitalize() }}"
+                val categories = entry.value
+
+                when {
+                    categories.size == 2 -> {
+                        "@Composable fun $methodName(modifier: Modifier = Modifier, style: IconStyle = IconStyle.OUTLINE, size: IconSize? = null) = FaIcon(\"$rawName\", modifier, style.category, size)"
+                    }
+
+                    categories.contains(IconCategory.SOLID) -> {
+                        "@Composable fun $methodName(modifier: Modifier = Modifier, size: IconSize? = null) = FaIcon(\"$rawName\", modifier, IconCategory.SOLID, size)"
+                    }
+
+                    categories.contains(IconCategory.REGULAR) -> {
+                        "@Composable fun $methodName(modifier: Modifier = Modifier, size: IconSize? = null) = FaIcon(\"$rawName\", modifier, IconCategory.REGULAR, size)"
+                    }
+
+                    categories.contains(IconCategory.BRAND) -> {
+                        "@Composable fun $methodName(modifier: Modifier = Modifier, size: IconSize? = null) = FaIcon(\"$rawName\", modifier, IconCategory.BRAND, size)"
+                    }
+
+                    else -> GradleException("Unhandled icon entry: $entry")
+                }
+            }
+
+        val iconsCode = """
+    //@formatter:off
+    @file:Suppress("unused", "SpellCheckingInspection")
+
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // THIS FILE IS AUTOGENERATED.
+    //
+    // Do not edit this file by hand. Instead, update `fa-icon-list.txt` in the module root and run the Gradle
+    // task "generateIcons"
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    package com.varabyte.kobweb.silk.components.icons.fa
+
+    import androidx.compose.runtime.*
+    import com.varabyte.kobweb.compose.ui.Modifier
+    import com.varabyte.kobweb.compose.ui.toAttrs
+    import org.jetbrains.compose.web.dom.Span
+
+    enum class IconCategory(internal val className: String) {
+        REGULAR("far"),
+        SOLID("fas"),
+        BRAND("fab");
     }
 
-    // Sanity check results
-    iconCategories
-        .filterNot { entry ->
-            val categories = entry.value
-            categories.size == 1 ||
-                (categories.size == 2 && categories.contains(IconCategory.SOLID) && categories.contains(IconCategory.REGULAR))
-        }
-        .let { invalidGroupings ->
-            if (invalidGroupings.isNotEmpty()) {
-                throw GradleException("Found unexpected groupings. An icon should only be in its own category OR it can have solid and regular versions: $invalidGroupings")
+    enum class IconStyle(internal val category: IconCategory) {
+        FILLED(IconCategory.SOLID),
+        OUTLINE(IconCategory.REGULAR);
+    }
+
+    // See: https://fontawesome.com/docs/web/style/size
+    enum class IconSize(internal val className: String) {
+        // Relative sizes
+        XXS("fa-2xs"),
+        XS("fa-xs"),
+        SM("fa-sm"),
+        LG("fa-lg"),
+        XL("fa-xl"),
+        XXL("fa-2xl"),
+
+        // Literal sizes
+        X1("fa-1x"),
+        X2("fa-2x"),
+        X3("fa-3x"),
+        X4("fa-4x"),
+        X5("fa-5x"),
+        X6("fa-6x"),
+        X7("fa-7x"),
+        X8("fa-8x"),
+        X9("fa-9x"),
+        X10("fa-10x");
+    }
+
+    @Composable
+    fun FaIcon(
+        name: String,
+        modifier: Modifier,
+        style: IconCategory = IconCategory.REGULAR,
+        size: IconSize? = null,
+    ) {
+        Span(
+            attrs = modifier.toAttrs {
+                classes(style.className, "fa-${'$'}name")
+                if (size != null) {
+                    classes(size.className)
+                }
             }
-        }
+        )
+    }
 
-    // Generate four types of functions: solid only, regular only, solid or regular, and brand
-    val iconMethodEntries = iconCategories
-        .map { entry ->
-            val rawName = entry.key
-            // Convert e.g. "align-items" to "FaAlignItems"
-            @Suppress("DEPRECATION") // capitalize is way more readable than a direct replacement
-            val methodName = "Fa${rawName.split("-").joinToString("") { it.capitalize() }}"
-            val categories = entry.value
+    ${iconMethodEntries.joinToString("\n")}
+        """.trimIndent()
 
-            when {
-                categories.size == 2 -> {
-                    "@Composable fun $methodName(modifier: Modifier = Modifier, style: IconStyle = IconStyle.OUTLINE, size: IconSize? = null) = FaIcon(\"$rawName\", modifier, style.category, size)"
-                }
-
-                categories.contains(IconCategory.SOLID) -> {
-                    "@Composable fun $methodName(modifier: Modifier = Modifier, size: IconSize? = null) = FaIcon(\"$rawName\", modifier, IconCategory.SOLID, size)"
-                }
-
-                categories.contains(IconCategory.REGULAR) -> {
-                    "@Composable fun $methodName(modifier: Modifier = Modifier, size: IconSize? = null) = FaIcon(\"$rawName\", modifier, IconCategory.REGULAR, size)"
-                }
-
-                categories.contains(IconCategory.BRAND) -> {
-                    "@Composable fun $methodName(modifier: Modifier = Modifier, size: IconSize? = null) = FaIcon(\"$rawName\", modifier, IconCategory.BRAND, size)"
-                }
-
-                else -> GradleException("Unhandled icon entry: $entry")
-            }
-        }
-
-    val iconsCode = """
-//@formatter:off
-@file:Suppress("unused", "SpellCheckingInspection")
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// THIS FILE IS AUTOGENERATED.
-//
-// Do not edit this file by hand. Instead, update `fa-icon-list.txt` in the module root and run the Gradle
-// task "regenerateIcons"
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-package com.varabyte.kobweb.silk.components.icons.fa
-
-import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.toAttrs
-import org.jetbrains.compose.web.dom.Span
-
-enum class IconCategory(internal val className: String) {
-    REGULAR("far"),
-    SOLID("fas"),
-    BRAND("fab");
-}
-
-enum class IconStyle(internal val category: IconCategory) {
-    FILLED(IconCategory.SOLID),
-    OUTLINE(IconCategory.REGULAR);
-}
-
-// See: https://fontawesome.com/docs/web/style/size
-enum class IconSize(internal val className: String) {
-    // Relative sizes
-    XXS("fa-2xs"),
-    XS("fa-xs"),
-    SM("fa-sm"),
-    LG("fa-lg"),
-    XL("fa-xl"),
-    XXL("fa-2xl"),
-
-    // Literal sizes
-    X1("fa-1x"),
-    X2("fa-2x"),
-    X3("fa-3x"),
-    X4("fa-4x"),
-    X5("fa-5x"),
-    X6("fa-6x"),
-    X7("fa-7x"),
-    X8("fa-8x"),
-    X9("fa-9x"),
-    X10("fa-10x");
-}
-
-@Composable
-fun FaIcon(
-    name: String,
-    modifier: Modifier,
-    style: IconCategory = IconCategory.REGULAR,
-    size: IconSize? = null,
-) {
-    Span(
-        attrs = modifier.toAttrs {
-            classes(style.className, "fa-${'$'}name")
-            if (size != null) {
-                classes(size.className)
-            }
-        }
-    )
-}
-
-${iconMethodEntries.joinToString("\n")}
-    """.trimIndent()
-
-    dstFile.asFile.writeText(iconsCode)
+        dstFile.asFile.writeText(iconsCode)
+    }
 }
 
 tasks.named("compileKotlinJs") {
-    dependsOn(regenerateIconsTask)
-}
-
-tasks.named("sourcesJar") {
-    dependsOn(regenerateIconsTask)
-}
-
-tasks.named("jsSourcesJar") {
-    dependsOn(regenerateIconsTask)
+    dependsOn(generateIconsTask)
 }
--- frontend/kobweb-silk-icons-mdi/build.gradle.kts
+++ frontend/kobweb-silk-icons-mdi/build.gradle.kts
@@ -10,6 +10,8 @@ plugins {
 group = "com.varabyte.kobweb"
 version = libs.versions.kobweb.libs.get()
 
+private val GENERATED_SRC_ROOT = "build/generated/kobweb-silk-icons-mdi/src/jsMain/kotlin"
+
 kotlin {
     js {
         browser()
@@ -18,6 +20,7 @@ kotlin {
 
     sourceSets {
         val jsMain by getting {
+            kotlin.srcDir(GENERATED_SRC_ROOT)
             dependencies {
                 implementation(compose.runtime)
                 implementation(compose.html.core)
@@ -42,154 +45,148 @@ enum class IconStyle {
     TWO_TONED;
 }
 
-val regenerateIconsTask = tasks.register("regenerateIcons") {
+val generateIconsTask = tasks.register("generateIcons") {
     val srcFile = layout.projectDirectory.file("md-icon-list.txt")
     val dstFile =
-        layout.projectDirectory.file("src/jsMain/kotlin/com/varabyte/kobweb/silk/components/icons/mdi/MdIcons.kt")
+        layout.projectDirectory.file("$GENERATED_SRC_ROOT/com/varabyte/kobweb/silk/components/icons/mdi/MdIcons.kt")
 
-    inputs.files(srcFile, layout.projectDirectory.file("build.gradle.kts"))
+    inputs.files(srcFile)
     outputs.file(dstFile)
 
-    val iconRawNames = srcFile.asFile
-        .readLines()
-        .asSequence()
-        .filter { line -> !line.startsWith("#") }
-        .associate { line ->
-            // Convert icon name to function name, e.g.
-            // align-left -> MdiAlignLeft
-            line.split("=", limit = 2).let { parts ->
-                val style = when (parts[0]) {
-                    "mdi" -> IconStyle.FILLED
-                    "mdio" -> IconStyle.OUTLINED
-                    "mdir" -> IconStyle.ROUNDED
-                    "mdis" -> IconStyle.SHARP
-                    "mdit" -> IconStyle.TWO_TONED
-                    else -> throw GradleException("Unexpected style grouping: ${parts[0]}")
-                }
-                val names = parts[1]
+    doLast {
+        val iconRawNames = srcFile.asFile
+            .readLines()
+            .asSequence()
+            .filter { line -> !line.startsWith("#") }
+            .associate { line ->
+                // Convert icon name to function name, e.g.
+                // align-left -> MdiAlignLeft
+                line.split("=", limit = 2).let { parts ->
+                    val style = when (parts[0]) {
+                        "mdi" -> IconStyle.FILLED
+                        "mdio" -> IconStyle.OUTLINED
+                        "mdir" -> IconStyle.ROUNDED
+                        "mdis" -> IconStyle.SHARP
+                        "mdit" -> IconStyle.TWO_TONED
+                        else -> throw GradleException("Unexpected style grouping: ${parts[0]}")
+                    }
+                    val names = parts[1]
 
-                style to names.split(",")
+                    style to names.split(",")
+                }
+            }
+
+        // For each icon name, figure out what categories they are in. This will affect the function signature we generate.
+        val iconStyleGroups = mutableMapOf<String, MutableSet<IconStyle>>()
+        iconRawNames.forEach { entry ->
+            val category = entry.key
+            entry.value.forEach { rawName ->
+                iconStyleGroups.computeIfAbsent(rawName) { mutableSetOf() }.add(category)
             }
         }
 
-    // For each icon name, figure out what categories they are in. This will affect the function signature we generate.
-    val iconStyleGroups = mutableMapOf<String, MutableSet<IconStyle>>()
-    iconRawNames.forEach { entry ->
-        val category = entry.key
-        entry.value.forEach { rawName ->
-            iconStyleGroups.computeIfAbsent(rawName) { mutableSetOf() }.add(category)
-        }
-    }
+        val iconMethodEntries = iconStyleGroups
+            .map { entry ->
+                // TODO(194): Figure out how ligature fallbacks to work, if people report us breaking on legacy browsers.
+                //  See also: https://developers.google.com/fonts/docs/material_icons#using_the_icons_in_html
+                val rawName = entry.key
 
-    val iconMethodEntries = iconStyleGroups
-        .map { entry ->
-            // TODO(194): Figure out how ligature fallbacks to work, if people report us breaking on legacy browsers.
-            //  See also: https://developers.google.com/fonts/docs/material_icons#using_the_icons_in_html
-            val rawName = entry.key
+                @Suppress("DEPRECATION") // capitalize is way more readable than a direct replacement
+                val methodName = "Mdi" + rawName.split("_").joinToString("") { it.capitalize() }
+                val styles = entry.value
 
-            @Suppress("DEPRECATION") // capitalize is way more readable than a direct replacement
-            val methodName = "Mdi" + rawName.split("_").joinToString("") { it.capitalize() }
-            val styles = entry.value
+                when {
+                    // A rare solo-style icon? No need to allow a user to pass in a style parameter in that case
+                    styles.size == 1 -> {
+                        "@Composable fun $methodName(modifier: Modifier = Modifier) = MdIcon(\"$rawName\", modifier, ${styles.first().name})"
+                    }
 
-            when {
-                // A rare solo-style icon? No need to allow a user to pass in a style parameter in that case
-                styles.size == 1 -> {
-                    "@Composable fun $methodName(modifier: Modifier = Modifier) = MdIcon(\"$rawName\", modifier, ${styles.first().name})"
-                }
-
-                styles.isEmpty() -> {
-                    // This shouldn't be possible, but just in case...
-                    throw GradleException("Unexpected icon entry with no styles: $entry")
-                }
+                    styles.isEmpty() -> {
+                        // This shouldn't be possible, but just in case...
+                        throw GradleException("Unexpected icon entry with no styles: $entry")
+                    }
 
                 styles.size == IconStyle.values().size -> {
                     // This icon supports all styles. No need to assert input parameters.
                     "@Composable fun $methodName(modifier: Modifier = Modifier, style: IconStyle = FILLED) = MdIcon(\"$rawName\", modifier, style)"
                 }
 
-                else -> {
-                    // Not all styles are valid. For now, we throw an exception to inform the user about this, but we
-                    // can always decide to relax this later.
-                    "@Composable fun $methodName(modifier: Modifier = Modifier, style: IconStyle = ${styles.first().name}) = MdIcon(\"$rawName\", modifier, style.also { it.assertValidStyle(\"$methodName\", ${styles.joinToString { it.name }}) })"
+                    else -> {
+                        // Not all styles are valid. For now, we throw an exception to inform the user about this, but we
+                        // can always decide to relax this later.
+                        "@Composable fun $methodName(modifier: Modifier = Modifier, style: IconStyle = ${styles.first().name}) = MdIcon(\"$rawName\", modifier, style.also { it.assertValidStyle(\"$methodName\", ${styles.joinToString { it.name }}) })"
+                    }
                 }
             }
+
+        val iconsCode = """
+    //@formatter:off
+    @file:Suppress("unused", "SpellCheckingInspection")
+
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // THIS FILE IS AUTOGENERATED.
+    //
+    // Do not edit this file by hand. Instead, update `md-icon-list.txt` in the module root and run the Gradle
+    // task "generateIcons"
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    package com.varabyte.kobweb.silk.components.icons.mdi
+
+    import androidx.compose.runtime.*
+    import com.varabyte.kobweb.compose.ui.Modifier
+    import com.varabyte.kobweb.compose.ui.toAttrs
+    import com.varabyte.kobweb.silk.components.icons.mdi.IconStyle.FILLED
+    import com.varabyte.kobweb.silk.components.icons.mdi.IconStyle.OUTLINED
+    import com.varabyte.kobweb.silk.components.icons.mdi.IconStyle.ROUNDED
+    import com.varabyte.kobweb.silk.components.icons.mdi.IconStyle.SHARP
+    import com.varabyte.kobweb.silk.components.icons.mdi.IconStyle.TWO_TONED
+    import org.jetbrains.compose.web.dom.Span
+    import org.jetbrains.compose.web.dom.Text
+
+    enum class IconStyle {
+        FILLED,
+        OUTLINED,
+        ROUNDED,
+        SHARP,
+        TWO_TONED;
+    }
+
+    private fun IconStyle.toClassNameSuffix(): String {
+        return when (this) {
+            FILLED -> ""
+            OUTLINED -> "-outlined"
+            ROUNDED -> "-round"
+            SHARP -> "-sharp"
+            TWO_TONED -> "-two-tone"
         }
-
-    val iconsCode = """
-//@formatter:off
-@file:Suppress("unused", "SpellCheckingInspection")
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// THIS FILE IS AUTOGENERATED.
-//
-// Do not edit this file by hand. Instead, update `md-icon-list.txt` in the module root and run the Gradle
-// task "regenerateIcons"
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-package com.varabyte.kobweb.silk.components.icons.mdi
-
-import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.toAttrs
-import com.varabyte.kobweb.silk.components.icons.mdi.IconStyle.FILLED
-import com.varabyte.kobweb.silk.components.icons.mdi.IconStyle.OUTLINED
-import com.varabyte.kobweb.silk.components.icons.mdi.IconStyle.ROUNDED
-import com.varabyte.kobweb.silk.components.icons.mdi.IconStyle.SHARP
-import com.varabyte.kobweb.silk.components.icons.mdi.IconStyle.TWO_TONED
-import org.jetbrains.compose.web.dom.Span
-import org.jetbrains.compose.web.dom.Text
-
-enum class IconStyle {
-    FILLED,
-    OUTLINED,
-    ROUNDED,
-    SHARP,
-    TWO_TONED;
-}
-
-private fun IconStyle.toClassNameSuffix(): String {
-    return when (this) {
-        FILLED -> ""
-        OUTLINED -> "-outlined"
-        ROUNDED -> "-round"
-        SHARP -> "-sharp"
-        TWO_TONED -> "-two-tone"
     }
-}
 
-private fun IconStyle.assertValidStyle(methodName: String, vararg validStyles: IconStyle) {
-    if (this !in validStyles) {
-        error("Attempted to render \"${'$'}methodName\" with style ${'$'}this. Must be one of: ${'$'}{validStyles.joinToString()}")
+    private fun IconStyle.assertValidStyle(methodName: String, vararg validStyles: IconStyle) {
+        if (this !in validStyles) {
+            error("Attempted to render \"${'$'}methodName\" with style ${'$'}this. Must be one of: ${'$'}{validStyles.joinToString()}")
+        }
     }
-}
 
-@Composable
-fun MdIcon(
-    name: String,
-    modifier: Modifier,
-    style: IconStyle = FILLED,
-) {
-    Span(
-        attrs = modifier.toAttrs { classes("material-icons${'$'}{style.toClassNameSuffix()}") }
+    @Composable
+    fun MdIcon(
+        name: String,
+        modifier: Modifier,
+        style: IconStyle = FILLED,
     ) {
-        Text(name)
+        Span(
+            attrs = modifier.toAttrs { classes("material-icons${'$'}{style.toClassNameSuffix()}") }
+        ) {
+            Text(name)
+        }
     }
-}
 
-${iconMethodEntries.joinToString("\n")}
-    """.trimIndent()
+    ${iconMethodEntries.joinToString("\n")}
+        """.trimIndent()
 
-    dstFile.asFile.writeText(iconsCode)
+        dstFile.asFile.writeText(iconsCode)
+    }
 }
 
 tasks.named("compileKotlinJs") {
-    dependsOn(regenerateIconsTask)
-}
-
-tasks.named("sourcesJar") {
-    dependsOn(regenerateIconsTask)
-}
-
-tasks.named("jsSourcesJar") {
-    dependsOn(regenerateIconsTask)
+    dependsOn(generateIconsTask)
 }
