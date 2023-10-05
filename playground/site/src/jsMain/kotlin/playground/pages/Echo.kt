63a3a886c3f3a91c2cb467e0b6a39fde601c297b
David Herman <bitspittle@gmail.com>
2023-10-05 10:39:46 -0700

    Nit: Update Echo playground page to use Silk's Input widget

--- playground/site/src/jsMain/kotlin/playground/pages/Echo.kt
+++ playground/site/src/jsMain/kotlin/playground/pages/Echo.kt
@@ -3,9 +3,8 @@ package playground.pages
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.silk.components.forms.Button
+import com.varabyte.kobweb.silk.components.forms.TextInput
 import com.varabyte.kobweb.streams.rememberApiStream
-import org.jetbrains.compose.web.attributes.InputType
-import org.jetbrains.compose.web.dom.Input
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
 import playground.components.layouts.PageLayout
@@ -26,15 +25,7 @@ fun EchoPage() {
             text = ""
         }
 
-        Input(
-            InputType.Text,
-            attrs = {
-                value(text)
-                onInput { e -> text = e.value }
-                onKeyDown { e -> if (e.key == "Enter") sendTextAndClear() }
-            }
-        )
-
+        TextInput(text, onTextChanged = { text = it }, onCommit = { sendTextAndClear() })
         P()
         Button(onClick = { sendTextAndClear() }, enabled = text.isNotBlank()) {
             Text("Send")
