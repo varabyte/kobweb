cb06f50b3ac23a3c101cc8400c18cc06ba9dfe35
Binay Shaw <binayshaw7777@gmail.com>
2023-10-13 03:04:29 +0530

    Added Arrows+, Attachment and Download Icons (#345)

--- frontend/silk-widgets/src/jsMain/kotlin/com/varabyte/kobweb/silk/components/icons/Icons.kt
+++ frontend/silk-widgets/src/jsMain/kotlin/com/varabyte/kobweb/silk/components/icons/Icons.kt
@@ -8,6 +8,7 @@ import com.varabyte.kobweb.compose.dom.svg.Path
 import com.varabyte.kobweb.compose.dom.svg.Polyline
 import com.varabyte.kobweb.compose.dom.svg.Rect
 import com.varabyte.kobweb.compose.dom.svg.SVGFillType
+import com.varabyte.kobweb.compose.dom.svg.SVGPaintType
 import com.varabyte.kobweb.compose.dom.svg.SVGStrokeLineCap
 import com.varabyte.kobweb.compose.dom.svg.SVGStrokeLineJoin
 import com.varabyte.kobweb.compose.dom.svg.SVGStrokeType
@@ -45,6 +46,119 @@ private fun createIcon(
         stroke(SVGStrokeType.CurrentColor)
         attrs?.invoke(this)
     }, content)
+}
+
+
+@Composable
+fun ArrowBackIcon() {
+    createIcon(attrs = { fill(SVGPaintType.CurrentColor) }) {
+        Path {
+            d {
+                moveTo(20, 11)
+                horizontalLineTo(7.83)
+                lineTo(5.59, -5.59, isRelative = true)
+                lineTo(12, 4)
+                lineTo(-8, 8, isRelative = true)
+                lineTo(8, 8, isRelative = true)
+                lineTo(1.41, -1.41, isRelative = true)
+                lineTo(7.83, 13)
+                horizontalLineTo(20)
+                verticalLineTo(-2, isRelative = true)
+                closePath()
+            }
+        }
+    }
+}
+
+@Composable
+fun ArrowDownIcon() {
+    createIcon(attrs = { fill(SVGPaintType.CurrentColor) }) {
+        Path {
+            d {
+                moveTo(20, 12)
+                lineTo(-1.41, -1.41, isRelative = true)
+                lineTo(13, 16.17)
+                verticalLineTo(4)
+                horizontalLineTo(-2, isRelative = true)
+                verticalLineTo(12.17, isRelative = true)
+                lineTo(-5.58, -5.59, isRelative = true)
+                lineTo(4, 12)
+                lineTo(8, 8, isRelative = true)
+                lineTo(8, -8, isRelative = true)
+                closePath()
+            }
+        }
+    }
+}
+
+@Composable
+fun ArrowForwardIcon() {
+    createIcon(attrs = { fill(SVGPaintType.CurrentColor) }) {
+        Path {
+            d {
+                moveTo(12, 4)
+                lineTo(-1.14, 1.41, isRelative = true)
+                lineTo(16.17, 11)
+                horizontalLineTo(4)
+                verticalLineTo(2, isRelative = true)
+                horizontalLineTo(12.17, isRelative = true)
+                lineTo(-5.58, 5.59, isRelative = true)
+                lineTo(12, 20)
+                lineTo(8, -8, isRelative = true)
+                closePath()
+            }
+        }
+    }
+}
+
+@Composable
+fun ArrowUpIcon() {
+    createIcon(attrs = { fill(SVGPaintType.CurrentColor) }) {
+        Path {
+            d {
+                moveTo(4, 12)
+                lineTo(1.41, 1.41, isRelative = true)
+                lineTo(11, 7.83)
+                verticalLineTo(20)
+                horizontalLineTo(2, isRelative = true)
+                verticalLineTo(7.83)
+                lineTo(5.58, 5.59, isRelative = true)
+                lineTo(20, 12)
+                lineTo(-8, -8, isRelative = true)
+                lineTo(-8, 8, isRelative = true)
+                closePath()
+            }
+        }
+    }
+}
+
+@Composable
+fun AttachmentIcon() {
+    createIcon(attrs = { fill(SVGPaintType.CurrentColor) }) {
+        Path {
+            d {
+                moveTo(21.843, 3.455)
+                ellipticalArc(6.961, 6.961, 0, 0, 0, -9.846, 0, isRelative = true)
+                lineTo(1.619, 13.832)
+                ellipticalArc(5.128, 5.128, 0, 0, 0, 7.252, 7.252, isRelative = true)
+                lineTo(17.3, 12.653)
+                ellipticalArc(3.293, 3.293, 0, 1, 0, 12.646, 8)
+                lineTo(7.457, 13.184)
+                ellipticalArc(1, 1, 0, 1, 0, 8.871, 14.6)
+                lineTo(14.06, 9.409)
+                ellipticalArc(1.294, 1.294, 0, 0, 1, 1.829, 1.83, isRelative = true)
+                lineTo(7.457, 19.67)
+                ellipticalArc(3.128, 3.128, 0, 0, 1, -4.424, -4.424, isRelative = true)
+                lineTo(13.411, 4.869)
+                ellipticalArc(4.962, 4.962, 0, 1, 1, 7.018, 7.018, isRelative = true)
+                lineTo(12.646, 19.67)
+                ellipticalArc(1, 1, 0, 1, 0, 1.414, 1.414, isRelative = true)
+                lineTo(21.843, 13.3)
+                ellipticalArc(6.96, 6.96, 0, 0, 0, 0, -9.846, isRelative = true)
+                closePath()
+            }
+        }
+    }
 }
 
 @Composable
@@ -149,6 +263,36 @@ fun CloseIcon() {
             x2(1)
             y1(1)
             y2(23)
+        }
+    }
+}
+
+@Composable
+fun DownloadIcon() {
+    createIcon(viewBox = ViewBox(0, 0, 14, 14), attrs = {
+        fill(SVGPaintType.CurrentColor)
+        strokeWidth(0)
+    }) {
+        Path {
+            d {
+                moveTo(11.2857, 6.05714)
+                lineTo(10.08571, 4.85714)
+                lineTo(7.85714, 7.14786)
+                lineTo(7.85714, 1)
+                lineTo(6.14286, 1)
+                lineTo(6.14286, 7.14786)
+                lineTo(3.91429, 4.85714)
+                lineTo(2.71429, 6.05714)
+                lineTo(7, 10.42857)
+                lineTo(11.2857, 6.05714)
+                closePath()
+                moveTo(1, 11.2857)
+                lineTo(1, 13)
+                lineTo(13, 13)
+                lineTo(13, 11.2857)
+                lineTo(1, 11.2857)
+                closePath()
+            }
         }
     }
 }
