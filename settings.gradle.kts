8fa62d9a8f121dd3f8b64ef0dd5e5a2188da9fc7
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2023-11-25 22:05:00 -0500

    Enable gradle build scans on CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,19 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.gradle.enterprise") version ("3.15.1")
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
+
 rootProject.name = "kobweb"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
