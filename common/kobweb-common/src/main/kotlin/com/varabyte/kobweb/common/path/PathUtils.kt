ba2771a27999549195d4ad3807e12ce71bd49a7b
David Herman <bitspittle@gmail.com>
2023-12-21 11:27:13 -0800

    Fix unexpected trailing slash getting removed with invariantSeparatorsPath extension property

--- common/kobweb-common/src/main/kotlin/com/varabyte/kobweb/common/path/PathUtils.kt
+++ common/kobweb-common/src/main/kotlin/com/varabyte/kobweb/common/path/PathUtils.kt
@@ -5,4 +5,6 @@ import java.io.File
 /**
  * A convenience version for [File.invariantSeparatorsPath] for strings that represent paths.
  */
-val String.invariantSeparatorsPath get() = File(this).invariantSeparatorsPath
+// NOTE: We don't delegate to File(this).invariantSeparatorsPath because that has a side effect of stripping out
+// trailing slashes, which we don't want.
+val String.invariantSeparatorsPath get() = if (File.separatorChar != '/') this.replace(File.separatorChar, '/') else this
