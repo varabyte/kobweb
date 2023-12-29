bd62cf2fe1a87e4e75713b7fbf618c70c7a1203a
David Herman <bitspittle@gmail.com>
2023-12-29 13:00:40 -0800

    Update Modifier.transform to stop experimental warning

--- frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/TransformModifiers.kt
+++ frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/TransformModifiers.kt
@@ -6,7 +6,7 @@ import com.varabyte.kobweb.compose.ui.styleModifier
 import org.jetbrains.compose.web.ExperimentalComposeWebApi
 import org.jetbrains.compose.web.css.*
 
-@ExperimentalComposeWebApi
+@OptIn(ExperimentalComposeWebApi::class)
 fun Modifier.transform(transformContext: TransformBuilder.() -> Unit) = styleModifier {
     transform(transformContext)
 }
