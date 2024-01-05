f4cfe3ced40c2daa914485a8b15458b3b57b4374
Ellet <ellet@freshplatform.net>
2024-01-05 11:38:45 +0300

    Update the MultiColumnModifiers.kt

--- frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/MultiColumn.kt
+++ frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/MultiColumn.kt
@@ -2,24 +2,6 @@ package com.varabyte.kobweb.compose.css
 
 import org.jetbrains.compose.web.css.*
 
-// https://developer.mozilla.org/en-US/docs/Web/CSS/column-rule
-class ColumnRule private constructor(private val value: String) : StylePropertyValue {
-    override fun toString() = value
-
-    companion object {
-
-        // Global values
-        val Inherit get() = ColumnRule("inherit")
-        val Initial get() = ColumnRule("initial")
-        val Revert get() = ColumnRule("revert")
-        val Unset get() = ColumnRule("unset")
-    }
-}
-
-fun StyleScope.columnRule(columnRule: ColumnRule) {
-    property("column-rule", columnRule)
-}
-
 fun StyleScope.columnRuleColor(color: CSSColorValue) {
     property("column-rule-color", color)
 }
--- frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/MultiColumnModifiers.kt
+++ frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/ui/modifiers/MultiColumnModifiers.kt
@@ -2,23 +2,30 @@ package com.varabyte.kobweb.compose.ui.modifiers
 
 import com.varabyte.kobweb.compose.css.*
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.styleModifier
 import org.jetbrains.compose.web.css.*
+class ColumnRuleScope internal constructor(private val styleScope: StyleScope) {
+    fun color(color: CSSColorValue) = styleScope.borderColor(color)
+    fun style(lineStyle: LineStyle) = styleScope.borderStyle(lineStyle)
+    fun width(width: CSSLengthNumericValue) = styleScope.borderWidth(width)
+    fun width(topAndBottom: CSSLengthNumericValue, leftAndRight: CSSLengthNumericValue) =
+        styleScope.borderWidth(topAndBottom, leftAndRight)
 
-fun Modifier.columnRule(columnRule: ColumnRule) = styleModifier {
-    columnRule(columnRule)
+    fun width(top: CSSLengthNumericValue, leftAndRight: CSSLengthNumericValue, bottom: CSSLengthNumericValue) =
+        styleScope.borderWidth(top, leftAndRight, bottom)
+
+    fun width(
+        top: CSSLengthNumericValue,
+        right: CSSLengthNumericValue,
+        bottom: CSSLengthNumericValue,
+        left: CSSLengthNumericValue
+    ) =
+        styleScope.borderWidth(top, right, bottom, left)
 }
 
-fun Modifier.columnRuleColor(color: CSSColorValue) = styleModifier {
-    columnRuleColor(color)
-}
-
-fun Modifier.columnRuleStyle(style: LineStyle) = styleModifier {
-    columnRuleStyle(style)
-}
-
-fun Modifier.columnRuleWidth(width: CSSLengthNumericValue) = styleModifier {
-    columnRuleWidth(width)
+fun Modifier.columnRule(scope: ColumnRuleScope.() -> Unit) = styleModifier {
+    ColumnRuleScope(this).apply(scope)
 }
 
 fun Modifier.columnRule(
@@ -26,7 +33,5 @@ fun Modifier.columnRule(
     style: LineStyle? = null,
     color: CSSColorValue? = null
 ) = styleModifier {
-    columnRule(
-        width, style, color
-    )
+    columnRule(width, style, color)
 }
