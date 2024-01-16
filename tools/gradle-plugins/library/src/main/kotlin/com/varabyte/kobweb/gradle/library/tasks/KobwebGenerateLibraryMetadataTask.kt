e8f8d8b45e1cc3d761f2179d17f446d02ec80e93
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-01-16 16:01:42 -0500

    Make `kobwebGenerateLibraryMetadata` compatible with configuration cache Also remove the "Task" suffix from the task name for consistency with other tasks.

--- tools/gradle-plugins/library/src/main/kotlin/com/varabyte/kobweb/gradle/library/tasks/KobwebGenerateLibraryMetadataTask.kt
+++ tools/gradle-plugins/library/src/main/kotlin/com/varabyte/kobweb/gradle/library/tasks/KobwebGenerateLibraryMetadataTask.kt
@@ -1,22 +1,23 @@
 package com.varabyte.kobweb.gradle.library.tasks
 
-import com.varabyte.kobweb.gradle.core.extensions.kobwebBlock
 import com.varabyte.kobweb.gradle.core.metadata.LibraryMetadata
 import com.varabyte.kobweb.gradle.core.tasks.KobwebTask
-import com.varabyte.kobweb.gradle.library.extensions.index
-import com.varabyte.kobweb.gradle.library.extensions.library
 import com.varabyte.kobweb.ksp.KOBWEB_METADATA_LIBRARY
+import kotlinx.html.HEAD
 import kotlinx.html.head
 import kotlinx.html.stream.createHTML
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import org.gradle.api.Project
+import org.gradle.api.provider.ListProperty
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
-import javax.inject.Inject
 
-abstract class KobwebGenerateLibraryMetadataTask @Inject constructor(private val project: Project) :
+abstract class KobwebGenerateLibraryMetadataTask :
     KobwebTask("Generate a library.json metadata file into this project's jar metadata, which identifies this artifact as a Kobweb library.") {
+
+    @get:Input
+    abstract val indexHead: ListProperty<HEAD.() -> Unit>
 
     @OutputDirectory
     fun getGenResDir() = projectLayout.buildDirectory.dir("generated/kobweb/library/metadata")
@@ -27,8 +28,7 @@ abstract class KobwebGenerateLibraryMetadataTask @Inject constructor(private val
         libraryMetadataFile.asFile.apply {
             parentFile.mkdirs()
 
-            val libraryBlock = project.kobwebBlock.library
-            val headElements = libraryBlock.index.head.orNull?.takeIf { it.isNotEmpty() }
+            val headElements = indexHead.orNull?.takeIf { it.isNotEmpty() }
             writeText(
                 Json.encodeToString(
                     LibraryMetadata(
