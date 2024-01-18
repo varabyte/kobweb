33e90ea5c8fb32cfd945d5b8d1aa888184e9dd43
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-01-17 20:13:45 -0500

    Introduce common `KobwebCopyTask`

--- tools/gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/tasks/KobwebCopyWorkerJsOutputTask.kt
+++ tools/gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/tasks/KobwebCopyWorkerJsOutputTask.kt
@@ -1,43 +1,25 @@
 package com.varabyte.kobweb.gradle.application.tasks
 
-import com.varabyte.kobweb.common.path.invariantSeparatorsPath
 import com.varabyte.kobweb.gradle.application.extensions.AppBlock
 import com.varabyte.kobweb.gradle.core.extensions.KobwebBlock
 import com.varabyte.kobweb.gradle.core.kmp.jsTarget
-import com.varabyte.kobweb.gradle.core.tasks.KobwebModuleTask
-import com.varabyte.kobweb.gradle.core.util.RootAndFile
-import com.varabyte.kobweb.ksp.KOBWEB_METADATA_MODULE
 import com.varabyte.kobweb.ksp.KOBWEB_METADATA_WORKER_SUBFOLDER
 import com.varabyte.kobweb.ksp.KOBWEB_PUBLIC_WORKER_ROOT
 import org.gradle.api.GradleException
-import org.gradle.api.file.ArchiveOperations
 import org.gradle.api.file.DuplicatesStrategy
-import org.gradle.api.file.FileSystemOperations
-import org.gradle.api.file.FileTree
-import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.util.PatternSet
-import java.io.File
 import javax.inject.Inject
 
 /**
  * Copy JS output which is automatically packaged into metadata by the Kobweb worker plugin.
  */
-abstract class KobwebCopyWorkerJsOutputTask @Inject constructor(kobwebBlock: KobwebBlock) : KobwebModuleTask(
+abstract class KobwebCopyWorkerJsOutputTask @Inject constructor(kobwebBlock: KobwebBlock) : KobwebCopyTask(
     kobwebBlock,
     "Copy any JS output files from any Kobweb worker dependencies and copy them to the final site's resources"
 ) {
-    @get:Inject
-    abstract val fileSystemOperations: FileSystemOperations
-
-    @get:Inject
-    abstract val objectFactory: ObjectFactory
-
-    @get:Inject
-    abstract val archiveOperations: ArchiveOperations
-
     @InputFiles
     fun getRuntimeClasspath() = project.configurations.named(project.jsTarget.runtimeClasspath)
 
@@ -49,28 +31,8 @@ abstract class KobwebCopyWorkerJsOutputTask @Inject constructor(kobwebBlock: Kob
     @TaskAction
     fun execute() {
         val classpath = getRuntimeClasspath().get()
-        val kobwebModulePattern = PatternSet().apply {
-            include(KOBWEB_METADATA_MODULE)
-        }
         val workerOutpusFilesPattern = PatternSet().apply {
             include("$KOBWEB_METADATA_WORKER_SUBFOLDER/**")
-        }
-
-        // Find the "worker.js" and "worker.js.map" files bundled into the jars that were built with by Kobweb worker
-        // plugin.
-        fun FileTree.toWorkerOutputResources(jar: File): List<Pair<File, RootAndFile>> {
-            val fileTree = this
-            if (fileTree.matching(kobwebModulePattern).isEmpty) return emptyList()
-
-            return buildList {
-                fileTree.matching(workerOutpusFilesPattern)
-                    .visit {
-                        if (this.isDirectory) return@visit
-                        val root =
-                            File(file.absolutePath.invariantSeparatorsPath.removeSuffix(relativePath))
-                        add(jar to RootAndFile(root, file))
-                    }
-            }
         }
 
         // We track "copied file path to source jar path" so we can warn users if there's a collision.
@@ -78,24 +40,7 @@ abstract class KobwebCopyWorkerJsOutputTask @Inject constructor(kobwebBlock: Kob
         // will have to be the same across different projects. But it could happen if a user duplicates a
         // worker project and forgets to update either the group name or worker name.
         val copiedFiles = mutableMapOf<String, String>()
-        val workerOutputData = classpath.flatMap { jar ->
-            if (jar.isDirectory) {
-                objectFactory.fileTree().from(jar).toWorkerOutputResources(jar)
-            } else {
-                try {
-                    archiveOperations.zipTree(jar).toWorkerOutputResources(jar)
-                } catch (ex: Exception) {
-                    // NOTE: I used to catch ZipException here, but it became GradleException at some point?? So
-                    // let's just be safe and block all exceptions here. It sucks if this task crashes here because
-                    // not being able to unzip a non-zip file is not really a big deal.
-
-                    // It's possible to get a classpath file that's not a jar -- npm dependencies are like this --
-                    // at which point the file isn't a zip nor a directory. Such dependencies will never contain
-                    // Kobweb resources, so we don't care about them. Just skip 'em!
-                    emptyList()
-                }
-            }
-        }
+        val workerOutputData = classpath.toKobwebOutputByPattern(workerOutpusFilesPattern)
 
         fileSystemOperations.sync {
             duplicatesStrategy = DuplicatesStrategy.EXCLUDE
