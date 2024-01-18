65ac20113c58a2e56785ee3c25e9330401efb6f5
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-01-17 20:02:22 -0500

    Use gradle's `Sync` for kobweb copy task. This should lead to better handling of stale files. Also reduce runtime usages of `task.project`.

--- tools/gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/tasks/KobwebCopyWorkerJsOutputTask.kt
+++ tools/gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/tasks/KobwebCopyWorkerJsOutputTask.kt
@@ -10,7 +10,11 @@ import com.varabyte.kobweb.ksp.KOBWEB_METADATA_MODULE
 import com.varabyte.kobweb.ksp.KOBWEB_METADATA_WORKER_SUBFOLDER
 import com.varabyte.kobweb.ksp.KOBWEB_PUBLIC_WORKER_ROOT
 import org.gradle.api.GradleException
+import org.gradle.api.file.ArchiveOperations
+import org.gradle.api.file.DuplicatesStrategy
+import org.gradle.api.file.FileSystemOperations
 import org.gradle.api.file.FileTree
+import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
@@ -22,8 +26,17 @@ import javax.inject.Inject
  * Copy JS output which is automatically packaged into metadata by the Kobweb worker plugin.
  */
 abstract class KobwebCopyWorkerJsOutputTask @Inject constructor(kobwebBlock: KobwebBlock) : KobwebModuleTask(
-    kobwebBlock, "Copy any JS output files from any Kobweb worker dependencies and copy them to the final site's resources"
+    kobwebBlock,
+    "Copy any JS output files from any Kobweb worker dependencies and copy them to the final site's resources"
 ) {
+    @get:Inject
+    abstract val fileSystemOperations: FileSystemOperations
+
+    @get:Inject
+    abstract val objectFactory: ObjectFactory
+
+    @get:Inject
+    abstract val archiveOperations: ArchiveOperations
 
     @InputFiles
     fun getRuntimeClasspath() = project.configurations.named(project.jsTarget.runtimeClasspath)
@@ -65,37 +78,43 @@ abstract class KobwebCopyWorkerJsOutputTask @Inject constructor(kobwebBlock: Kob
         // will have to be the same across different projects. But it could happen if a user duplicates a
         // worker project and forgets to update either the group name or worker name.
         val copiedFiles = mutableMapOf<String, String>()
-        classpath
-            .flatMap { jar ->
-                if (jar.isDirectory) {
-                    project.fileTree(jar).toWorkerOutputResources(jar)
-                } else {
-                    try {
-                        project.zipTree(jar).toWorkerOutputResources(jar)
-                    } catch (ex: Exception) {
-                        // NOTE: I used to catch ZipException here, but it became GradleException at some point?? So
-                        // let's just be safe and block all exceptions here. It sucks if this task crashes here because
-                        // not being able to unzip a non-zip file is not really a big deal.
+        val workerOutputData = classpath.flatMap { jar ->
+            if (jar.isDirectory) {
+                objectFactory.fileTree().from(jar).toWorkerOutputResources(jar)
+            } else {
+                try {
+                    archiveOperations.zipTree(jar).toWorkerOutputResources(jar)
+                } catch (ex: Exception) {
+                    // NOTE: I used to catch ZipException here, but it became GradleException at some point?? So
+                    // let's just be safe and block all exceptions here. It sucks if this task crashes here because
+                    // not being able to unzip a non-zip file is not really a big deal.
 
-                        // It's possible to get a classpath file that's not a jar -- npm dependencies are like this --
-                        // at which point the file isn't a zip nor a directory. Such dependencies will never contain
-                        // Kobweb resources, so we don't care about them. Just skip 'em!
-                        emptyList()
-                    }
+                    // It's possible to get a classpath file that's not a jar -- npm dependencies are like this --
+                    // at which point the file isn't a zip nor a directory. Such dependencies will never contain
+                    // Kobweb resources, so we don't care about them. Just skip 'em!
+                    emptyList()
                 }
-            }.forEach { (jar, rootAndFile) ->
-                val targetFile =
-                    getGenPublicRoot()
+            }
+        }
+
+        fileSystemOperations.sync {
+            duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+            into(getGenPublicRoot())
+            workerOutputData.forEach { (jar, rootAndFile) ->
+                from(rootAndFile.file) {
+                    val targetFile = getGenPublicRoot()
                         .resolve(KOBWEB_PUBLIC_WORKER_ROOT)
                         .resolve(
                             rootAndFile.relativeFile.invariantSeparatorsPath.removePrefix("$KOBWEB_METADATA_WORKER_SUBFOLDER/")
                         )
 
-                copiedFiles.putIfAbsent(targetFile.absolutePath, jar.name)?.let { sourceJar ->
-                    throw GradleException("You are attempting to use two different worker dependencies that both have the same group and worker name: $sourceJar and ${jar.name}. They are incompatible and you cannot use both. If you own the project for either, consider changing the Gradle group or the worker name (via `kobweb { worker { name.set(...) } }`) on one or the other to avoid this naming collision.")
-                }
+                    copiedFiles.putIfAbsent(targetFile.absolutePath, jar.name)?.let { sourceJar ->
+                        throw GradleException("You are attempting to use two different worker dependencies that both have the same group and worker name: $sourceJar and ${jar.name}. They are incompatible and you cannot use both. If you own the project for either, consider changing the Gradle group or the worker name (via `kobweb { worker { name.set(...) } }`) on one or the other to avoid this naming collision.")
+                    }
 
-                rootAndFile.file.copyTo(targetFile, overwrite = true)
+                    into(targetFile.relativeTo(getGenPublicRoot()).parentFile) // relative to top-level "into" dir
+                }
             }
+        }
     }
 }
