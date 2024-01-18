8d38304ba02f70568312f2bd0f76d52d46ccd839
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-01-18 01:58:01 -0500

    Remove unnecessary functions in `KobwebModuleTask` We do not need to depend on the buildscripts directly, as each task declares the inputs it needs, and gradle will rerun the task if they change.

--- tools/gradle-plugins/core/src/main/kotlin/com/varabyte/kobweb/gradle/core/tasks/KobwebModuleTask.kt
+++ tools/gradle-plugins/core/src/main/kotlin/com/varabyte/kobweb/gradle/core/tasks/KobwebModuleTask.kt
@@ -3,29 +3,17 @@ package com.varabyte.kobweb.gradle.core.tasks
 import com.varabyte.kobweb.gradle.core.extensions.KobwebBlock
 import com.varabyte.kobweb.gradle.core.kmp.jsTarget
 import com.varabyte.kobweb.gradle.core.util.RootAndFile
-import com.varabyte.kobweb.gradle.core.util.getBuildScripts
 import com.varabyte.kobweb.gradle.core.util.getResourceFilesWithRoots
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Internal
-import org.gradle.api.tasks.PathSensitive
-import org.gradle.api.tasks.PathSensitivity
-import java.io.File
 
 /**
  * Base class for any task that needs to know about any source / resources related to Kobweb in the current module.
  */
 abstract class KobwebModuleTask(@get:Internal val kobwebBlock: KobwebBlock, desc: String) : KobwebTask(desc) {
-    @InputFiles
-    @PathSensitive(PathSensitivity.RELATIVE) // rerun if contents or path relative to project root changes
-    fun getBuildScripts(): List<File> = project.getBuildScripts().toList()
-
     @Internal
     fun getResourceFilesJsWithRoots(): Sequence<RootAndFile> = project.getResourceFilesWithRoots(project.jsTarget)
         .filter { rootAndFile -> rootAndFile.relativeFile.invariantSeparatorsPath.startsWith("${getPublicPath()}/") }
-
-    @Internal
-    protected fun getResourceFilesJs(): List<File> = getResourceFilesJsWithRoots().map { it.file }.toList()
 
     /**
      * The root package of all pages.
