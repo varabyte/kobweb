2bf77d0a3759252aa33f36ab5c92e193abe710a6
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-01-27 01:33:55 -0500

    Nit: Avoid using raw strings & other cleanup

--- tools/gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/util/JarExtensions.kt
+++ tools/gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/util/JarExtensions.kt
@@ -1,11 +1,14 @@
 package com.varabyte.kobweb.gradle.application.util
 
 import com.varabyte.kobweb.common.text.suffixIfNot
+import com.varabyte.kobweb.gradle.core.kmp.JvmTarget
 import com.varabyte.kobweb.gradle.core.util.suggestKobwebProjectName
 import com.varabyte.kobweb.ksp.KOBWEB_METADATA_SUBFOLDER
 import org.gradle.api.file.DuplicatesStrategy
 import org.gradle.api.tasks.util.PatternSet
 import org.gradle.jvm.tasks.Jar
+import org.gradle.kotlin.dsl.assign
+import org.gradle.kotlin.dsl.named
 import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
 
 /**
@@ -21,15 +24,15 @@ import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
  */
 fun KotlinJvmTarget.kobwebServerJar(kobwebName: String? = null) {
     val archiveFileName = (kobwebName ?: project.suggestKobwebProjectName()).suffixIfNot(".jar")
-    val jvmTargetName = name
-    project.tasks.named("${jvmTargetName}Jar", Jar::class.java).configure {
-        this.archiveFileName.set(archiveFileName)
-
+    val jvmTarget = JvmTarget(this)
+    project.tasks.named<Jar>(jvmTarget.jar) {
+        this.archiveFileName = archiveFileName
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
         val patterns = PatternSet().apply {
             exclude("$KOBWEB_METADATA_SUBFOLDER/**")
         }
-        val classpathFiles = project.configurations.named("${jvmTargetName}RuntimeClasspath").map { configuration ->
+        val classpathFiles = project.configurations.named(jvmTarget.runtimeClasspath).map { configuration ->
             configuration.map { if (it.isDirectory) it else project.zipTree(it).matching(patterns) }
         }
         from(classpathFiles)
