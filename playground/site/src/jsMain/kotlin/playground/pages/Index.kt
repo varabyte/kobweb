38a5c69f31de482f4decff9e28eacd4e1a86cbd1
David Herman <bitspittle@gmail.com>
2024-03-04 10:26:58 -0800

    README: Add details about exporting

--- playground/site/src/jsMain/kotlin/playground/pages/Index.kt
+++ playground/site/src/jsMain/kotlin/playground/pages/Index.kt
@@ -2,20 +2,10 @@ package playground.pages
 
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.core.Page
-import com.varabyte.kobweb.silk.components.forms.TextInput
-import kotlinx.browser.window
-import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
-import playground.components.layouts.PageLayout
 
 @Page
 @Composable
-fun HomePage() {
-    PageLayout("Welcome to Kobweb!") {
-        Text("Please enter your name")
-        var name by remember { mutableStateOf("") }
-        TextInput(name, onTextChanged = { name = it })
-        P()
-        Text("Hello ${name.takeIf { it.isNotBlank() } ?: "World"}!")
-    }
+fun ExamplePage() {
+    Text("This is a minimal example to demonstrate exporting.")
 }
