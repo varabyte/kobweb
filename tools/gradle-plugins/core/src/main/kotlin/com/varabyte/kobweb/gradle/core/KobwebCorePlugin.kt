adb673b26543a0702c25ae09c5a345810c31d0df
David Herman <bitspittle@gmail.com>
2024-03-13 10:57:38 -0700

    Stop relocating legacy packages
    It's been more than 6 months since we did this, so it's time to stop
    and clean up code we don't need to support anymore.

--- tools/gradle-plugins/core/src/main/kotlin/com/varabyte/kobweb/gradle/core/KobwebCorePlugin.kt
+++ tools/gradle-plugins/core/src/main/kotlin/com/varabyte/kobweb/gradle/core/KobwebCorePlugin.kt
@@ -20,67 +20,6 @@ import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 class KobwebCorePlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val rootProject = project.rootProject
-        val versionCatalogPath = "gradle/libs.versions.toml"
-
-        run {
-            val groupIdKobweb = "com.varabyte.kobweb"
-            val groupIdKobwebx = "com.varabyte.kobwebx"
-            // Original artifact ID (grouped under com.varabyte.kobweb) to coordinate
-            val relocatedArtifacts = mapOf(
-                "kobweb-silk-widgets" to "$groupIdKobweb:silk-widgets",
-                "kobweb-silk-icons-fa" to "$groupIdKobwebx:silk-icons-fa",
-                "kobweb-silk-icons-mdi" to "$groupIdKobwebx:silk-icons-mdi",
-            )
-
-            val migrateDepsName = "kobwebMigrateDeps"
-            if (rootProject.tasks.findByName(migrateDepsName) != null) return@run
-
-            val migrateDepsTask = rootProject.tasks.register(migrateDepsName) {
-                inputs.file(versionCatalogPath)
-                outputs.file(versionCatalogPath)
-                group = "kobweb"
-                description =
-                    "Migrate any Kobweb dependencies found in `$versionCatalogPath` that should be relocated to new coordinates."
-
-                doLast {
-                    rootProject.layout.projectDirectory.file(versionCatalogPath).asFile.takeIf { it.exists() }
-                        ?.let { tomlFile ->
-                            val originalText = tomlFile.readText()
-                            var updatedText = originalText
-                            relocatedArtifacts.forEach { (legacyArtifactId, newCoordinate) ->
-                                updatedText =
-                                    updatedText.replace("$groupIdKobweb:$legacyArtifactId", newCoordinate)
-                            }
-                            if (originalText != updatedText) {
-                                println(
-                                    "Your `$versionCatalogPath` has been updated to use the latest Kobweb dependency names."
-                                )
-                                tomlFile.writeText(updatedText)
-                            } else {
-                                println(
-                                    "We did not find any Kobweb dependencies in `$versionCatalogPath` that need to be migrated."
-                                )
-                            }
-                        }
-                        ?: println(
-                            "We could not find `$versionCatalogPath` so we cannot try to migrate dependencies."
-                        )
-                }
-            }
-
-            project.gradle.taskGraph.whenReady {
-                // Warn the user about any relocated deps, if used, unless we're already running the task which will
-                // migrate them. If we show the warning then, it feels like the task failed even as it succeeded.
-                if (!this.hasTask(migrateDepsTask.get())) {
-                    relocatedArtifacts.forEach { (legacyArtifactId, newCoordinate) ->
-                        if (project.hasJsDependencyNamed(legacyArtifactId)) {
-                            project.logger.warn("w: The dependency `$groupIdKobweb:$legacyArtifactId` has been renamed to `$newCoordinate`. Please migrate to the new name. You can run `./gradlew :$migrateDepsName` to attempt to do this automatically. Failing to migrate will become an error in a future version of Kobweb.")
-                        }
-                    }
-                }
-            }
-
-        }
 
         // A `kobweb` block is not used directly here in the core plugin but is provided as a foundational building
         // block for both library and application plugins.
