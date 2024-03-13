c642c29b542c6f4d6a5e2a2b17b17e31abbcfdf0
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-03-13 15:08:21 -0400

    Update route name docs to refer to kebab-case transformation

--- frontend/kobweb-core/src/jsMain/kotlin/com/varabyte/kobweb/core/Page.kt
+++ frontend/kobweb-core/src/jsMain/kotlin/com/varabyte/kobweb/core/Page.kt
@@ -8,7 +8,7 @@ import androidx.compose.runtime.*
  * The page's filename will be used to generate its slug, e.g. "pages/account/Profile.kt" ->
  * "/account/profile".
  *
- * Additionally, the name transformation lowercases the route, e.g. "pages/WelcomeIntro.kt" -> "/welcomeintro"
+ * Additionally, the name is converted into kebab-case, e.g. "pages/WelcomeIntro.kt" -> "/welcome-intro"
  *
  * The name "index" is special, and if encountered, it will be understood that this is a default page given the current
  * URL. For example, "pages/blog/Index.kt" will be rendered when the user visits "/blog".
