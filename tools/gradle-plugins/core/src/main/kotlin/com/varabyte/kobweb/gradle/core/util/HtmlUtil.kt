ab4ceeb71d4b69629913e5c67d37fd05d2a22168
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-04-28 01:34:14 -0400

    Improve docs

--- tools/gradle-plugins/core/src/main/kotlin/com/varabyte/kobweb/gradle/core/util/HtmlUtil.kt
+++ tools/gradle-plugins/core/src/main/kotlin/com/varabyte/kobweb/gradle/core/util/HtmlUtil.kt
@@ -5,7 +5,7 @@ import kotlinx.html.TagConsumer
 import kotlinx.html.stream.createHTML
 
 object HtmlUtil {
-    // Generate the html nodes without the containing <head> tag
+    // Workaround for generating child nodes without the containing <head> tag
     // See: https://github.com/Kotlin/kotlinx.html/issues/228
     private inline fun <T, C : TagConsumer<T>> C.headFragment(crossinline block: HEAD.() -> Unit): T {
         HEAD(emptyMap(), this).block()
@@ -14,6 +14,12 @@ object HtmlUtil {
 
     // Use `xhtmlCompatible = true` to include a closing slash as currently kotlinx.html needs them when adding raw text.
     // See: https://github.com/Kotlin/kotlinx.html/issues/247
+    /**
+     * Serialize the child nodes created by [block], excluding the opening and closing `<head>` tag.
+     *
+     * This is useful when accumulating elements from several sources, which can then be wrapped altogether in a single
+     * `<head>` tag.
+     */
     fun serializeHeadContents(block: HEAD.() -> Unit): String =
         createHTML(prettyPrint = false, xhtmlCompatible = true).headFragment(block)
 }
