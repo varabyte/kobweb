d45226aa5161944be0c845e69ebf206a17fb251b
carlosgub <carlosgabrielugazburga@gmail.com>
2024-05-17 23:10:18 -0500

    Improve accesibility via adding content to the label

--- frontend/silk-widgets/src/jsMain/kotlin/com/varabyte/kobweb/silk/components/forms/Switch.kt
+++ frontend/silk-widgets/src/jsMain/kotlin/com/varabyte/kobweb/silk/components/forms/Switch.kt
@@ -28,7 +28,6 @@ import com.varabyte.kobweb.silk.theme.colors.palette.toPalette
 import org.jetbrains.compose.web.attributes.InputType
 import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.Label
-import org.jetbrains.compose.web.dom.Text
 import org.w3c.dom.HTMLElement
 
 object SwitchVars {
@@ -167,13 +166,11 @@ fun Switch(
     // Use a label so it intercepts clicks and passes them to the inner Input
     Label(
         attrs = SwitchStyle.toModifier(variant)
-            .fontSize(0.px)
             .then(size.toModifier())
             .then(shape.toModifier())
             .then(modifier)
             .toAttrs()
     ) {
-        Text("Switch")
         registerRefScope(ref)
         // We base Switch on a checkbox input for a11y + built-in input/keyboard support, but hide the checkbox itself
         // and render the switch separately. We do however allow it to be focused, which combined with the outer label
@@ -184,6 +181,8 @@ fun Switch(
             onValueChanged = { onCheckedChange(!checked) },
             variant = SwitchInputVariant,
             enabled = enabled,
+            modifier = Modifier
+                .ariaLabel("switch")
         )
         Box(
             SwitchTrackStyle.toModifier()
