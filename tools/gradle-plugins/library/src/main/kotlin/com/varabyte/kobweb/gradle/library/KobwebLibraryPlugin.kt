023572d191cecc426e023fdec85745a37675a2bd
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-05-23 13:40:33 -0400

    Add K2 migration task

--- tools/gradle-plugins/library/src/main/kotlin/com/varabyte/kobweb/gradle/library/KobwebLibraryPlugin.kt
+++ tools/gradle-plugins/library/src/main/kotlin/com/varabyte/kobweb/gradle/library/KobwebLibraryPlugin.kt
@@ -13,7 +13,7 @@ import com.varabyte.kobweb.gradle.core.ksp.applyKspPlugin
 import com.varabyte.kobweb.gradle.core.ksp.setKspMode
 import com.varabyte.kobweb.gradle.core.ksp.setupKspJs
 import com.varabyte.kobweb.gradle.core.ksp.setupKspJvm
-import com.varabyte.kobweb.gradle.core.registerMigrationTasks
+import com.varabyte.kobweb.gradle.core.registerKobwebMigrationTasks
 import com.varabyte.kobweb.gradle.core.util.generateModuleMetadataFor
 import com.varabyte.kobweb.gradle.library.extensions.createLibraryBlock
 import com.varabyte.kobweb.gradle.library.extensions.index
@@ -57,6 +57,6 @@ class KobwebLibraryPlugin : Plugin<Project> {
             project.generateModuleMetadataFor(jvmTarget)
         }
 
-        project.tasks.registerMigrationTasks()
+        project.registerKobwebMigrationTasks()
     }
 }
