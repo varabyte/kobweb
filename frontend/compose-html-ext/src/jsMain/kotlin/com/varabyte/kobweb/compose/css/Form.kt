bef645cf9f22fcb5e1cf8ad4dca411c16a067e89
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-06-02 16:59:19 -0400

    Fix `disabled` attribute implementation
    Fixes #546

--- frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Form.kt
+++ frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Form.kt
@@ -3,5 +3,7 @@ package com.varabyte.kobweb.compose.css
 import org.jetbrains.compose.web.attributes.AttrsScope
 
 fun AttrsScope<*>.disabled(value: Boolean = true) {
-    attr("disabled", value.toString())
+    if (value) {
+        attr("disabled", "")
+    }
 }
