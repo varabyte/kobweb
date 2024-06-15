455a68e622ed9857fff3e5c63657d5c0fa3d4969
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-06-14 20:08:59 -0400

    Apply spacing only along the axis of the layout

--- frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/style/Arrangement.kt
+++ frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/style/Arrangement.kt
@@ -19,8 +19,9 @@ internal const val KOBWEB_ARRANGE_TOP = "kobweb-arrange-top"
 internal val ArrangeSpacedByVar by StyleVariable<CSSLengthOrPercentageNumericValue>(prefix = "kobweb")
 
 internal fun KobwebComposeStyleSheet.initArrangeSpacedByStyle() {
-    ".kobweb-row.${KOBWEB_ARRANGE_SPACED_BY}" { gap(ArrangeSpacedByVar.value()) }
-    ".kobweb-col.${KOBWEB_ARRANGE_SPACED_BY}" { gap(ArrangeSpacedByVar.value()) }
+    // Apply spacing only along the axis of the layout, which is opposite the flow of the layout itself
+    ".kobweb-row.${KOBWEB_ARRANGE_SPACED_BY}" { columnGap(ArrangeSpacedByVar.value()) }
+    ".kobweb-col.${KOBWEB_ARRANGE_SPACED_BY}" { rowGap(ArrangeSpacedByVar.value()) }
 }
 
 fun Arrangement.Horizontal.toClassNames() = when (this) {
