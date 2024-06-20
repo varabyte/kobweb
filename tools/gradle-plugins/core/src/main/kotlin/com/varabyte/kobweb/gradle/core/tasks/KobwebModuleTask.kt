4188e84453daae069790c307b2a7a4475e0a75eb
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-06-20 00:36:49 -0400

    Pass explicit inputs to `kobwebExport`
    This is required for compatibility with gradle configuration cache.

--- tools/gradle-plugins/core/src/main/kotlin/com/varabyte/kobweb/gradle/core/tasks/KobwebModuleTask.kt
+++ tools/gradle-plugins/core/src/main/kotlin/com/varabyte/kobweb/gradle/core/tasks/KobwebModuleTask.kt
@@ -1,24 +1,19 @@
 package com.varabyte.kobweb.gradle.core.tasks
 
-import com.varabyte.kobweb.gradle.core.extensions.KobwebBlock
 import com.varabyte.kobweb.gradle.core.extensions.kobwebBlock
 import com.varabyte.kobweb.gradle.core.kmp.jsTarget
 import com.varabyte.kobweb.gradle.core.util.RootAndFile
 import com.varabyte.kobweb.gradle.core.util.getResourceFilesWithRoots
 import com.varabyte.kobweb.gradle.core.util.getSourceFilesWithRoots
-import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 
 /**
  * Base class for any task that needs to know about any source / resources related to Kobweb in the current module.
  */
 abstract class KobwebModuleTask(desc: String) : KobwebTask(desc) {
-    @get:Internal
-    val kobwebBlock: KobwebBlock = project.kobwebBlock
-
     @Internal
     fun getResourceFilesJsWithRoots(): Sequence<RootAndFile> = project.getResourceFilesWithRoots(project.jsTarget)
-        .filter { rootAndFile -> rootAndFile.relativeFile.invariantSeparatorsPath.startsWith("${getPublicPath()}/") }
+        .filter { rootAndFile -> rootAndFile.relativeFile.invariantSeparatorsPath.startsWith("$publicPath/") }
 
     @Internal
     fun getSourceFilesJsWithRoots(): Sequence<RootAndFile> = project.getSourceFilesWithRoots(project.jsTarget)
@@ -27,6 +22,5 @@ abstract class KobwebModuleTask(desc: String) : KobwebTask(desc) {
      * The path of public resources inside the project's resources folder, e.g. "public" ->
      * "src/jsMain/resources/public"
      */
-    @Input
-    fun getPublicPath(): String = kobwebBlock.publicPath.get()
+    private val publicPath = project.kobwebBlock.publicPath.get()
 }
