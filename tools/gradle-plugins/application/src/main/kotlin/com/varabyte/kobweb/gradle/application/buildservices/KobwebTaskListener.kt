daf60a638c85f9414e2c3116e0e95b851c73ab50
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-06-28 16:33:42 -0400

    Make `KobwebTaskListener` compatible with configuration cache

--- tools/gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/buildservices/KobwebTaskListener.kt
+++ tools/gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/buildservices/KobwebTaskListener.kt
@@ -1,23 +1,80 @@
 package com.varabyte.kobweb.gradle.application.buildservices
 
+import com.varabyte.kobweb.project.KobwebFolder
+import com.varabyte.kobweb.server.api.ServerRequest
+import com.varabyte.kobweb.server.api.ServerRequestsFile
+import com.varabyte.kobweb.server.api.ServerStateFile
 import org.gradle.api.services.BuildService
 import org.gradle.api.services.BuildServiceParameters
+import org.gradle.tooling.events.FailureResult
 import org.gradle.tooling.events.FinishEvent
 import org.gradle.tooling.events.OperationCompletionListener
-import java.util.concurrent.CopyOnWriteArrayList
+import java.io.File
 
 /**
  * A build service which listens to task states and communicates information to a running Kobweb server about them
  */
-abstract class KobwebTaskListener : BuildService<BuildServiceParameters.None>, OperationCompletionListener,
-    AutoCloseable {
-    val onFinishCallbacks = CopyOnWriteArrayList<(FinishEvent) -> Unit>()
+abstract class KobwebTaskListener : BuildService<KobwebTaskListener.Parameters>, OperationCompletionListener {
+    interface Parameters : BuildServiceParameters {
+        var isKobwebStartBuild: Boolean
+        var kobwebStartTaskName: String
 
-    override fun onFinish(event: FinishEvent) {
-        onFinishCallbacks.forEach { it.invoke(event) }
+        // take a `File` instead of a `Path` as parameters must be serializable
+        var kobwebFolderFile: File
     }
 
-    override fun close() {
-        onFinishCallbacks.clear()
+    val kobwebFolder = KobwebFolder.inPath(parameters.kobwebFolderFile.toPath())!!
+    var isBuilding = false
+
+    // NOTE: When a project uses an `includeBuild` for a plugin that needs to be built only after the kobweb application
+    // plugin is applied, the tasks for building that plugin will be caught by this TaskListener, and thus the code
+    // below which reads the server `state.yaml` file will run during the project's configuration phase. This will
+    // cause the file to be considered a configuration cache input, which is not ideal, as changes to the file will
+    // invalidate the configuration cache, even though there is no need to do so. To the best of our knowledge, there
+    // are no Gradle APIs to avoid detecting the file as a configuration cache input. However, one workaround is to
+    // ensure the `includeBuild` plugin(s) are built earlier than project configuration by including them in the root
+    // buildscript (e.g. `my-project/build.gradle.kts`) `plugins {}` declaration:
+    // plugins {
+    //     alias(libs.plugins.kotlin.multiplatform) apply false
+    //     id("<includeBuild plugin>") apply false
+    // }
+    // Warning: The above approach may cause unnecessary builds depending on project setup, use at your own discretion.
+    var isServerRunning = run {
+        val stateFile = ServerStateFile(kobwebFolder)
+        stateFile.content?.let { serverState ->
+            ProcessHandle.of(serverState.pid).isPresent
+        }
+    } ?: false
+
+    override fun onFinish(event: FinishEvent) {
+        if (!parameters.isKobwebStartBuild) return
+
+        val taskName = event.descriptor.name.substringAfterLast(":")
+        val serverRequestsFile = ServerRequestsFile(kobwebFolder)
+        val taskFailed = event.result is FailureResult
+
+        if (isServerRunning) {
+            if (taskFailed) {
+                serverRequestsFile.enqueueRequest(
+                    ServerRequest.SetStatus(
+                        "Failed.",
+                        isError = true,
+                        timeoutMs = 500
+                    )
+                )
+            } else {
+                if (taskName == parameters.kobwebStartTaskName) {
+                    serverRequestsFile.enqueueRequest(ServerRequest.ClearStatus())
+                    serverRequestsFile.enqueueRequest(ServerRequest.IncrementVersion())
+                } else if (!isBuilding) {
+                    serverRequestsFile.enqueueRequest(ServerRequest.SetStatus("Building..."))
+                    isBuilding = true
+                }
+            }
+        } else {
+            if (!taskFailed && taskName == parameters.kobwebStartTaskName) {
+                isServerRunning = true
+            }
+        }
     }
 }
