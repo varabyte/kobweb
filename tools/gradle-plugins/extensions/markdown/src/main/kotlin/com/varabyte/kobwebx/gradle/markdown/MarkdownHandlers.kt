490182fdb8c91f05b8d588cf96c3c07591925b2c
David Herman <bitspittle@gmail.com>
2024-09-06 10:53:06 -0700

    Nit: Add typealias pointing to new MarkdownHandlers location
    so we can give any users using the old location a warning instead
    of a compile error, although we expect the number of such users
    to be extremely small.

---
+++ tools/gradle-plugins/extensions/markdown/src/main/kotlin/com/varabyte/kobwebx/gradle/markdown/MarkdownHandlers.kt
@@ -0,0 +1,6 @@
+@file:Suppress("LeakingThis") // Following official Gradle guidance
+
+package com.varabyte.kobwebx.gradle.markdown
+
+@Deprecated("`MarkdownHandlers` has moved to `com.varabyte.kobwebx.gradle.markdown.handlers.MarkdownHandlers` so please update any imports at your earliest convenience.")
+typealias MarkdownHandlers = com.varabyte.kobwebx.gradle.markdown.handlers.MarkdownHandlers
