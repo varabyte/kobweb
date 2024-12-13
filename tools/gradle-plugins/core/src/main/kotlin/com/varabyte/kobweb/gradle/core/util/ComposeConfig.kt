76ba4af1c4a4849cfd9522bc8fc2bec4da27c076
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-12-13 16:48:52 -0500

    exceptions

--- tools/gradle-plugins/core/src/main/kotlin/com/varabyte/kobweb/gradle/core/util/ComposeConfig.kt
+++ tools/gradle-plugins/core/src/main/kotlin/com/varabyte/kobweb/gradle/core/util/ComposeConfig.kt
@@ -7,6 +7,12 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 
 internal const val KOBWEB_CONFIGURE_COMPOSE_COMPILER = "kobweb.configureComposeCompiler"
 
+private const val FAILURE_EXPLANATION = "w: The Kobweb plugin failed to configure the compose compiler plugin. " +
+    "This prevents it from applying settings which may improve the size / performance of your site."
+
+private const val DISABLE_INSTRUCTIONS = "To disable Kobweb's configuration of the compose compiler plugin, " +
+    "add `$KOBWEB_CONFIGURE_COMPOSE_COMPILER=false` to your project's `gradle.properties` file."
+
 fun Project.configureComposeCompiler() {
     pluginManager.withPlugin("org.jetbrains.kotlin.plugin.compose") {
         // As described in https://github.com/gradle/gradle/issues/23792#issuecomment-1431053503,
@@ -17,14 +23,12 @@ fun Project.configureComposeCompiler() {
             when (e) {
                 is TypeNotPresentException, is ClassNotFoundException, is NoClassDefFoundError -> {
                     project.logger.warn(
-                        "w: The Kobweb plugin failed to configure the compose compiler plugin. " +
-                            "This prevents it from applying settings which may improve the size / performance of your site. " +
-                            "To fix this, ensure that the plugin `org.jetbrains.kotlin.plugin.compose` is loaded at the same " +
+                        FAILURE_EXPLANATION +
+                            " To fix this, ensure that the plugin `org.jetbrains.kotlin.plugin.compose` is loaded at the same " +
                             "level as the Kobweb plugin. For example, if you load the Kobweb plugin in the project root " +
                             "with `alias(libs.plugins.kobweb.application) apply false`, you should also load the compose " +
                             "plugin there (e.g. with `alias(libs.plugins.compose.compiler) apply false`). " +
-                            "To disable Kobweb's configuration of the compose compiler plugin, " +
-                            "add `$KOBWEB_CONFIGURE_COMPOSE_COMPILER=false` to your project's `gradle.properties` file."
+                            DISABLE_INSTRUCTIONS
                     )
                     null
                 }
@@ -32,11 +36,16 @@ fun Project.configureComposeCompiler() {
                 else -> throw e
             }
         } ?: return@withPlugin
-        // Trace markers are "pure overhead" for the JS target & needlessly increase the bundle size, but
-        // must be explicitly disabled until https://youtrack.jetbrains.com/issue/KT-69900 is resolved.
-        composeCompilerExt.includeTraceMarkers.set(false)
-        // Unlike standard multiplatform applications, Kobweb projects only use compose on the JS frontend,
-        // so we disable the compiler plugin for other targets.
-        composeCompilerExt.targetKotlinPlatforms.set(setOf(KotlinPlatformType.js))
+        try {
+            // Trace markers are "pure overhead" for the JS target & needlessly increase the bundle size, but
+            // must be explicitly disabled until https://youtrack.jetbrains.com/issue/KT-69900 is resolved.
+            composeCompilerExt.includeTraceMarkers.set(false)
+            // Unlike standard multiplatform applications, Kobweb projects only use compose on the JS frontend,
+            // so we disable the compiler plugin for other targets.
+            composeCompilerExt.targetKotlinPlatforms.set(setOf(KotlinPlatformType.js))
+        } catch (_: NoSuchMethodError) {
+            // Handle potential breaking changes in the compose plugin API
+            project.logger.warn("$FAILURE_EXPLANATION $DISABLE_INSTRUCTIONS")
+        }
     }
 }
