28ec2e7bf90b1a706d94145459e4b03f01045048
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2025-01-02 09:16:33 -0800

    Simplify kobweb-gradle-portal convention plugin

--- gradle/build-logic/conventions/src/main/kotlin/kobweb-gradle-portal.gradle.kts
+++ gradle/build-logic/conventions/src/main/kotlin/kobweb-gradle-portal.gradle.kts
@@ -1,27 +1,15 @@
-import org.gradle.kotlin.dsl.findByType
-import org.gradle.plugin.devel.GradlePluginDevelopmentExtension
-
 plugins {
     id("com.gradle.plugin-publish")
 }
 
-val gradlePluginExtension = extensions.findByType<GradlePluginDevelopmentExtension>()
-gradlePluginExtension?.apply {
+extensions.configure<GradlePluginDevelopmentExtension> {
     website.convention("https://kobweb.varabyte.com/")
     vcsUrl.convention("https://github.com/varabyte/kobweb.git")
 
-    afterEvaluate {
-        gradlePluginExtension.plugins.forEach { pluginDeclaration ->
-            @Suppress("UnstableApiUsage")
-            pluginDeclaration.tags.addAll(
-                "kotlin",
-                "kobweb",
-                "web",
-            )
-        }
+    plugins.configureEach {
+        @Suppress("UnstableApiUsage")
+        tags.addAll("kotlin", "kobweb", "web")
     }
-} ?: run {
-    logger.error("Kobweb gradle portal plugin applied but no `gradlePlugin` block was found.")
 }
 
 // Snapshots are not allowed in the Gradle plugin portal. Let's play it safe and disable the task in this case.
@@ -37,7 +25,7 @@ tasks.named("publishPlugins") {
     onlyIf {
         val isSnapshot = isSnapshotProvider.get()
         if (isSnapshot) {
-            project.logger.warn("Disabling task '${fullTaskNameProvider.get()}' because version is a snapshot (${versionProvider.get()}).")
+            logger.warn("Disabling task '${fullTaskNameProvider.get()}' because version is a snapshot (${versionProvider.get()}).")
         }
 
         !isSnapshot
