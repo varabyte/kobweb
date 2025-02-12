fcfad78afa2c8f464930177b89b97a8aa45ef0a6
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2025-02-12 00:24:19 -0500

    Update Kotlin to 2.1.20

--- gradle/build-logic/conventions/src/main/kotlin/kobweb-compose.gradle.kts
+++ gradle/build-logic/conventions/src/main/kotlin/kobweb-compose.gradle.kts
@@ -6,4 +6,7 @@ composeCompiler {
     // Trace markers are "pure overhead" for the JS target & needlessly increase the bundle size, but
     // must be explicitly disabled until https://youtrack.jetbrains.com/issue/KT-69900 is resolved
     includeTraceMarkers = false
+    // As per its KDoc, source information is meant to be removed in production builds, but is explicitly
+    // disabled since Webpack does not remove it.
+    includeSourceInformation = false
 }
