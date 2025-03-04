a99b758cba7a1ac442267651453f499c292ac094
David Herman <bitspittle@gmail.com>
2025-03-04 11:41:44 -0800

    Add docs and ability to exclude dynamic routes in KobwebListRoutesTask

--- tools/gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/tasks/KobwebListRoutesTask.kt
+++ tools/gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/tasks/KobwebListRoutesTask.kt
@@ -2,23 +2,49 @@ package com.varabyte.kobweb.gradle.application.tasks
 
 import com.varabyte.kobweb.gradle.core.tasks.KobwebTask
 import com.varabyte.kobweb.project.frontend.AppFrontendData
+import com.varabyte.kobweb.project.frontend.PageEntry
 import kotlinx.serialization.json.Json
 import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
 
-abstract class KobwebListRoutesTask : KobwebTask("Enumerate all routes for your site managed by Kobweb") {
+
+/**
+ * Print all routes for the current site to the console.
+ *
+ * This includes dynamic routes too, e.g. "/users/{user}/posts/{post}". If you'd like to filter those out, add the
+ * following code to your build script:
+ *
+ * ```
+ * tasks.named<KobwebListRoutesTask>("kobwebListRoutes").configure {
+ *     excludeDynamicRoutes.set(true)
+ * }
+ * ```
+ */
+abstract class KobwebListRoutesTask : KobwebTask("Enumerate all routes for your site managed by Kobweb, printing them out to the console") {
     @get:InputFile
     abstract val appDataFile: RegularFileProperty
 
+    @get:Input
+    @get:Optional
+    abstract val excludeDynamicRoutes: Property<Boolean>
+
     @TaskAction
     fun execute() {
-        val pageEntries = Json.decodeFromString<AppFrontendData>(appDataFile.asFile.get().readText()).frontendData.pages
-        if (pageEntries.isNotEmpty()) {
+        val excludeDynamicRoutes = excludeDynamicRoutes.getOrElse(false)
+        val routes = Json.decodeFromString<AppFrontendData>(appDataFile.get().asFile.readText()).frontendData.pages
+            .asSequence()
+            .map { it.route }
+            .filter { !(excludeDynamicRoutes && it.contains('{')) }
+            .sorted()
+            .toList()
+
+        if (routes.isNotEmpty()) {
             println("Your site defines the following routes:")
-            pageEntries.map { it.route }.sorted().forEach { route ->
-                println("  $route")
-            }
+            routes.forEach { route -> println("  $route") }
         } else {
             println("No routes have been defined in your project yet.")
         }
