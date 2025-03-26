eda660316fc048a9b20a9c6d5735a632712dbdf9
David Herman <bitspittle@gmail.com>
2025-03-26 11:11:11 -0700

    Add support for resolving "." as a package shortcut
    Now:
    
    "com.example" + ".nested" -> "com.example.nested"
    "com.example" + "absolute" -> "absolute"
    "com.example" + "." -> "com.example"

--- tools/processor-common/src/main/kotlin/com/varabyte/kobweb/project/common/PackageUtils.kt
+++ tools/processor-common/src/main/kotlin/com/varabyte/kobweb/project/common/PackageUtils.kt
@@ -4,9 +4,14 @@ object PackageUtils {
     /**
      * Given a value that is potentially a package shortcut (e.g. ".pages"), get its fully qualified name,
      * e.g. ".pages" -> "org.example.pages"
+     *
+     * If it is an absolute package (e.g. "org.example.pages"), it will be returned as-is.
+     *
+     * Finally, if the package is set to "." by itself, that will just return the group.
      */
     fun resolvePackageShortcut(group: String, pkg: String): String {
         return when {
+            pkg == "." -> group
             pkg.startsWith('.') -> "$group$pkg"
             else -> pkg
         }
