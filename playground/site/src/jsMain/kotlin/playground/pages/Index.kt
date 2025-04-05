3e620f030a7adccca1321b824aa3cb739c9f6088
David Herman <bitspittle@gmail.com>
2025-04-04 21:43:38 -0700

    Add color mode style tests

--- playground/site/src/jsMain/kotlin/playground/pages/Index.kt
+++ playground/site/src/jsMain/kotlin/playground/pages/Index.kt
@@ -1,20 +1,100 @@
 package playground.pages
 
 import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.css.*
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.thenIf
 import com.varabyte.kobweb.core.Page
-import com.varabyte.kobweb.silk.components.forms.TextInput
-import org.jetbrains.compose.web.dom.P
+import com.varabyte.kobweb.silk.components.layout.Surface
+import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.base
+import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.style.extendedByBase
+import com.varabyte.kobweb.silk.style.toAttrs
+import com.varabyte.kobweb.silk.style.toModifier
+import com.varabyte.kobweb.silk.theme.colors.ColorMode
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.Div
 import org.jetbrains.compose.web.dom.Text
 import playground.components.layouts.PageLayout
+
+val TestStyle = CssStyle.base {
+    Modifier
+        .display(DisplayStyle.Block)
+        .padding(4.px)
+        .background(if (colorMode.isLight) Colors.Red else Colors.Green)
+        .thenIf(colorMode.isLight, Modifier.margin { left(1.cssRem) })
+}
+
+val MyStyle = CssStyle {
+    base {
+        Modifier
+            .height(100.px)
+            .backgroundColor(Colors.Red)
+            .width(if (colorMode.isLight) 50.px else 60.px)
+    }
+    Breakpoint.MD {
+        Modifier.width(300.px)
+    }
+}
+
+val MyStyle2 = CssStyle {
+    base {
+        Modifier
+            .height(100.px)
+            .backgroundColor(Colors.Red)
+            .width(300.px)
+    }
+    Breakpoint.MD {
+        Modifier
+            .width(if (colorMode.isLight) 50.px else 60.px)
+    }
+}
+
+val Style1 = CssStyle.base {
+    Modifier.backgroundColor(Colors.Red).width(if (colorMode.isLight) 50.px else 60.px)
+}
+
+val Style2 = Style1.extendedByBase {
+    Modifier.width(500.px)
+}
 
 @Page
 @Composable
 fun HomePage() {
-    PageLayout("Welcome to Kobweb!") {
-        Text("Please enter your name")
-        var name by remember { mutableStateOf("") }
-        TextInput(name, onTextChange = { name = it })
-        P()
-        Text("Hello ${name.takeIf { it.isNotBlank() } ?: "World"}!")
+    PageLayout("asf") {
+        Div(MyStyle.toAttrs()) { Text("I should be thick") }
+        Surface(MyStyle.toModifier(), colorModeOverride = ColorMode.current.opposite) { Text("I should be thick") }
+        Div(MyStyle2.toAttrs()) { Text("I should be thin") }
+        Surface(MyStyle2.toModifier(), colorModeOverride = ColorMode.current.opposite) { Text("I should be thin") }
+        Div(Style2.toAttrs()) { Text("I should be thick") }
+        Surface(Style2.toModifier(), colorModeOverride = ColorMode.current.opposite) { Text("I should be thick") }
+
+        Surface(TestStyle.toModifier().textAlign(TextAlign.Start), colorModeOverride = ColorMode.current.opposite) {
+            Text("Local theme: ${ColorMode.current}")
+            Div(TestStyle.toAttrs()) { Text("Div(TestStyle.toAttrs())") }
+            Surface(TestStyle.toModifier(), colorModeOverride = ColorMode.current.opposite) {
+                Text("Local theme: ${ColorMode.current}")
+                Div(TestStyle.toAttrs()) { Text("Div(TestStyle.toAttrs())") }
+                Surface(TestStyle.toModifier(), colorModeOverride = ColorMode.current.opposite) {
+                    Text("Local theme: ${ColorMode.current}")
+                    Div(TestStyle.toAttrs()) { Text("Div(TestStyle.toAttrs())") }
+                    Surface(TestStyle.toModifier(), colorModeOverride = ColorMode.current.opposite) {
+                        Text("Local theme: ${ColorMode.current}")
+                        Div(TestStyle.toAttrs()) { Text("Div(TestStyle.toAttrs())") }
+                        Surface(TestStyle.toModifier(), colorModeOverride = ColorMode.current.opposite) {
+                            Text("Local theme: ${ColorMode.current}")
+                            Div(TestStyle.toAttrs()) { Text("Div(TestStyle.toAttrs())") }
+                            Surface(TestStyle.toModifier(), colorModeOverride = ColorMode.current.opposite) {
+                                Text("Local theme: ${ColorMode.current}")
+                                Div(TestStyle.toAttrs()) { Text("Div(TestStyle.toAttrs())") }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
