ae017cfd994cdc3df05596c810b10259ebf2f9f2
David Herman <bitspittle@gmail.com>
2025-04-25 16:22:44 -0700

    Add warnings if any projects are missing from aggregate-docs

--- tools/aggregate-docs/build.gradle.kts
+++ tools/aggregate-docs/build.gradle.kts
@@ -9,39 +9,61 @@ dokka {
     moduleName = "kobweb"
 }
 
-dependencies {
-    // (*) indicates dependencies we explicitly excluded as their API is meant to be internal only; no need to make the
-    // docs more complicated by including them.
+val includedProjects = setOf(
+    projects.common.frameworkAnnotations,
+    projects.common.kobwebCommon,
+    projects.common.kobwebSerialization,
+    projects.common.kobwebxFrontmatter,
+    projects.common.kobwebxSerializationKotlinx,
+    projects.frontend.kobwebCore,
+    projects.frontend.kobwebCompose,
+    projects.frontend.kobwebSilk,
+    projects.frontend.kobwebWorker,
+    projects.frontend.kobwebWorkerInterface,
+    projects.frontend.silkFoundation,
+    projects.frontend.silkWidgets,
+    projects.frontend.silkWidgetsKobweb,
+    projects.frontend.silkIconsFa,
+    projects.frontend.silkIconsMdi,
+    projects.frontend.kobwebxMarkdown,
+    projects.frontend.composeHtmlExt,
+    projects.frontend.browserExt,
+    projects.backend.kobwebApi,
+    projects.backend.serverPlugin,
+    projects.tools.gradlePlugins.core,
+    projects.tools.gradlePlugins.library,
+    projects.tools.gradlePlugins.application,
+    projects.tools.gradlePlugins.worker,
+    projects.tools.gradlePlugins.extensions.markdown,
+)
 
-    // dokka(projects.common.clientServerInternal) (*)
-    dokka(projects.common.frameworkAnnotations)
-    dokka(projects.common.kobwebCommon)
-    dokka(projects.common.kobwebSerialization)
-    dokka(projects.common.kobwebxFrontmatter)
-    dokka(projects.common.kobwebxSerializationKotlinx)
-    dokka(projects.frontend.kobwebCore)
-    dokka(projects.frontend.kobwebCompose)
-    dokka(projects.frontend.kobwebSilk)
-    dokka(projects.frontend.kobwebWorker)
-    dokka(projects.frontend.kobwebWorkerInterface)
-    dokka(projects.frontend.silkFoundation)
-    dokka(projects.frontend.silkWidgets)
-    dokka(projects.frontend.silkWidgetsKobweb)
-    dokka(projects.frontend.silkIconsFa)
-    dokka(projects.frontend.silkIconsMdi)
-    dokka(projects.frontend.kobwebxMarkdown)
-    dokka(projects.frontend.composeHtmlExt)
-    dokka(projects.frontend.browserExt)
-    dokka(projects.backend.kobwebApi)
-    dokka(projects.backend.serverPlugin)
-    // dokka(projects.backend.server) (*)
-    dokka(projects.tools.gradlePlugins.core)
-    dokka(projects.tools.gradlePlugins.library)
-    dokka(projects.tools.gradlePlugins.application)
-    dokka(projects.tools.gradlePlugins.worker)
-    dokka(projects.tools.gradlePlugins.extensions.markdown)
-    // dokka(projects.tools.ksp.siteProcessors) (*)
-    // dokka(projects.tools.ksp.workerProcessor) (*)
-    // dokka(projects.tools.ksp.kspExt) (*)
-    // dokka(projects.tools.processorCommon) (*)
+val excludedProjects = setOf(
+    projects.common.clientServerInternal,
+    projects.tools.ksp.siteProcessors,
+    projects.tools.ksp.workerProcessor,
+    projects.tools.ksp.kspExt,
+    projects.tools.aggregateDocs,
+    projects.tools.processorCommon,
+)
+
+dependencies {
+    includedProjects.forEach { project -> dokka(project) }
 }
+
+// Warn if we ever add a new module and forget to add a dokka entry for it (which is too easy to do).
+// The Gradle team may decide to prevent this code from working in the future; if that happens, then I guess we'll just
+// have to remove it.
+gradle.projectsEvaluated {
+    val combinedProjects = (includedProjects + excludedProjects).toSet()
+    val referencedPaths = combinedProjects.map { it.path }.toSet()
+    val dokkaId = libs.plugins.dokka.get().pluginId
+
+    rootProject.subprojects {
+        if (referencedPaths.contains(project.path) && !project.plugins.hasPlugin(dokkaId)) {
+            logger.warn("w: Project ${project.path} doesn't apply the dokka plugin and doesn't have to be included in `aggregate-docs`")
+        } else if (!referencedPaths.contains(project.path) && project.plugins.hasPlugin(dokkaId)) {
+            logger.warn("w: Project ${project.path} has no dokka entry. Please make an explicit choice to include or exclude it in `aggregate-docs`")
+        }
+    }
+}
+
