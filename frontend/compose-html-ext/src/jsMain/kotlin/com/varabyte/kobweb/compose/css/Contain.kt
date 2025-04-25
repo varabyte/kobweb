f6fe85b5b413b534347c6680796a2da6edce02fb
Sandeep Karanam <sandeepjak2007@gmail.com>
2025-04-25 09:25:37 +0530

    Contain CSS Property (In Progress)

--- frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Contain.kt
+++ frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Contain.kt
@@ -3,30 +3,36 @@ package com.varabyte.kobweb.compose.css
 import org.jetbrains.compose.web.css.*
 
 // https://developer.mozilla.org/en-US/docs/Web/CSS/contain
-class Contain private constructor(private val value: String) : StylePropertyValue {
+sealed class Contain(private val value: String) : StylePropertyValue {
+
+    override fun toString() = value
+
+    sealed class SingleValue(value: String) : Contain(value)
+    class RepeatableValue internal constructor(value: String) : SingleValue(value)
+    private class Keyword(value: String) : SingleValue(value)
+    private class TwoValue(width: RepeatableValue, height: RepeatableValue) : Contain("$width $height")
+    private class AutoSingleValue(value: String) : Contain("auto $value")
 
     companion object {
         /* Keyword values */
-        val None get() = Contain("none")
-        val Strict get() = Contain("strict")
-        val Content get() = Contain("content")
-        val Size get() = Contain("size")
-        val InlineSize get() = Contain("inline-size")
-        val Layout get() = Contain("layout")
-        val Style get() = Contain("style")
-        val Paint get() = Contain("paint")
+        val None get() = RepeatableValue("none")
+        val Strict get() = RepeatableValue("strict")
+        val Content get() = RepeatableValue("content")
+        val Size get() = RepeatableValue("size")
+        val InlineSize get() = RepeatableValue("inline-size")
+        val Layout get() = RepeatableValue("layout")
+        val Style get() = RepeatableValue("style")
+        val Paint get() = RepeatableValue("paint")
 
         /* Multiple keywords */
-        fun of(firstValue: String, secondValue: String) = Contain("$firstValue $secondValue")
-        fun of(firstValue: String, secondValue: String, thirdValue: String) =
-            Contain("$firstValue $secondValue $thirdValue")
+        fun of(vararg value: RepeatableValue): Contain = Keyword("$value")
 
         /* Global values */
-        val Inherit get() = Contain("inherit")
-        val Initial get() = Contain("initial")
-        val Revert get() = Contain("revert")
-        val RevertLayer get() = Contain("revert-layer")
-        val Unset get() = Contain("unset")
+        val Inherit: Contain get() = Keyword("inherit")
+        val Initial: Contain get() = Keyword("initial")
+        val Revert: Contain get() = Keyword("revert")
+        val RevertLayer: Contain get() = Keyword("revert-layer")
+        val Unset: Contain get() = Keyword("unset")
     }
 }
 
@@ -34,150 +40,26 @@ fun StyleScope.contain(contain: Contain) {
     property("contain", contain)
 }
 
-// https://developer.mozilla.org/en-US/docs/Web/CSS/contain-intrinsic-block-size
-class ContainIntrinsicBlockSize private constructor(private val value: String) : StylePropertyValue {
-
-    companion object {
-        /* Keyword Values */
-        val None get() = ContainIntrinsicBlockSize("none")
-
-        /* auto <length>  & <length> values */
-        fun of(length: CSSLengthNumericValue, isAuto: Boolean = false) = if (!isAuto) {
-            ContainIntrinsicBlockSize("auto $length")
-        } else {
-            ContainIntrinsicBlockSize("$length")
-        }
-
-        /* Global values */
-        val Inherit get() = ContainIntrinsicBlockSize("inherit")
-        val Initial get() = ContainIntrinsicBlockSize("initial")
-        val Revert get() = ContainIntrinsicBlockSize("revert")
-        val RevertLayer get() = ContainIntrinsicBlockSize("revert-layer")
-        val Unset get() = ContainIntrinsicBlockSize("unset")
-
-    }
+fun StyleScope.contain(vararg value: Contain.SingleValue) {
+    property("contain", value.joinToString(" "))
 }
 
-fun StyleScope.containIntrinsicBlockSize(containIntrinsicBlockSize: ContainIntrinsicBlockSize) {
+fun StyleScope.containIntrinsicBlockSize(containIntrinsicBlockSize: Contain.SingleValue) {
     property("contain-intrinsic-block-size", containIntrinsicBlockSize)
 }
 
-// https://developer.mozilla.org/en-US/docs/Web/CSS/contain-intrinsic-height
-class ContainIntrinsicHeight private constructor(private val value: String) : StylePropertyValue {
-
-    companion object {
-        /* Keyword Values */
-        val None get() = ContainIntrinsicHeight("none")
-
-        /* auto <length>  & <length> values */
-        fun of(length: CSSLengthNumericValue, isAuto: Boolean = false) = if (!isAuto) {
-            ContainIntrinsicHeight("auto $length")
-        } else {
-            ContainIntrinsicHeight("$length")
-        }
-
-        /* Global values */
-        val Inherit get() = ContainIntrinsicHeight("inherit")
-        val Initial get() = ContainIntrinsicHeight("initial")
-        val Revert get() = ContainIntrinsicHeight("revert")
-        val RevertLayer get() = ContainIntrinsicHeight("revert-layer")
-        val Unset get() = ContainIntrinsicHeight("unset")
-
-    }
-}
-
-fun StyleScope.containIntrinsicHeight(containIntrinsicHeight: ContainIntrinsicHeight) {
-    property("contain-intrinsic-height", containIntrinsicHeight)
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/CSS/contain-intrinsic-inline-size
-class ContainIntrinsicInlineSize private constructor(private val value: String) : StylePropertyValue {
-
-    companion object {
-        /* Keyword Values */
-        val None get() = ContainIntrinsicInlineSize("none")
-
-        /* auto <length>  & <length> values */
-        fun of(length: CSSLengthNumericValue, isAuto: Boolean = false) = if (!isAuto) {
-            ContainIntrinsicInlineSize("auto $length")
-        } else {
-            ContainIntrinsicInlineSize("$length")
-        }
-
-        /* Global values */
-        val Inherit get() = ContainIntrinsicInlineSize("inherit")
-        val Initial get() = ContainIntrinsicInlineSize("initial")
-        val Revert get() = ContainIntrinsicInlineSize("revert")
-        val RevertLayer get() = ContainIntrinsicInlineSize("revert-layer")
-        val Unset get() = ContainIntrinsicInlineSize("unset")
-
-    }
-}
-
-fun StyleScope.containIntrinsicInlineSize(containIntrinsicInlineSize: ContainIntrinsicInlineSize) {
+fun StyleScope.containIntrinsicInlineSize(containIntrinsicInlineSize: Contain.SingleValue) {
     property("contain-intrinsic-inline-size", containIntrinsicInlineSize)
 }
 
-// https://developer.mozilla.org/en-US/docs/Web/CSS/contain-intrinsic-size
-class ContainIntrinsicSize private constructor(private val value: String) : StylePropertyValue {
-
-    companion object {
-        /* Keyword Values */
-        val None get() = ContainIntrinsicSize("none")
-
-        /* auto <length>  & <length> values */
-        fun of(length: CSSLengthNumericValue, isAuto: Boolean = false) = if (isAuto) {
-            ContainIntrinsicSize("auto $length")
-        } else {
-            ContainIntrinsicSize("$length")
-        }
-
-        /* width | height & auto width | auto height */
-        fun of(width: CSSLengthNumericValue, height: CSSLengthNumericValue, isAuto: Boolean = false) = if (isAuto) {
-            ContainIntrinsicSize("auto $width auto $height")
-        } else {
-            ContainIntrinsicSize("$width $height")
-        }
-
-
-        /* Global values */
-        val Inherit get() = ContainIntrinsicSize("inherit")
-        val Initial get() = ContainIntrinsicSize("initial")
-        val Revert get() = ContainIntrinsicSize("revert")
-        val RevertLayer get() = ContainIntrinsicSize("revert-layer")
-        val Unset get() = ContainIntrinsicSize("unset")
-
-    }
-}
-
-fun StyleScope.containIntrinsicSize(containIntrinsicSize: ContainIntrinsicSize) {
+fun StyleScope.containIntrinsicSize(containIntrinsicSize: Contain.SingleValue) {
     property("contain-intrinsic-size", containIntrinsicSize)
 }
 
-// https://developer.mozilla.org/en-US/docs/Web/CSS/contain-intrinsic-width
-class ContainIntrinsicWidth private constructor(private val value: String) : StylePropertyValue {
-
-    companion object {
-        /* Keyword Values */
-        val None get() = ContainIntrinsicWidth("none")
-
-        /* auto <length>  & <length> values */
-        fun of(length: CSSLengthNumericValue, isAuto: Boolean = false) = if (!isAuto) {
-            ContainIntrinsicWidth("auto $length")
-        } else {
-            ContainIntrinsicWidth("$length")
-        }
-
-        /* Global values */
-        val Inherit get() = ContainIntrinsicWidth("inherit")
-        val Initial get() = ContainIntrinsicWidth("initial")
-        val Revert get() = ContainIntrinsicWidth("revert")
-        val RevertLayer get() = ContainIntrinsicWidth("revert-layer")
-        val Unset get() = ContainIntrinsicWidth("unset")
-
-    }
+fun StyleScope.containIntrinsicWidth(containIntrinsicWidth: Contain.SingleValue) {
+    property("contain-intrinsic-width", containIntrinsicWidth)
 }
 
-fun StyleScope.containIntrinsicWidth(containIntrinsicWidth: ContainIntrinsicWidth) {
-    property("contain-intrinsic-width", containIntrinsicWidth)
+fun StyleScope.containIntrinsicHeight(containIntrinsicHeight: Contain.SingleValue) {
+    property("contain-intrinsic-height", containIntrinsicHeight)
 }
