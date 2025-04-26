a5fee1e2da09ea7eca22fd7df152727a75f16de4
David Herman <bitspittle@gmail.com>
2025-04-25 21:33:23 -0700

    Update and simplify the `@Layout` annotation docs
    The previous docs were both very long in the tooth AND out of date.

--- frontend/kobweb-core/src/jsMain/kotlin/com/varabyte/kobweb/core/layout/Layout.kt
+++ frontend/kobweb-core/src/jsMain/kotlin/com/varabyte/kobweb/core/layout/Layout.kt
@@ -1,27 +1,46 @@
 package com.varabyte.kobweb.core.layout
 
 import androidx.compose.runtime.*
+import com.varabyte.kobweb.core.Page
 
 /**
- * An annotation which declares that a [Composable] function is one that should be wrapped by a parent layout.
+ * An annotation which declares that a [Composable] function is either a layout or a page with a layout.
  *
- * A `Layout` annotation takes a path to a composable method which is designed to serve as the parent layout for this
- * method. For example, a declaration might look like `@Layout(".components.layout.ExampleLayout")`.
+ * ## Declaring layouts
  *
- * This target layout method may optionally accept a `ctx: PageContext` argument and must end with a
- * `content: @Composable () -> Unit` lambda parameter.
+ * A `Layout` annotation can be used to tag a method as one that provides a layout, which is high-level UI scaffolding
+ * that is sharable across many pages.
  *
- * The motivation behind this feature is it allows us to convert pages from this:
+ * A layout method may contain an optional first parameter which is a `PageContext` and must end with a
+ * `@Composable () -> Unit` content callback:
+ *
  * ```
- * @Page
+ * // Note: You can omit `ctx: PageContext` if you don't need it
+ * @Layout
  * @Composable
- * fun BlogPage() {
- *   BlogLayout {
- *     /* ... */
- *   }
+ * fun PageLayout(ctx: PageContext, content: @Composable () -> Unit) {
+ *    /*...*/
+ *    content()
+ *    /*...*/
  * }
  * ```
- * to this:
+ *
+ * A layout may also be nested within another layout. In that case, the `@Layout` annotation should contain a path to
+ * the other layout:
+ *
+ * ```
+ * @Layout(".components.layouts.PageLayout")
+ * @Composable
+ * fun NestedLayout(content: @Composable () -> Unit) {
+ *    /*...*/
+ * }
+ * ```
+ *
+ * ## Setting a page's layout
+ *
+ * A `Layout` annotation can also be added to a [Page] method, which in that case would indicate that this page is
+ * nested inside that layout.
+ *
  * ```
  * @Page
  * @Layout(".components.layouts.BlogLayout")
@@ -30,98 +49,16 @@ import androidx.compose.runtime.*
  *   /* ... */
  * }
  * ```
- * which, in addition to reducing one level of nesting, plays nicer with Compose by correcting the composable hierarchy
- * to what it should be.
  *
- * For example, above, users will mentally model the page as "App > BlogLayout > BlogPage" intuitively (even if, in
- * practice, the actual hierarchy is "App > BlogPage > BlogLayout").
+ * By setting up your pages like this, Kobweb will call the methods for you in a way that ensures the composition
+ * hierarchy looks like what you'd intuitively expect: `App { Layout { Page() } } }`
  *
- * Without using `@Layout`, a page will still render just fine. However, if users navigate between different pages,
- * Compose will lose any state normally stored inside the layout, because from its point of view, it sees the "layout"
- * composable as a child to each page, so each one is considered separate.
+ * ## Default layouts
  *
- * In other words, the remembered `state` value in a layout like this:
- * ```
- * @Composable
- * fun ExampleLayout(content: @Composable () -> Unit) {
- *   val state = remember { /* ... */ }
- * }
- * ```
- * will be remembered across pages that use `@Layout` to reference the layout method, but it will be discarded on pages
- * that manually call `ExampleLayout` directly themselves:
- * ```
- * // Navigating between the following two pages will result in `ExampleLayout` from resetting any remembered state.
+ * Finally, the `@Layout` annotation can be applied to a file as well, which, as long as it lives under the root `pages`
+ * package, can let users specify a default layout that will apply to all pages defined under that package.
  *
- * @Page
- * @Composable
- * fun FirstPage() {
- *   ExampleLayout { /* ... */ }
- * }
- *
- * @Page
- * @Composable
- * fun SecondPage() {
- *   ExampleLayout { /* ... */ }
- * }
- * ```
- *
- * Users will often want to pass arguments from a child page to its parent layout. If so, the `PageContext` class
- * includes a `data` map they can use for this purpose:
- *
- * ```
- * @Composable
- * fun PageLayout(ctx: PageContext, content: @Composable () -> Unit) {
- *   val title = ctx.data["title"] as? String ?: ""
- *   LaunchedEffect(title) {
- *     document.title = title
- *   }
- * }
- *
- * @Page
- * @Composable
- * @Layout(".components.layout.PageLayout")
- * fun HomePage(ctx: PageContext) {
- *   LaunchedEffect(Unit) {
- *     ctx.data["title"] = "Home"
- *   }
- * }
- * ```
- *
- * Note that a layout can delegate to another layout, like so:
- * ```
- * @Composable()
- * fun PageLayout(content: @Composable () -> Unit) { /* ... */ }
- *
- * @Composable
- * @Layout(".components.layout.PageLayout")
- * fun BlogLayout(content: @Composable () -> Unit) { /* ... */ }
- *
- * @Page
- * @Composable
- * @Layout(".components.layout.BlogLayout")
- * fun BlogPage() { /* ... */ }
- * ```
- * which, before layouts, used to look like this:
- * ```
- * @Composable()
- * fun PageLayout(content: @Composable () -> Unit) { /* ... */ }
-
- * @Composable
- * fun BlogLayout(content: @Composable () -> Unit) {
- *   PageLayout(content)
- * }
- *
- * @Page
- * @Composable
- * @Layout(".components.layout.BlogLayout")
- * fun BlogPage() {
- *   BlogLayout { /* ... */ }
- * }
- * ```
- *
- * And finally, note that the `@Layout` annotation can be applied to a file as well, which, as long as it is nested
- * under the root page package, can let users specify a default layout for all pages under that package. For example,
- * you can set up default layouts for all pages in your site like so:
+ * For example, you can set up default layouts for all pages in your site like so:
  *
  * ```
  * // pages/Layout.kt
@@ -130,7 +67,8 @@ import androidx.compose.runtime.*
  * package pages
  *
  * import com.varabyte.kobweb.core.layout.Layout
- *
+ * ```
+ * ```
  * // pages/blog/Layout.kt
  * @file:Layout(".components.layouts.BlogLayout")
  *
@@ -139,14 +77,8 @@ import androidx.compose.runtime.*
  * import com.varabyte.kobweb.core.layout.Layout
  * ```
  *
- * If a `@Layout` is annotated on a page directly, that will always take precedence. Otherwise, the most specific
- * `@Layout` will be used. For example, if a page is in the `pages.blog` package, it will use the `BlogLayout` above,
- * not the `PageLayout`.
- *
- * Ultimately, users are heavily encouraged to use `@Layout` for their pages (as opposed to manually calling a layout
- * composable method directly inside their page), as remembering layout state across pages is very useful. For example,
- * your layout can contain a sidebar with collapsable sections. When you reference this layout via the `@Layout`
- * annotation, it will remember which sections are collapsed even as you navigate across pages.
+ * When multiple default layouts could apply to a page, the most specific one will be used. For example, if a page is in
+ * the `pages.blog` package, it will use `BlogLayout` above, not `PageLayout`.
  */
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.FILE)
 annotation class Layout(@Suppress("unused") val fqn: String = "")
