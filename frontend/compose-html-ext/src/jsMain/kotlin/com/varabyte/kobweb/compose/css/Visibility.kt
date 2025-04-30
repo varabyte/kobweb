eb659eca857d68a9a86460f99e67206d789cd02b
David Herman <bitspittle@gmail.com>
2025-04-30 14:45:18 -0700

    Convert "Visibility.kt" to sealed interface style

--- frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Visibility.kt
+++ frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Visibility.kt
@@ -3,14 +3,12 @@ package com.varabyte.kobweb.compose.css
 import org.jetbrains.compose.web.css.*
 
 // See: https://developer.mozilla.org/en-US/docs/Web/CSS/visibility
-class Visibility private constructor(private val value: String) : StylePropertyValue {
-    override fun toString() = value
-
+sealed interface Visibility : StylePropertyValue {
     companion object : CssGlobalValues<Visibility> {
         // Keyword
-        val Visible get() = Visibility("visible")
-        val Hidden get() = Visibility("hidden")
-        val Collapse get() = Visibility("collapse")
+        val Visible get() = "visible".unsafeCast<Visibility>()
+        val Hidden get() = "hidden".unsafeCast<Visibility>()
+        val Collapse get() = "collapse".unsafeCast<Visibility>()
     }
 }
 
