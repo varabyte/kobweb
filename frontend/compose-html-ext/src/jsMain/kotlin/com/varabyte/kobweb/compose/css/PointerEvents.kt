2f92a30a7fe98e57533c015939aa438357b7592c
David Herman <bitspittle@gmail.com>
2025-04-30 14:19:11 -0700

    Convert "PointerEventsˆ.kt" to sealed interface style

--- frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/PointerEvents.kt
+++ frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/PointerEvents.kt
@@ -3,13 +3,11 @@ package com.varabyte.kobweb.compose.css
 import org.jetbrains.compose.web.css.*
 
 // See: https://developer.mozilla.org/en-US/docs/Web/CSS/pointer-events
-class PointerEvents private constructor(private val value: String) : StylePropertyValue {
-    override fun toString() = value
-
+sealed interface PointerEvents : StylePropertyValue {
     companion object : CssGlobalValues<PointerEvents> {
         // Keyword
-        val Auto get() = PointerEvents("auto")
-        val None get() = PointerEvents("none")
+        val Auto get() = "auto".unsafeCast<PointerEvents>()
+        val None get() = "none".unsafeCast<PointerEvents>()
     }
 }
 
