87827e5b548df7368284e427d4d8d37d859d7cc1
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2025-05-05 20:13:26 -0400

    Nit: Fix `@InitRoute` doc

--- frontend/kobweb-core/src/jsMain/kotlin/com/varabyte/kobweb/core/init/InitRoute.kt
+++ frontend/kobweb-core/src/jsMain/kotlin/com/varabyte/kobweb/core/init/InitRoute.kt
@@ -2,7 +2,7 @@ package com.varabyte.kobweb.core.init
 
 /**
  * An annotation which identifies a function as one which will be called when a page is about to be rendered.
- * The function should take an [InitKobwebContext] as its only parameter.
+ * The function should take an [InitRouteContext] as its only parameter.
  */
 @Target(AnnotationTarget.FUNCTION)
 annotation class InitRoute
