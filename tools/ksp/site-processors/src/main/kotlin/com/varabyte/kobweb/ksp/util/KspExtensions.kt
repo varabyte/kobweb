b3057af880638b1a1c7e10ef7c0a88e2692fe93f
David Herman <bitspittle@gmail.com>
2025-06-27 11:19:50 -0700

    Super nit: Add header docs for `receiverClass` KSP extension method

--- tools/ksp/site-processors/src/main/kotlin/com/varabyte/kobweb/ksp/util/KspExtensions.kt
+++ tools/ksp/site-processors/src/main/kotlin/com/varabyte/kobweb/ksp/util/KspExtensions.kt
@@ -7,5 +7,8 @@ import com.varabyte.kobweb.common.text.camelCaseToKebabCase
 import com.varabyte.kobweb.common.text.prefixIfNot
 import com.varabyte.kobweb.ksp.symbol.nameWithoutExtension
 
+/**
+ * A receiver class that this method extends, e.g. `Scope` in `fun Scope.someFun()`, or null if none.
+ */
 val KSFunctionDeclaration.receiverClass: KSClassDeclaration?
     get() = extensionReceiver?.resolve()?.declaration as? KSClassDeclaration
