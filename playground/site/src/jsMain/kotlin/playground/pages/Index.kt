ad098970ebbaa3fd0ccdfd1ebece3d0053c7ec55
shevapro <theshevapro@gmail.com>
2025-08-26 10:41:48 -0400

    Update Lucide icon examples on the playground site index page

--- playground/site/src/jsMain/kotlin/playground/pages/Index.kt
+++ playground/site/src/jsMain/kotlin/playground/pages/Index.kt
@@ -1,15 +1,23 @@
 package playground.pages
 
 import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.size
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.core.data.add
 import com.varabyte.kobweb.core.init.InitRoute
 import com.varabyte.kobweb.core.init.InitRouteContext
 import com.varabyte.kobweb.silk.components.forms.TextInput
+import com.varabyte.kobweb.silk.components.icons.fa.FaCheck
+import com.varabyte.kobweb.silk.components.icons.lucide.LucideCamera
+import com.varabyte.kobweb.silk.components.icons.lucide.LucideCheck
+import com.varabyte.kobweb.silk.components.icons.lucide.LucideZoomIn
+import com.varabyte.kobweb.silk.components.icons.mdi.MdiCheck
 import org.jetbrains.compose.web.css.em
+import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
-import com.varabyte.kobweb.silk.components.icons.lucide.*
 
 import playground.components.layouts.PageLayoutData
 
@@ -30,5 +38,10 @@ fun HomePage() {
 
     P()
     Text("Here are some icons:")
-    LucideCamera()
-    LucideZoomIn()}
+    LucideCamera(size = 10.em, color = Colors.Red)
+    LucideZoomIn()
+    LucideCheck(modifier = Modifier.size(20.px))
+
+
+
+}
