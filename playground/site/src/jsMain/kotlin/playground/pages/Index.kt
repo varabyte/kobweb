ab720e88501665c47cb34825ba78b9ce1cd9422b
shevapro <theshevapro@gmail.com>
2025-09-04 00:32:15 -0400

    Remove Lucide icon examples from the playground site index page

--- playground/site/src/jsMain/kotlin/playground/pages/Index.kt
+++ playground/site/src/jsMain/kotlin/playground/pages/Index.kt
@@ -1,24 +1,13 @@
 package playground.pages
 
 import androidx.compose.runtime.*
-import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.graphics.Colors
-import com.varabyte.kobweb.compose.ui.modifiers.size
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.core.data.add
 import com.varabyte.kobweb.core.init.InitRoute
 import com.varabyte.kobweb.core.init.InitRouteContext
 import com.varabyte.kobweb.silk.components.forms.TextInput
-import com.varabyte.kobweb.silk.components.icons.fa.FaCheck
-import com.varabyte.kobweb.silk.components.icons.lucide.LucideCamera
-import com.varabyte.kobweb.silk.components.icons.lucide.LucideCheck
-import com.varabyte.kobweb.silk.components.icons.lucide.LucideZoomIn
-import com.varabyte.kobweb.silk.components.icons.mdi.MdiCheck
-import org.jetbrains.compose.web.css.em
-import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
-
 import playground.components.layouts.PageLayoutData
 
 @InitRoute
@@ -34,14 +23,4 @@ fun HomePage() {
     TextInput(name, onTextChange = { name = it })
     P()
     Text("Hello ${name.takeIf { it.isNotBlank() } ?: "World"}!")
-
-
-    P()
-    Text("Here are some icons:")
-    LucideCamera(size = 10.em, color = Colors.Red)
-    LucideZoomIn()
-    LucideCheck(modifier = Modifier.size(20.px))
-
-
-
 }
