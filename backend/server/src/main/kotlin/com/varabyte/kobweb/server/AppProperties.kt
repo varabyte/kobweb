070bdce419c929895e869b8336dcc44dab8feede
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2025-09-04 17:39:44 -0700

    Fix crash where Kobweb server could fetch the wrong manifest file
    ... at which point it would NPE due to us force querying missing
    properties.
    
    To repro, before this fix...
    
    * download the Jetbrains Runtime with JCEF installed
    * set it as your Gradle runner (e.g. set JAVA_HOME)
    * `kobweb run`
    
    Result:
    
    ```
    * What went wrong:
    Execution failed for task ':site:kobwebStart'.
    > Unable to start the Kobweb server.
      Error: Exception in thread "main" java.lang.NullPointerException
        at com.varabyte.kobweb.server.AppProperties$Companion.fromManifest(AppProperties.kt:19)
        at com.varabyte.kobweb.server.ApplicationKt.main(Application.kt:118)
        ...
        at com.varabyte.kobweb.server.ApplicationKt.main(Application.kt)
    ```
    
    After this change, we now target OUR jar explicitly, so there's no
    chance of getting the wrong manifest.
    
    Fixes #745

--- backend/server/src/main/kotlin/com/varabyte/kobweb/server/AppProperties.kt
+++ backend/server/src/main/kotlin/com/varabyte/kobweb/server/AppProperties.kt
@@ -1,7 +1,8 @@
 package com.varabyte.kobweb.server
 
 import io.ktor.server.application.*
-import java.util.*
+import java.io.File
+import java.util.jar.JarFile
 
 class AppProperties(
     val kobwebVersion: String,
@@ -9,15 +10,14 @@ class AppProperties(
 ) {
     companion object {
         fun fromManifest(): AppProperties {
-            val appProperties = Properties()
-            val manifest = Application::class.java.classLoader.getResource("META-INF/MANIFEST.MF")!!
-            manifest.openStream().use { inputString ->
-                appProperties.load(inputString)
-            }
-
+            // Get this JAR's manifest to read app properties we added from Gradle.
+            // Using `protectionDomain.codeSource` directly targets our JAR, solving a classpath shadowing
+            // issue where searching for "META-INF/MANIFEST.MF" could return the wrong file from a dependency.
+            val uri = Application::class.java.protectionDomain.codeSource.location.toURI()
+            val attributes = JarFile(File(uri)).use { it.manifest }.mainAttributes
             return AppProperties(
-                appProperties.getProperty("Kobweb-Version")!!,
-                appProperties.getProperty("Ktor-Version")!!,
+                attributes.getValue("Kobweb-Version")!!,
+                attributes.getValue("Ktor-Version")!!,
             )
         }
     }
