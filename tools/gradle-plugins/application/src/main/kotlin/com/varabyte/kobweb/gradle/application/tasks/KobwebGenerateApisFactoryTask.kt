14cdfa9865ae0fc73c968c84c8c6457959aabff7
David Herman <bitspittle@gmail.com>
2026-02-11 13:44:56 -0800

    Nit: Optimize imports in KobwebGenerateApisFactoryTask

--- tools/gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/tasks/KobwebGenerateApisFactoryTask.kt
+++ tools/gradle-plugins/application/src/main/kotlin/com/varabyte/kobweb/gradle/application/tasks/KobwebGenerateApisFactoryTask.kt
@@ -2,18 +2,10 @@ package com.varabyte.kobweb.gradle.application.tasks
 
 import com.varabyte.kobweb.gradle.application.extensions.AppBlock
 import com.varabyte.kobweb.gradle.application.templates.createApisFactoryImpl
-import com.varabyte.kobweb.gradle.core.util.searchZipFor
-import com.varabyte.kobweb.ksp.KOBWEB_METADATA_BACKEND
 import com.varabyte.kobweb.project.backend.AppBackendData
-import com.varabyte.kobweb.project.backend.BackendData
-import com.varabyte.kobweb.project.backend.merge
 import kotlinx.serialization.json.Json
-import org.gradle.api.GradleException
-import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import javax.inject.Inject
