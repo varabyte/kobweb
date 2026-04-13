1c18263ba41455d6c1fedbf2005a1ef2d0004835
digitalby <yury@digitalby.me>
2026-04-13 16:01:16 +0200

    feat: scaffold silk-icons-ms module to host Material Symbols icons

--- tools/aggregate-docs/build.gradle.kts
+++ tools/aggregate-docs/build.gradle.kts
@@ -30,6 +30,7 @@ val includedProjects = setOf(
     projects.frontend.silkWidgetsKobweb,
     projects.frontend.silkIconsFa,
     projects.frontend.silkIconsMdi,
+    projects.frontend.silkIconsMs,
     projects.frontend.silkIconsLucide,
     projects.frontend.kobwebxMarkdown,
     projects.frontend.composeHtmlExt,
