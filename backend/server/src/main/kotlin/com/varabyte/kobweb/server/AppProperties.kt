e6b42f5469d43debd396d54487ec1840ea60d09b
David Herman <bitspittle@gmail.com>
2026-06-19 15:14:22 -0700

    Update jar manifest searching logic to ensure server doesn't crash when run in IDE

--- backend/server/src/main/kotlin/com/varabyte/kobweb/server/AppProperties.kt
+++ backend/server/src/main/kotlin/com/varabyte/kobweb/server/AppProperties.kt
@@ -1,6 +1,5 @@
 package com.varabyte.kobweb.server
 
-import io.ktor.server.application.*
 import java.io.File
 import java.util.jar.JarFile
 
@@ -13,11 +12,13 @@ class AppProperties(
             // Get this JAR's manifest to read app properties we added from Gradle.
             // Using `protectionDomain.codeSource` directly targets our JAR, solving a classpath shadowing
             // issue where searching for "META-INF/MANIFEST.MF" could return the wrong file from a dependency.
-            val uri = Application::class.java.protectionDomain.codeSource.location.toURI()
-            val attributes = JarFile(File(uri)).use { it.manifest }.mainAttributes
+            val uri = AppProperties::class.java.protectionDomain.codeSource.location.toURI()
+            val attributes = if (uri.path.endsWith(".jar")) {
+                JarFile(File(uri)).use { it.manifest }.mainAttributes
+            } else null // Will be null when running from the IDE
             return AppProperties(
-                attributes.getValue("Kobweb-Version")!!,
-                attributes.getValue("Ktor-Version")!!,
+                attributes?.getValue("Kobweb-Version") ?: "?",
+                attributes?.getValue("Ktor-Version") ?: "?",
             )
         }
     }
