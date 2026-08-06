a82c6562fe6d6bf092f338178085438a5fdc920f
David Herman <bitspittle@gmail.com>
2026-08-05 22:27:46 -0700

    Nit: Tweak inconsistent styles / approaches in Contain.kt

--- frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Contain.kt
+++ frontend/compose-html-ext/src/jsMain/kotlin/com/varabyte/kobweb/compose/css/Contain.kt
@@ -2,13 +2,23 @@ package com.varabyte.kobweb.compose.css
 
 import org.jetbrains.compose.web.css.*
 
+@Suppress("PropertyName", "FunctionName")
+internal sealed interface CssContainIntrinsicValues<T : StylePropertyValue> {
+    fun of(value: CSSLengthNumericValue) = "$value".unsafeCast<T>()
+    fun Auto(value: CSSLengthNumericValue) = "auto $value".unsafeCast<T>()
+
+    // Keywords
+    val None get() = "none".unsafeCast<T>()
+}
+
 // https://developer.mozilla.org/en-US/docs/Web/CSS/contain
 sealed interface Contain : StylePropertyValue {
-
     sealed interface Listable : Contain
 
     companion object : CssGlobalValues<Contain> {
-        /* Keyword values */
+        fun list(vararg values: Listable): Contain = values.joinToString(" ").unsafeCast<Contain>()
+
+        // Keywords
         val None: Contain get() = "none".unsafeCast<Contain>()
         val Strict: Contain get() = "strict".unsafeCast<Contain>()
         val Content: Contain get() = "content".unsafeCast<Contain>()
@@ -17,9 +27,6 @@ sealed interface Contain : StylePropertyValue {
         val Layout: Listable get() = "layout".unsafeCast<Listable>()
         val Style: Listable get() = "style".unsafeCast<Listable>()
         val Paint: Listable get() = "paint".unsafeCast<Listable>()
-
-        /* Multiple keywords */
-        fun list(vararg values: Listable): Contain = values.joinToString(" ").unsafeCast<Contain>()
     }
 }
 
@@ -29,7 +36,6 @@ fun StyleScope.contain(contain: Contain) {
 
 // https://developer.mozilla.org/en-US/docs/Web/CSS/contain-intrinsic-block-size
 sealed interface ContainIntrinsicBlockSize : StylePropertyValue {
-
     companion object : CssContainIntrinsicValues<ContainIntrinsicBlockSize>, CssGlobalValues<ContainIntrinsicBlockSize>
 }
 
@@ -37,27 +43,12 @@ fun StyleScope.containIntrinsicBlockSize(containIntrinsicBlockSize: ContainIntri
     property("contain-intrinsic-block-size", containIntrinsicBlockSize)
 }
 
-
 // https://developer.mozilla.org/en-US/docs/Web/CSS/contain-intrinsic-inline-size
 sealed interface ContainIntrinsicInlineSize : StylePropertyValue {
-
     companion object : CssContainIntrinsicValues<ContainIntrinsicInlineSize>,
         CssGlobalValues<ContainIntrinsicInlineSize>
 }
 
 fun StyleScope.containIntrinsicInlineSize(containIntrinsicInlineSize: ContainIntrinsicInlineSize) {
     property("contain-intrinsic-inline-size", containIntrinsicInlineSize)
-}
-
-internal sealed interface CssContainIntrinsicValues<T : StylePropertyValue> {
-    /* Keyword Values */
-    val None get() = "none".unsafeCast<T>()
-
-    /* withAuto <length>  & <length> values */
-    fun of(value: CSSLengthNumericValue, auto: Boolean = false) = buildString {
-        if (auto) append("auto ")
-        append(value)
-    }.unsafeCast<T>()
-
-    fun Auto(value: CSSLengthNumericValue) = of(value, auto = true)
 }
